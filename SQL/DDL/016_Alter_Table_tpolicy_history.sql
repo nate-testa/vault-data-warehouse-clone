0997d9b92f0440ae73f80a6d3eee35c88174729d
Sandeep Gundreddy <sandeep.gundreddy@vault.insurance>
2023-09-25 23:37:44 -0400

    Added new columns to tpolicy_history

---
+++ SQL/DDL/016_Alter_Table_tpolicy_history.sql
@@ -0,0 +1,9 @@
+ALTER TABLE edw_core.tpolicy_history ADD
+commission_pc float,
+override_commission_pc float,
+commission_retention  varchar(255),
+premium_adjustment_method varchar(255),
+premium_adjustment_value  float,
+premium_adjustment_retention varchar(255),
+premium_adjustement_reason   varchar(255)
+;
