88b5b0f0891edde31cc3cd9ad08be209e06de7bd
Sandeep Gundreddy <sandeep.gundreddy@vault.insurance>
2023-09-26 21:23:25 -0400

    Deleted premium mod columns from the table

--- SQL/DDL/016_Alter_Table_tpolicy_history.sql
+++ SQL/DDL/016_Alter_Table_tpolicy_history.sql
@@ -1,9 +1,5 @@
 ALTER TABLE edw_core.tpolicy_history ADD
 commission_pc float,
 override_commission_pc float,
-commission_retention  varchar(255),
-premium_adjustment_method varchar(255),
-premium_adjustment_value  float,
-premium_adjustment_retention varchar(255),
-premium_adjustement_reason   varchar(255)
+commission_retention  varchar(255)
 ;
