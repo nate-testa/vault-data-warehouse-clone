dbb6e9eaf1161115a4297768a52680b8e6dbe01f
Hernando Gonzalez Garcia <Hernando.Gonzalez.Garcia@Vault.Insurance>
2023-10-12 09:18:37 -0500

    Ivans Home SP and renamed SP for Customer

--- SQL/Stored Procedures/sp_get_policy_customer_portal_api_search.sql
+++ SQL/Stored Procedures/sp_get_policy_customer_portal_api_search.sql
@@ -7,9 +7,9 @@ GO
 -- =================================================================================================
 -- Author:		Hernando Gonzalez Garcia
 -- Create Date: 2023-10-05
--- Description: This stored search data related to Billing Account
+-- Description: This stored search data related to Policy Customer
 -- ================================================================================================= 
-CREATE OR ALTER PROCEDURE [edw_integration].[sp_billing_account_customer_portal_api_search]
+CREATE OR ALTER PROCEDURE [edw_integration].[sp_get_policy_customer_portal_api_search]
 (
   @billingaccount_no varchar(255)
 )
@@ -20,26 +20,15 @@ BEGIN
     SET NOCOUNT ON
 
 	SELECT
-	[billingaccount_no]
-      ,[first_nm]
-      ,[last_nm]
-      ,[mailing_address_line_1]
-      ,[mailing_address_line_2]
-      ,[mailing_city_nm]
-      ,[mailing_state_cd]
-      ,[mailing_zip_cd]
-      ,[email]
-      ,[auto_pay_in]
-      ,[birth_dt]
-      ,[effective_dt]
-      ,[expiration_dt]
-      ,[payor_nm]
-      ,[phone_no]
+	[policy_no]
+      ,[billingaccount_no]
+      ,[product_nm]
+      ,[insured_nm]
       ,[create_ts]
       ,[update_ts]
       ,[etl_audit_sk]
 	FROM
-	[edw_integration].[billing_account_customer_portal_api]
+	[edw_integration].[policy_customer_portal_api]
 	WHERE  
 		[billingaccount_no]=@billingaccount_no
 END
