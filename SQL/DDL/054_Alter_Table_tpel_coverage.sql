d9e8b7770f09f0921cec009d5b2b3da5df6f6bdf
Sandeep Gundreddy <sandeep.gundreddy@vault.insurance>
2023-10-19 21:39:56 -0400

    data model changes to auto and pel

---
+++ SQL/DDL/054_Alter_Table_tpel_coverage.sql
@@ -0,0 +1,5 @@
+ALTER TABLE edw_core.tpel_coverage ADD profile_adjustment varchar(255);
+ALTER TABLE edw_core.tpel_coverage ADD criminal_traffic_violation_in varchar(255);
+ALTER TABLE edw_core.tpel_coverage ADD criminal_traffic_violation_desc nvarchar(max);
+ALTER TABLE edw_core.tpel_coverage ADD youthful_drivers_ct varchar(255);
+ALTER TABLE edw_core.tpel_coverage ADD adult_drivers_ct varchar(255);
---
+++ SQL/DDL/056_Alter_Table_tauto_vehicle.sql
@@ -0,0 +1,2 @@
+ALTER TABLE edw_core.tauto_vehicle ADD vehicle_vin_invalid_in      varchar(255);
+ALTER TABLE edw_core.tauto_vehicle ADD vehicle_vin_invalid_message varchar(255);
