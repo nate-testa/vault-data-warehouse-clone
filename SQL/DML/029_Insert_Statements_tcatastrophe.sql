a085f0d3d82082c387873fb25792ea30dbae8e98
Sandeep Gundreddy <sandeep.gundreddy@vault.insurance>
2023-11-17 23:34:16 -0500

    prod batch related fixes

---
+++ SQL/DML/029_Insert_Statements_tcatastrophe.sql
@@ -0,0 +1,29 @@
+INSERT INTO vault_edw.edw_core.tcatastrophe
+(catastrophe_cd, catastrophe_nm, catastrophe_desc, source_system_sk, create_ts, update_ts, etl_audit_sk)
+VALUES('1744', 'Hurricane Irma (09.06.17 - 09.07.17)', 'DATES: September 6, 2017 To September 7, 2017 ---- STATES: Puerto Rico, U.S. Virgin Islands, and Possibly Other Areas ---- PERILS: Flooding, Hurricane, Wind ---- STORM FAMILY: Hurricane Irma', 1, 
+getdate(), getdate(), 0);
+
+INSERT INTO vault_edw.edw_core.tcatastrophe
+(catastrophe_cd, catastrophe_nm, catastrophe_desc, source_system_sk, create_ts, update_ts, etl_audit_sk)
+VALUES('1857', 'Hurricane Michael (10.10.18 - 10.10.18)', 'DATES: October 10, 2018 To October 10, 2018 ---- STATES: Alabama, Florida, Georgia, and Possibly Other Areas ---- PERILS: Flooding, Hurricane, Wind ---- STORM FAMILY: Hurricane Michael', 1, 
+getdate(), getdate(), 0);
+
+INSERT INTO vault_edw.edw_core.tcatastrophe
+(catastrophe_cd, catastrophe_nm, catastrophe_desc, source_system_sk, create_ts, update_ts, etl_audit_sk)
+VALUES('2020', 'Wind and Thunderstorm Event (04.10.20 - 04.12.20)', 'DATES: April 10, 2020 To April 12, 2020 ---- STATES: Kansas, Nebraska, Oklahoma, Texas, and Possibly Other Areas ---- PERILS: Flooding, Hail, Wind ---- STORM FAMILY: Wind and Thunderstorm Event', 1, 
+getdate(), getdate(), 0);
+
+INSERT INTO vault_edw.edw_core.tcatastrophe
+(catastrophe_cd, catastrophe_nm, catastrophe_desc, source_system_sk, create_ts, update_ts, etl_audit_sk)
+VALUES('2063', 'Hurricane Sally (09.14.20 - 09.15.20)', 'DATES: September 14, 2020 To September 15, 2020 ---- STATES: Alabama, Florida, Louisiana, Mississippi, and Possibly Other Areas ---- PERILS: Flooding, Hurricane, Wind ---- STORM FAMILY: Hurricane Sally', 1, 
+getdate(), getdate(), 0);
+
+INSERT INTO vault_edw.edw_core.tcatastrophe
+(catastrophe_cd, catastrophe_nm, catastrophe_desc, source_system_sk, create_ts, update_ts, etl_audit_sk)
+VALUES('2076', 'Tropical Storm Eta (11/08/2020 - 11/12/2020)', 'DATES: November 8, 2020 To November 11, 2020. ---- EXTENDED TO INCLUDE: November 11, 2020 To November 12, 2020. ---- STATES: Florida, and Possibly Other Areas. ---- PERILS: Flooding, Hurricane, Tropical Storm, Wind. ---- STORM FAMILY: Tropical Storm Eta', 1, 
+getdate(), getdate(), 0);
+
+INSERT INTO vault_edw.edw_core.tcatastrophe
+(catastrophe_cd, catastrophe_nm, catastrophe_desc, source_system_sk, create_ts, update_ts, etl_audit_sk)
+VALUES('MAJ-19', 'COVID (03.11.20 - 02.28.21)', 'DATES: March 11, 2020 To February 28, 2021 ---- STATES: All ---- PERILS: NA ---- STORM FAMILY: NA', 1, 
+getdate(), getdate(), 0);
