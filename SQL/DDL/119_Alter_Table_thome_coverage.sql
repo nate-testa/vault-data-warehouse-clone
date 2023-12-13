5fb36511d55494aea237e4c651f858b60ac61760
Architha Gudimalla <architha.gudimalla@vault.insurance>
2023-12-12 23:25:43 -0500

    added_new_cols

---
+++ SQL/DDL/119_Alter_Table_thome_coverage.sql
@@ -0,0 +1,2 @@
+ALTER TABLE edw_core.thome_coverage ADD wind_or_hailstorm_deductible_manual varchar(255);
+ALTER TABLE edw_core.tquote_home_coverage ADD wind_or_hailstorm_deductible_manual varchar(255);
