fb1c1b939ac22e8d233085e8bbd3e1dd950869d2
Alberto Almario <alberto.almario@vault.insurance>
2024-01-29 18:45:57 -0500

    change

--- SQL/Stored Procedures/000_temp_research_4803.sql
+++ SQL/Stored Procedures/000_temp_research_4803.sql
@@ -1,10 +1,5 @@
-SELECT * FROM edw_core.tpolicy WHERE policy_no = 'AU100064161';
+select * from edw_core.tpolicy_history where policy_no =  'AU100004762-01';
+select DISTINCT policy_no, vehicle_vin from edw_core.tauto_vehicle where trim(vehicle_type) = '/';--299
 
-SELECT pt.policy_sk, pt.effective_dt_sk, ic.internal_coverage_desc ,sum(premium_amt) as premium_amt, sum(net_premium_amt) as net_premium_amt, sum(annual_premium_amt) annual_premium_amt
-FROM edw_core.tpolicy_transaction pt 
-LEFT JOIN edw_core.tinternal_coverage ic ON ic.internal_coverage_sk = pt.internal_coverage_sk
-WHERE pt.policy_sk = 85654
-AND pt.transaction_seq_no = 4
-GROUP BY pt.policy_sk, pt.effective_dt_sk, ic.internal_coverage_desc
-;
 
+select * from edw_core.tauto_vehicle where policy_no =  'AU100012285';
