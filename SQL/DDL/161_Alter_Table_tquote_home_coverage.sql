13359154de38891ae23525d44dc62152f37327e5
Sandeep Gundreddy <sandeep.gundreddy@vault.insurance>
2024-02-21 17:10:05 -0500

    Metal Feb Release related DB changes

---
+++ SQL/DDL/161_Alter_Table_tquote_home_coverage.sql
@@ -0,0 +1,8 @@
+ALTER TABLE edw_core.tquote_home_coverage ADD aon_hurricane_cat_score_amt varchar(255);
+ALTER TABLE edw_core.tquote_home_coverage ADD aon_hurricane_reinsurance_margin_amt varchar(255);
+ALTER TABLE edw_core.tquote_home_coverage ADD aon_hurricane_ceded_loss_amt varchar(255);
+ALTER TABLE edw_core.tquote_home_coverage ADD aon_hurricane_reinsurance_premium_amt varchar(255);
+ALTER TABLE edw_core.tquote_home_coverage ADD aon_hurricane_capital_cost_amt varchar(255);
+ALTER TABLE edw_core.tquote_home_coverage ADD aon_hurricane_cat_score_to_premium_ratio varchar(255);
+ALTER TABLE edw_core.tquote_home_coverage ADD aon_hurricane_aal_to_premium_ratio varchar(255);
+ALTER TABLE edw_core.tquote_home_coverage ADD aon_hurricane_aal_amt varchar(255);
---
+++ SQL/DDL/163_Alter_Table_tquote_auto_vehicle_coverage.sql
@@ -0,0 +1,2 @@
+ALTER TABLE edw_core.tquote_auto_vehicle_coverage ADD carfax_wholesale_value_amt varchar(255);
+
