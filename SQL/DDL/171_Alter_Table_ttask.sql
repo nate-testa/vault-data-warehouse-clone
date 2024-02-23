4077fc9206e094d22b9028dfbbc0f0a52a8476cc
Sandeep Gundreddy <sandeep.gundreddy@vault.insurance>
2024-02-23 18:54:42 -0500

    Modified DB script to prevent release issues

--- SQL/DDL/171_Alter_Table_ttask.sql
+++ SQL/DDL/171_Alter_Table_ttask.sql
@@ -1,6 +1,15 @@
 ALTER TABLE edw_core.ttask ALTER COLUMN policy_no varchar(255) NULL;
 ALTER TABLE edw_core.ttask ALTER COLUMN effective_dt date NULL;
-ALTER TABLE edw_core.ttask ADD customer_sk int NULL; 
+
+IF NOT EXISTS (
+    SELECT 1
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE 
+    TABLE_SCHEMA='edw_core'
+    AND TABLE_NAME = 'ttask'
+    AND COLUMN_NAME = 'customer_sk'
+) BEGIN ALTER TABLE edw_core.ttask ADD customer_sk decimal (15,2) null END;
+
 
  IF OBJECT_ID('edw_core.uidx_ttask_polno_effdt_task')  IS NOT NULL 
     ALTER TABLE edw_core.ttask DROP CONSTRAINT uidx_ttask_polno_effdt_task;
