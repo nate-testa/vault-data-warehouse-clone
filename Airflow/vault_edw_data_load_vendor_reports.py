94b23b7026822f05f9146fea2ce3f0e7ee1126f4
Alberto Almario <alberto.almario@vault.insurance>
2024-03-12 17:41:02 -0500

    Chnages on quotes, vendor reports and release notes DAGs

--- Airflow/vault_edw_data_load_vendor_reports.py
+++ Airflow/vault_edw_data_load_vendor_reports.py
@@ -106,15 +106,7 @@ with DAG(
             database="vault_edw",
             autocommit=True,
         )
-
-        sp_tvendor_report_ClueProperty = MsSqlOperator(
-            task_id='sp_tvendor_report_ClueProperty',
-            mssql_conn_id='Vault_EDW',
-            sql="EXEC edw_core.sp_tvendor_report 'Clue Property'",
-            database="vault_edw",
-            autocommit=True,
-        )
-
+        
         sp_tvendor_report_GuyCarpenter = MsSqlOperator(
             task_id='sp_tvendor_report_GuyCarpenter',
             mssql_conn_id='Vault_EDW',
@@ -163,18 +155,18 @@ with DAG(
             autocommit=True,
         )
 
-        sp_tvendor_report_SAQ = MsSqlOperator(
-            task_id='sp_tvendor_report_SAQ',
-            mssql_conn_id='Vault_EDW',
-            sql="EXEC edw_core.sp_tvendor_report 'SAQ'",
-            database="vault_edw",
-            autocommit=True,
-        )
-
         sp_tvendor_report_TransUnion = MsSqlOperator(
             task_id='sp_tvendor_report_TransUnion',
             mssql_conn_id='Vault_EDW',
             sql="EXEC edw_core.sp_tvendor_report 'TransUnion'",
+            database="vault_edw",
+            autocommit=True,
+        )
+
+        sp_tvendor_report_SAQ = MsSqlOperator(
+            task_id='sp_tvendor_report_SAQ',
+            mssql_conn_id='Vault_EDW',
+            sql="EXEC edw_core.sp_tvendor_report 'SAQ'",
             database="vault_edw",
             autocommit=True,
         )
@@ -186,7 +178,7 @@ with DAG(
             html_content=get_sp_success_data_HTML(vendor_report_group_items, 'All executions of stored procedure vendor report executed successfully'),
         )
 
-        sp_tvendor_report_stage_data >> sp_tvendor_report_AonCatStore >> sp_tvendor_report_CarfaxMileage >> sp_tvendor_report_CarfaxValue >> sp_tvendor_report_CLUEAuto >> sp_tvendor_report_ClueProperty >> sp_tvendor_report_GuyCarpenter >> sp_tvendor_report_IsoVehicle >> sp_tvendor_report_IsoProperty >> sp_tvendor_report_LC360 >> sp_tvendor_report_MVR >> sp_tvendor_report_NHTSA >> sp_tvendor_report_SAQ >> sp_tvendor_report_TransUnion >> send_vendor_report_email
+        sp_tvendor_report_stage_data >> sp_tvendor_report_AonCatStore >> sp_tvendor_report_CarfaxMileage >> sp_tvendor_report_CarfaxValue >> sp_tvendor_report_CLUEAuto >> sp_tvendor_report_GuyCarpenter >> sp_tvendor_report_IsoVehicle >> sp_tvendor_report_IsoProperty >> sp_tvendor_report_LC360 >> sp_tvendor_report_MVR >> sp_tvendor_report_NHTSA >> sp_tvendor_report_TransUnion >> sp_tvendor_report_SAQ >> send_vendor_report_email
 
 
     end = DummyOperator(
