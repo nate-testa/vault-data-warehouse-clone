4eb582b7226e8940379e51a9c8824312823817c4
Sandeep Gundreddy <sandeep.gundreddy@vault.insurance>
2024-03-13 23:31:36 -0400

    Data Model Changes

---
+++ SQL/DDL/197_Alter_Table_policy_hsb_cyber_feed.sql
@@ -0,0 +1,1 @@
+EXEC sp_rename '[edw_integration].[policy_hsb_cyber_feed].slc_limit_amt', 'hcp_limit_amt', 'COLUMN';
---
+++ SQL/DDL/200_Create_STAGE_dw2_oneshield_migrated.sql
@@ -0,0 +1,5 @@
+CREATE TABLE edw_stage.dw2_oneshield_migrated
+(
+policynumber varchar(255),
+issystemconversion  varchar(255)
+);
