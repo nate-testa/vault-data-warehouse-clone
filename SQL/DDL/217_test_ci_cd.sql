77a3aec939e4b99aaee6d46d8877f991c4de6852
Sandeep Gundreddy <sandeep.gundreddy@vault.insurance>
2024-03-25 15:59:01 -0400

    test CI/CD

--- SQL/DDL/217_test_ci_cd.sql
+++ SQL/DDL/217_test_ci_cd.sql
@@ -7,3 +7,5 @@ WHERE name='idx_tpolicy_transaction_create_ts' AND object_id = OBJECT_ID('edw_co
 BEGIN
 CREATE INDEX idx_tpolicy_transaction_create_ts ON edw_core.tpolicy_transaction(create_ts)
 END;
+
+---This is a TEST
