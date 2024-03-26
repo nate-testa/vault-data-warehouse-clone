c325dfdbdb36e03f045f683fa2d726cdb8d37c70
Architha Gudimalla <architha.gudimalla@vault.insurance>
2024-03-25 20:25:06 -0400

    Updated

--- SQL/Stored Procedures/sp_tpolicy_transaction_update.sql
+++ SQL/Stored Procedures/sp_tpolicy_transaction_update.sql
@@ -32,6 +32,7 @@ BEGIN
 		update edw_core.tpolicy_transaction 
 		set collection_class_type_sk = 0;
 
+		--update all policies that have an exact match on class type
 		update a
 		set a.collection_class_type_sk = b.collection_class_type_sk
 		from edw_core.tpolicy_transaction a 
@@ -46,13 +47,13 @@ BEGIN
 																			else replace(replace(ic.internal_coverage_cd,' (Blanket)',''),' (Scheduled)','')
 																		end = cc.class_type   
 					where 	tr.collection_class_type_sk = 0
-					and		pol.source_system_sk <> 1 
 					and 	tr.product_sk in (1,2,5) 
 					and 	ic.primary_coverage_cd = 'Lux' 
 					and 	ic.internal_coverage_category_nm = 'Premium'
 					group by policy_transaction_sk, tr.transaction_seq_no,  internal_coverage_cd,  cc.class_type
 		) b on a.policy_transaction_sk = b.policy_transaction_sk;
 
+		--update all policies that have class type as Jewelry (Scheduled) in tpolicy_transaction and class type as Bank Vaulted Jewelry in tcollection_class_type
 		update a
 		set a.collection_class_type_sk = b.collection_class_type_sk
 		from edw_core.tpolicy_transaction a 
@@ -62,15 +63,33 @@ BEGIN
 					inner join  edw_core.tpolicy_transaction tr on pol.policy_sk = tr.policy_sk  
 					inner join 	edw_core.tinternal_coverage ic on ic.internal_coverage_sk = tr.internal_coverage_sk 
 					inner join 	edw_core.tcollection_class_type cc on     pol.policy_no = cc.policy_no and pol.effective_dt = cc.effective_dt and tr.transaction_seq_no = cc.transaction_seq_no 
-																	and internal_coverage_cd = 'Jewelry (Scheduled)' and cc.class_type like '%jew%' 
+																	and internal_coverage_cd = 'Jewelry (Scheduled)' and cc.class_type like '%Bank Vaulted Jewelry%'
 					where 	tr.collection_class_type_sk = 0
-					and		pol.source_system_sk <> 1 
+					and 	tr.product_sk in (1,2,5) 
+					and 	ic.primary_coverage_cd = 'Lux' 
+					and 	ic.internal_coverage_category_nm = 'Premium' --and pol.policy_sk = 111021
+					group by policy_transaction_sk, tr.transaction_seq_no,  internal_coverage_cd,  cc.class_type
+		) b on a.policy_transaction_sk = b.policy_transaction_sk;
+
+		--update all policies that have class type as Jewelry (Blanket) in tpolicy_transaction and class type as Bank Vaulted Jewelry in tcollection_class_type
+		update a
+		set a.collection_class_type_sk = b.collection_class_type_sk
+		from edw_core.tpolicy_transaction a 
+		inner join (
+					select  policy_transaction_sk, tr.transaction_seq_no,  internal_coverage_cd,  cc.class_type ,  max(cc.collection_class_type_sk  ) collection_class_type_sk
+					from 		edw_core.tpolicy pol
+					inner join  edw_core.tpolicy_transaction tr on pol.policy_sk = tr.policy_sk  
+					inner join 	edw_core.tinternal_coverage ic on ic.internal_coverage_sk = tr.internal_coverage_sk 
+					inner join 	edw_core.tcollection_class_type cc on     pol.policy_no = cc.policy_no and pol.effective_dt = cc.effective_dt and tr.transaction_seq_no = cc.transaction_seq_no 
+																	and internal_coverage_cd = 'Jewelry (Blanket)' and cc.class_type like '%Worldwide Jewelry%'-- and cc.blanket_limit_amt <> 0
+					where 	tr.collection_class_type_sk = 0
 					and 	tr.product_sk in (1,2,5) 
 					and 	ic.primary_coverage_cd = 'Lux' 
 					and 	ic.internal_coverage_category_nm = 'Premium'
 					group by policy_transaction_sk, tr.transaction_seq_no,  internal_coverage_cd,  cc.class_type
 		) b on a.policy_transaction_sk = b.policy_transaction_sk;
 
+		--update all policies that do not have the same seq record in tcollection_class_type to a record prior to max
 		update a
 		set a.collection_class_type_sk = b.collection_class_type_sk
 		from edw_core.tpolicy_transaction a 
@@ -85,10 +104,27 @@ BEGIN
 																			else replace(replace(ic.internal_coverage_cd,' (Blanket)',''),' (Scheduled)','')
 																		end = cc.class_type   
 					where 	tr.collection_class_type_sk = 0
-					and		pol.source_system_sk <> 1 
 					and 	tr.product_sk in (1,2,5) 
 					and 	ic.primary_coverage_cd = 'Lux' 
 					and 	ic.internal_coverage_category_nm = 'Premium'
+					group by policy_transaction_sk, tr.transaction_seq_no,  internal_coverage_cd,  cc.class_type
+		) b on a.policy_transaction_sk = b.policy_transaction_sk;
+
+		--update all policies that do not have the same seq record in tcollection_class_type to a record prior to max for pols that have class type as Jewelry (Blanket) in tpolicy_transaction and class type as Bank Vaulted Jewelry in tcollection_class_type
+		update a
+		set a.collection_class_type_sk = b.collection_class_type_sk
+		from edw_core.tpolicy_transaction a 
+		inner join (
+					select  policy_transaction_sk, tr.transaction_seq_no,  internal_coverage_cd,  cc.class_type ,  max(cc.collection_class_type_sk  ) collection_class_type_sk
+					from 		edw_core.tpolicy pol
+					inner join  edw_core.tpolicy_transaction tr on pol.policy_sk = tr.policy_sk  
+					inner join 	edw_core.tinternal_coverage ic on ic.internal_coverage_sk = tr.internal_coverage_sk 
+					inner join 	edw_core.tcollection_class_type cc on     pol.policy_no = cc.policy_no and pol.effective_dt = cc.effective_dt and tr.transaction_seq_no > cc.transaction_seq_no 
+																	and internal_coverage_cd = 'Jewelry (Scheduled)' and cc.class_type like '%Bank Vaulted Jewelry%' 
+					where 	tr.collection_class_type_sk = 0
+					and 	tr.product_sk in (1,2,5) 
+					and 	ic.primary_coverage_cd = 'Lux' 
+					and 	ic.internal_coverage_category_nm = 'Premium' --and pol.policy_sk = 111021
 					group by policy_transaction_sk, tr.transaction_seq_no,  internal_coverage_cd,  cc.class_type
 		) b on a.policy_transaction_sk = b.policy_transaction_sk;
 
