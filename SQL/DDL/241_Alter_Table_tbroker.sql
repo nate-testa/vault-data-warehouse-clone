2bd567fe43cc895f7c1683db3d3e7587ef555285
Rushin Shah <rushin.shah@vault.insurance>
2024-04-10 15:43:40 -0400

    DDL to add 3 columns to tbroker

---
+++ SQL/DDL/241_Alter_Table_tbroker.sql
@@ -0,0 +1,3 @@
+ALTER TABLE vault_edw.edw_core.tbroker ADD related_broker_id varchar(255);
+ALTER TABLE vault_edw.edw_core.tbroker ADD related_brokerage_nm varchar(255);
+ALTER TABLE vault_edw.edw_core.tbroker ADD relationship_type varchar(255);
