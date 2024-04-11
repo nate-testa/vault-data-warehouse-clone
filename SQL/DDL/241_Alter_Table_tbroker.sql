09508ad145b5e582c63d31244a669e80c053e381
Rushin Shah <rushin.shah@vault.insurance>
2024-04-11 17:20:50 -0400

    DDL for tbroker_relation table

--- SQL/DDL/241_Alter_Table_tbroker.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE vault_edw.edw_core.tbroker ADD related_broker_id varchar(255);
-ALTER TABLE vault_edw.edw_core.tbroker ADD related_brokerage_nm varchar(255);
-ALTER TABLE vault_edw.edw_core.tbroker ADD relationship_type varchar(255);
