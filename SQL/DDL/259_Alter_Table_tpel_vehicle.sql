5667c58b725787222e449c3c841f7b7fa7c83ea7
Rushin Shah <rushin.shah@vault.insurance>
2024-05-01 15:28:34 -0400

    DDL for May Release additional fields

---
+++ SQL/DDL/259_Alter_Table_tpel_vehicle.sql
@@ -0,0 +1,8 @@
+ALTER TABLE edw_core.tpel_vehicle ADD garage_address_line1 varchar(255);
+ALTER TABLE edw_core.tpel_vehicle ADD garage_address_line2 varchar(255);
+ALTER TABLE edw_core.tpel_vehicle ADD garage_address_unit_no varchar(255);
+ALTER TABLE edw_core.tpel_vehicle ADD garage_address_city_nm varchar(255);
+ALTER TABLE edw_core.tpel_vehicle ADD garage_address_zip_cd varchar(255);
+ALTER TABLE edw_core.tpel_vehicle ADD garage_address_state_cd varchar(255);
+ALTER TABLE edw_core.tpel_vehicle ADD garage_address_county_nm varchar(255);
+ALTER TABLE edw_core.tpel_vehicle ADD garage_address_country_nm varchar(255);
