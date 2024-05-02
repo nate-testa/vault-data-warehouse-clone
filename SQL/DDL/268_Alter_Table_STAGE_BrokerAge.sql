efa37e1f61f45ecf385c926cef9e90126c2d9ff2
Hernando Gonzalez Garcia <Hernando.Gonzalez.Garcia@Vault.Insurance>
2024-05-02 14:04:47 -0500

    Add Tier column to BrokerAge

---
+++ SQL/DDL/268_Alter_Table_STAGE_BrokerAge.sql
@@ -0,0 +1,7 @@
+IF NOT EXISTS (SELECT *
+        FROM INFORMATION_SCHEMA.COLUMNS
+        WHERE lower(TABLE_NAME) = 'Brokerage'
+        AND LOWER(COLUMN_NAME) = 'Tier')
+BEGIN
+    ALTER TABLE edw_stage.Brokerage ADD Tier INT NULL;
+END
