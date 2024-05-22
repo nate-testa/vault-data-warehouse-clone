533cf9736d39c7fec3364cbe80754544e4fa6772
Sandeep Gundreddy <sandeep.gundreddy@vault.insurance>
2024-05-22 11:58:38 -0400

    Added prem mod columns Lux tables

---
+++ SQL/DDL/278_Alter_Table_tcollection_class_type.sql
@@ -0,0 +1,23 @@
+ALTER TABLE edw_core.tcollection_class_type
+ADD blanket_premium_adjustment_method varchar(255);
+
+ALTER TABLE edw_core.tcollection_class_type
+ADD blanket_premium_adjustment_factor decimal(16,4);
+
+ALTER TABLE edw_core.tcollection_class_type
+ADD blanket_premium_adjustment_retention varchar(255);
+
+ALTER TABLE edw_core.tcollection_class_type
+ADD blanket_premium_adjustment_retention_reason varchar(255);
+
+ALTER TABLE edw_core.tcollection_class_type
+ADD  scheduled_premium_adjustment_method varchar(255);
+
+ALTER TABLE edw_core.tcollection_class_type
+ADD scheduled_premium_adjustment_factor decimal(16,4);
+
+ALTER TABLE edw_core.tcollection_class_type
+ADD scheduled_premium_adjustment_retention varchar(255);
+
+ALTER TABLE edw_core.tcollection_class_type
+ADD scheduled_premium_adjustment_retention_reason varchar(255);
