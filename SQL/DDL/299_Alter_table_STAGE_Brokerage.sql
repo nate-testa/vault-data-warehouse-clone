4c51f4ca53478ba91894d0266b24f55f725134ea
Rushin Shah <rushin.shah@vault.insurance>
2024-06-04 11:48:44 -0400

    Update to Brokerage Table on Stage

---
+++ SQL/DDL/299_Alter_table_STAGE_Brokerage.sql
@@ -0,0 +1,7 @@
+IF NOT EXISTS (SELECT *
+        FROM INFORMATION_SCHEMA.COLUMNS
+        WHERE lower(TABLE_NAME) = 'Brokerage'
+        AND LOWER(COLUMN_NAME) = 'IsNationalAgency')
+BEGIN
+    ALTER TABLE edw_stage.Brokerage ADD IsNationalAgency INT NULL;
+END
