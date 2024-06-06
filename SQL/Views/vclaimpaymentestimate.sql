c90d60b37583599228e0104c9a76c8af4afa306f
yunusmohammedvault <yunus.mohammed@vault.insurance>
2024-06-06 20:21:13 +0530

    Created  view

---
+++ SQL/Views/vclaimpaymentestimate.sql
@@ -0,0 +1,13 @@
+-- ==============================================================================
+-- Author:		Yunus Mohammed 
+-- Description: This view returns claim payment estimate
+---------------------------------------------------------------------------------
+-- Change date			|Author						|	Change Description
+---------------------------------------------------------------------------------
+-- 06/0706/2024 		Yunus Mohammed				1. Created this view 
+-- ==============================================================================
+
+CREATE VIEW [edw_core].[vclaimpaymentestimate] 
+AS 
+select * from edw_stage.dms_claim_payment_estimate
+GO
