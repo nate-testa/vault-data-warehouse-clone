34e6236498a49b71382b632dfe9183e1399579e9
Architha Gudimalla <architha.gudimalla@vault.insurance>
2024-06-14 10:09:35 -0400

    Changed_to_user_sk

--- SQL/DDL/303_Alter_Table_ttask.sql
+++ SQL/DDL/303_Alter_Table_ttask.sql
@@ -3,21 +3,21 @@ IF NOT EXISTS (
     FROM    INFORMATION_SCHEMA.COLUMNS
     WHERE   TABLE_SCHEMA='edw_core'
     AND     TABLE_NAME = 'ttask'
-    AND     COLUMN_NAME = 'created_by_sk'
-) BEGIN ALTER TABLE edw_core.ttask ADD created_by_sk int null END; 
+    AND     COLUMN_NAME = 'created_by_user_sk'
+) BEGIN ALTER TABLE edw_core.ttask ADD created_by_user_sk int null END; 
 
 IF NOT EXISTS (
     SELECT  1
     FROM    INFORMATION_SCHEMA.COLUMNS
     WHERE   TABLE_SCHEMA='edw_core'
     AND     TABLE_NAME = 'ttask'
-    AND     COLUMN_NAME = 'assigned_to_sk'
-) BEGIN ALTER TABLE edw_core.ttask ADD assigned_to_sk int null END; 
+    AND     COLUMN_NAME = 'assigned_to_user_sk'
+) BEGIN ALTER TABLE edw_core.ttask ADD assigned_to_user_sk int null END; 
 
 IF NOT EXISTS (
     SELECT  1
     FROM    INFORMATION_SCHEMA.COLUMNS
     WHERE   TABLE_SCHEMA='edw_core'
     AND     TABLE_NAME = 'ttask'
-    AND     COLUMN_NAME = 'completed_by_sk'
-) BEGIN ALTER TABLE edw_core.ttask ADD completed_by_sk int null END; 
+    AND     COLUMN_NAME = 'completed_by_user_sk'
+) BEGIN ALTER TABLE edw_core.ttask ADD completed_by_user_sk int null END; 
