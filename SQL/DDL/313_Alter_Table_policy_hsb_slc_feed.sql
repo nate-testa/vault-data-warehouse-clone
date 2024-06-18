14c03c5d306529911f7d06f2208eda42a94860c1
Alberto Almario <alberto.almario@vault.insurance>
2024-06-19 06:14:14 +1000

    hsb for blanks values.

---
+++ SQL/DDL/313_Alter_Table_policy_hsb_slc_feed.sql
@@ -0,0 +1,11 @@
+ALTER TABLE [edw_integration].[policy_hsb_slc_feed] 
+    ALTER COLUMN [no_of_units_in_dwelling] varchar(255)
+;
+
+ALTER TABLE [edw_integration].[policy_hsb_slc_feed]
+    ALTER COLUMN [distance_to_hydrant] varchar(255)
+;
+
+ALTER TABLE [edw_integration].[policy_hsb_slc_feed] 
+    ALTER COLUMN [insurance_score] varchar(255)
+;
