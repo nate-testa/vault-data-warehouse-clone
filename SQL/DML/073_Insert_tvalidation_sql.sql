d251aa99cbcda61bab638ab6eacf765995ac3934
yunusmohammedvault <yunus.mohammed@vault.insurance>
2024-07-02 19:53:44 +0530

    Updated validation sql

--- SQL/DML/073_Insert_tvalidation_sql.sql
+++ SQL/DML/073_Insert_tvalidation_sql.sql
@@ -7,7 +7,9 @@ insert into edw_core.tvalidation_sql
 		, create_ts
 		, update_ts)
 select	 'tpolicy_history - transactions after policy expiry' as validation_sql_desc
-		,'select count(*) from edw_core.tpolicy_history where cast(transaction_ts as Date) > expiration_dt and expiration_dt > getdate() ' as source_sql
+		,'select count(*) from edw_core.tpolicy_history
+		where datediff("d",expiration_dt,cast(transaction_ts as date)) > 0
+		and datediff("d",cast(transaction_ts as date),getdate()) <= 30 ' as source_sql
 		,'select 0' as target_sql
 		,'Y' as active_in
 		,'Daily' as frequency_desc
