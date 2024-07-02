d5465a83c909f8a610c4a5ad5ade07cb6919205b
Architha Gudimalla <architha.gudimalla@vault.insurance>
2024-07-02 00:24:58 -0400

    Added foreign key

--- SQL/DDL/320_Alter_Table_tinternal_coverage_summary.sql
+++ SQL/DDL/320_Alter_Table_tinternal_coverage_summary.sql
@@ -1,1 +1,11 @@
-ALTER TABLE edw_core.tinternal_coverage_summary ADD policy_history_sk int null;
+IF NOT EXISTS (
+    SELECT  1
+    FROM    INFORMATION_SCHEMA.COLUMNS
+    WHERE   TABLE_SCHEMA='edw_core'
+    AND     TABLE_NAME = 'tinternal_coverage_summary'
+    AND     COLUMN_NAME = 'policy_history_sk'
+) BEGIN ALTER TABLE edw_core.tinternal_coverage_summary ADD policy_history_sk int null END; 
+
+ALTER TABLE [edw_core].[tinternal_coverage_summary]  
+ADD  CONSTRAINT [fk_tinternal_coverage_summary_policy_history_sk] FOREIGN KEY([policy_history_sk])
+REFERENCES [edw_core].[tpolicy_history] ([policy_history_sk]) 
--- SQL/DDL/322_Alter_Table_tpolicy_transaction.sql
+++ SQL/DDL/322_Alter_Table_tpolicy_transaction.sql
@@ -1,1 +1,11 @@
-ALTER TABLE edw_core.tpolicy_transaction ADD policy_history_sk int null;
+IF NOT EXISTS (
+    SELECT  1
+    FROM    INFORMATION_SCHEMA.COLUMNS
+    WHERE   TABLE_SCHEMA='edw_core'
+    AND     TABLE_NAME = 'tpolicy_transaction'
+    AND     COLUMN_NAME = 'policy_history_sk'
+) BEGIN ALTER TABLE edw_core.tpolicy_transaction ADD policy_history_sk int null END; 
+
+ALTER TABLE [edw_core].[tpolicy_transaction]  
+ADD  CONSTRAINT [fk_tpolicy_transaction_policy_history_sk] FOREIGN KEY([policy_history_sk])
+REFERENCES [edw_core].[tpolicy_history] ([policy_history_sk]) 
