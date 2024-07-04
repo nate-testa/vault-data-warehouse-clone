bc8fd57f5f18b4b5c0298b2c15165461e55a0a4e
Sandeep Gundreddy <sandeep.gundreddy@vault.insurance>
2024-07-04 09:22:43 -0400

    auto driver table changes

---
+++ SQL/DDL/326_Alter_Table_tauto_driver.sql
@@ -0,0 +1,7 @@
+ALTER TABLE edw_core.tauto_driver ADD aaf_factor float;
+ALTER TABLE edw_core.tauto_driver ADD afb_factor float;
+ALTER TABLE edw_core.tauto_driver ADD naf_factor float;
+ALTER TABLE edw_core.tauto_driver ADD cpa_factor float;
+ALTER TABLE edw_core.tauto_driver ADD min_factor float;
+ALTER TABLE edw_core.tauto_driver ADD maj_factor float;
+ALTER TABLE edw_core.tauto_driver ADD sdp_factor float;
