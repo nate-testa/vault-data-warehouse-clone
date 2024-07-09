c775b9e71cca1b4dced9398252cc998a6c994fb0
Architha Gudimalla <architha.gudimalla@vault.insurance>
2024-07-09 17:59:56 -0400

    Dropping_unique_contraint

--- SQL/DDL/338_Alter_Table_tquote_auto_vehicle.sql
+++ SQL/DDL/338_Alter_Table_tquote_auto_vehicle.sql
@@ -5,3 +5,6 @@ IF NOT EXISTS (
     AND     TABLE_NAME = 'tquote_auto_vehicle'
     AND     COLUMN_NAME = 'vehicle_unique_id'
 ) BEGIN ALTER TABLE edw_core.tquote_auto_vehicle ADD vehicle_unique_id varchar(255) end;
+
+ALTER TABLE edw_core.tquote_auto_vehicle  
+DROP CONSTRAINT uidx_tquote_auto_vehicle_qtno_vehicleno;
--- SQL/DDL/339_Alter_Table_tquote_auto_vehicle_coverage.sql
+++ SQL/DDL/339_Alter_Table_tquote_auto_vehicle_coverage.sql
@@ -5,3 +5,6 @@ IF NOT EXISTS (
     AND     TABLE_NAME = 'tquote_auto_vehicle_coverage'
     AND     COLUMN_NAME = 'vehicle_unique_id'
 ) BEGIN ALTER TABLE edw_core.tquote_auto_vehicle_coverage ADD vehicle_unique_id varchar(255) end;
+
+ALTER TABLE edw_core.tquote_auto_vehicle_coverage  
+DROP CONSTRAINT uidx_tquote_auto_vehicle_coverage_qtno_effdt_vehno_transeq;
