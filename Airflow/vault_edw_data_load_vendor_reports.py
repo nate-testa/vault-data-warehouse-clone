3795f5e6d972922213b8696110795134bc3b7bf5
Hernando Gonzalez Garcia <Hernando.Gonzalez.Garcia@Vault.Insurance>
2024-07-16 08:50:14 -0500

    Created sp_tvendor_report_clue_auto

--- Airflow/vault_edw_data_load_vendor_reports.py
+++ Airflow/vault_edw_data_load_vendor_reports.py
@@ -171,6 +171,14 @@ with DAG(
             autocommit=True,
         )
 
+        sp_tvendor_report_clue_auto = MsSqlOperator(
+            task_id='sp_tvendor_report_clue_auto',
+            mssql_conn_id='Vault_EDW',
+            sql="EXEC edw_core.sp_tvendor_report_clue_auto",
+            database="vault_edw",
+            autocommit=True,
+        )
+
         sp_tvendor_report_capeanalytics = MsSqlOperator(
             task_id='sp_tvendor_report_capeanalytics',
             mssql_conn_id='Vault_EDW',
@@ -194,7 +202,7 @@ with DAG(
             html_content=get_sp_success_data_HTML(vendor_report_group_items, 'All executions of stored procedure vendor report executed successfully'),
         )
 
-        sp_tvendor_report_stage_data >> sp_tvendor_report_AonCatStore >> sp_tvendor_report_CarfaxMileage >> sp_tvendor_report_CarfaxValue >> sp_tvendor_report_GuyCarpenter >> sp_tvendor_report_IsoVehicle >> sp_tvendor_report_IsoProperty >> sp_tvendor_report_LC360 >> sp_tvendor_report_MVR >> sp_tvendor_report_NHTSA >> 1sp_tvendor_report_TransUnion >> sp_tvendor_report_capeanalytics >> sp_tvendor_report_hazardhub >> sp_tvendor_report_clue_property >> sp_refresh_views >> send_vendor_report_email
+        sp_tvendor_report_stage_data >> sp_tvendor_report_AonCatStore >> sp_tvendor_report_CarfaxMileage >> sp_tvendor_report_CarfaxValue >> sp_tvendor_report_GuyCarpenter >> sp_tvendor_report_IsoVehicle >> sp_tvendor_report_IsoProperty >> sp_tvendor_report_LC360 >> sp_tvendor_report_MVR >> sp_tvendor_report_NHTSA >> 1sp_tvendor_report_TransUnion >> sp_tvendor_report_capeanalytics >> sp_tvendor_report_hazardhub >> sp_tvendor_report_clue_property >> sp_tvendor_report_clue_auto >> sp_refresh_views >> send_vendor_report_email
 
 
     end = DummyOperator(
