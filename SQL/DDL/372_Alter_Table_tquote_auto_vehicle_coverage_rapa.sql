f3c7b2e86b47e2d9c3654e015958ced8614ae200
Architha Gudimalla <architha.gudimalla@vault.insurance>
2024-08-08 16:57:43 -0400

    Removed_new _uique_constraint

--- SQL/DDL/372_Alter_Table_tquote_auto_vehicle_coverage_rapa.sql
+++ SQL/DDL/372_Alter_Table_tquote_auto_vehicle_coverage_rapa.sql
@@ -1,6 +1,7 @@
 ALTER TABLE edw_core.tquote_auto_vehicle_coverage_rapa 
 DROP CONSTRAINT IF EXISTS uidx_tquote_auto_vehicle_coverage_rapa_qtno_effdt_vehno_transeq; 
 
+/*
 ALTER TABLE edw_core.tquote_auto_vehicle_coverage_rapa  
 ADD CONSTRAINT uidx_tquote_auto_vehicle_coverage_rapa_qtno_effdt_unqid_transeq unique (
 	[quote_no] ,
@@ -8,3 +9,4 @@ ADD CONSTRAINT uidx_tquote_auto_vehicle_coverage_rapa_qtno_effdt_unqid_transeq u
 	[vehicle_unique_id] ,
 	[transaction_seq_no] 
 )
+*/
