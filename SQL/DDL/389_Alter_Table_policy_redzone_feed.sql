3ff7c8893673d99d494198e7ab56b8f083e94cae
Architha Gudimalla <architha.gudimalla@vault.insurance>
2024-08-29 09:56:42 -0400

    Added_not_exists_clause

--- SQL/DDL/389_Alter_Table_policy_redzone_feed.sql
+++ SQL/DDL/389_Alter_Table_policy_redzone_feed.sql
@@ -1,12 +1,32 @@
-Alter TABLE edw_integration.policy_redzone_feed
-add  bdm_nm varchar(255);
+IF NOT EXISTS (
+    SELECT  1
+    FROM    INFORMATION_SCHEMA.COLUMNS
+    WHERE   TABLE_SCHEMA='edw_integration'
+    AND     TABLE_NAME = 'policy_redzone_feed'
+    AND     COLUMN_NAME = 'bdm_nm'
+) BEGIN ALTER TABLE edw_integration.policy_redzone_feed ADD bdm_nm varchar(255) END; 
 
-Alter TABLE edw_integration.policy_redzone_feed
-add  new_business_underwriter_nm varchar(255); 
+IF NOT EXISTS (
+    SELECT  1
+    FROM    INFORMATION_SCHEMA.COLUMNS
+    WHERE   TABLE_SCHEMA='edw_integration'
+    AND     TABLE_NAME = 'policy_redzone_feed'
+    AND     COLUMN_NAME = 'new_business_underwriter_nm'
+) BEGIN ALTER TABLE edw_integration.policy_redzone_feed ADD new_business_underwriter_nm varchar(255) END; 
 
-Alter TABLE edw_integration.policy_redzone_feed
-add  renewal_underwriter_nm varchar(255); 
+IF NOT EXISTS (
+    SELECT  1
+    FROM    INFORMATION_SCHEMA.COLUMNS
+    WHERE   TABLE_SCHEMA='edw_integration'
+    AND     TABLE_NAME = 'policy_redzone_feed'
+    AND     COLUMN_NAME = 'renewal_underwriter_nm'
+) BEGIN ALTER TABLE edw_integration.policy_redzone_feed ADD renewal_underwriter_nm varchar(255) END; 
 
-Alter TABLE edw_integration.policy_redzone_feed
-add  effective_dt date;  
+IF NOT EXISTS (
+    SELECT  1
+    FROM    INFORMATION_SCHEMA.COLUMNS
+    WHERE   TABLE_SCHEMA='edw_integration'
+    AND     TABLE_NAME = 'policy_redzone_feed'
+    AND     COLUMN_NAME = 'effective_dt'
+) BEGIN ALTER TABLE edw_integration.policy_redzone_feed ADD effective_dt date END; 
  
