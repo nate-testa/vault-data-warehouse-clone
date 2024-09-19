d1e897bf8185318e8c4c6df1b09d09b7b00896a0
Rushin Shah <rushin.shah@vault.insurance>
2024-09-19 07:38:37 -0400

    Create stage table stage_iss_territory

--- SQL/DDL/415_Create_STAGE_iss_territory.sql
+++ SQL/DDL/415_Create_STAGE_iss_territory.sql
@@ -1,5 +1,5 @@
 CREATE TABLE [edw_stage].[stage_iss_territory](
-[state_abbreviation] [varchar](255),
+[state_cd] [varchar](255),
 [zip_cd] [varchar](255),
 [territory] [varchar](255),
 [create_ts] [datetime]
