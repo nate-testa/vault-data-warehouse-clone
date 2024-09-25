106e097b0c883432abc6a918c3ce2ff4d689505e
yunusmohammedvault <yunus.mohammed@vault.insurance>
2024-09-25 23:04:27 +0530

    Validation sql for HSB amt

--- SQL/DML/094_Insert_tvalidation_sql.sql
+++ SQL/DML/094_Insert_tvalidation_sql.sql
@@ -2,8 +2,8 @@ INSERT INTO edw_core.tvalidation_sql (validation_sql_desc , source_sql , target_
 SELECT
 	'Cyber Protection Indicator/Amount mismatch' AS validation_sql_desc ,
        'select count(*) from edw_core.thome_additional_coverage where
-(home_cyber_protection_coverage_in = ''Yes'' and (home_cyber_protection_coverage_limit_amt = 0 or home_cyber_protection_coverage_limit_amt is null)) OR
-((home_cyber_protection_coverage_in = ''No'' or home_cyber_protection_coverage_in is null) and home_cyber_protection_coverage_limit_amt > 0)' AS source_sql ,
+(home_cyber_protection_coverage_in = ''Yes'' and (home_cyber_protection_coverage_limit_amt = ''0'' or home_cyber_protection_coverage_limit_amt is null)) OR
+((home_cyber_protection_coverage_in = ''No'' or home_cyber_protection_coverage_in is null) and home_cyber_protection_coverage_limit_amt > ''0'')' AS source_sql ,
        'select 0' AS target_sql ,
        'Y' AS active_in ,
        'Daily' AS frequency_desc ,
@@ -14,8 +14,8 @@ SELECT
 INSERT INTO edw_core.tvalidation_sql (validation_sql_desc , source_sql , target_sql , active_in , frequency_desc , create_ts , update_ts)
 SELECT 'Home Systems Protection Indicator/Amount mismatch' AS validation_sql_desc ,
        'select count(*) from edw_core.thome_additional_coverage where
-(home_systems_protection_in = ''Yes'' and (home_systems_protection_limit_amt = 0 or home_systems_protection_limit_amt is null)) OR
-((home_systems_protection_in = ''No'' or home_systems_protection_in is null) and home_systems_protection_limit_amt > 0)' AS source_sql ,
+(home_systems_protection_in = ''Yes'' and (home_systems_protection_limit_amt = ''0'' or home_systems_protection_limit_amt is null)) OR
+((home_systems_protection_in = ''No'' or home_systems_protection_in is null) and home_systems_protection_limit_amt > ''0'')' AS source_sql ,
        'select 0' AS target_sql ,
        'Y' AS active_in ,
        'Daily' AS frequency_desc ,
