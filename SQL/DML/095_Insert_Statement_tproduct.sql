b898ff32fab32dfa50083e0f4523932af31f68b0
yunusmohammedvault <yunus.mohammed@vault.insurance>
2024-10-01 20:58:04 +0530

    Query updated

--- SQL/DML/095_Insert_Statement_tproduct.sql
+++ SQL/DML/095_Insert_Statement_tproduct.sql
@@ -1,2 +1,3 @@
-INSERT INTO edw_core.tproduct (product_cd,product_nm,ebao_product_cd,update_ts) VALUES
-	 (N'BY',N'Marine Boat & Yacht',N'NA',getdate())
+INSERT INTO edw_core.tproduct (product_cd,product_nm,ebao_product_cd,update_ts)
+SELECT 'BY' AS product_cd,'Marine Boat & Yacht' AS product_nm,'NA' as ebao_product_cd,getdate() as update_ts
+where not exists(select 1 from edw_core.tproduct where product_cd = 'BY')
