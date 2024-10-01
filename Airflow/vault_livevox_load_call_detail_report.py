8552c054a70dac43138f2b54946aecb04ec30463
Alberto Almario <alberto.almario@vault.insurance>
2024-10-02 07:21:57 +1000

    remove import

--- Airflow/vault_livevox_load_call_detail_report.py
+++ Airflow/vault_livevox_load_call_detail_report.py
@@ -12,7 +12,6 @@ from airflow.operators.email_operator import EmailOperator
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 from vault_edw_HTML_format import get_sp_error_data_HTML, get_HTML_on_vault_format
-from airflow.models import Variable
 
 to_email = "itdatateam@vault.insurance"
 # to_email = "alberto.valbuena@vault.insurance"
