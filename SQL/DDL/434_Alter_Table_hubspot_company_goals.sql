6f788f92b2556e085cb68a4cead1089e10ab0bfe
Architha Gudimalla <architha.gudimalla@vault.insurance>
2024-10-11 13:33:21 -0400

    added_if_exists

--- SQL/DDL/434_Alter_Table_hubspot_company_goals.sql
+++ SQL/DDL/434_Alter_Table_hubspot_company_goals.sql
@@ -1,4 +1,35 @@
-EXEC sp_rename 'edw_stage.hubspot_company_goals.target_2024_policy_inforce_renewal_rentention_pct', 'target_2024_policy_inforce_renewal_retention__', 'COLUMN';
-EXEC sp_rename 'edw_stage.hubspot_company_goals.target_monthly_nb_quote_commitment_$', 'target_monthly_nb_quote_commitment__', 'COLUMN';
-EXEC sp_rename 'edw_stage.hubspot_company_goals.Pct_target_growth_2024_inforce_premium_over_last_year', 'target_growth_2024_inforce_premium_over_last_year', 'COLUMN';
-EXEC sp_rename 'edw_stage.hubspot_company_goals.Pct_target_growth_2024_nb_premium_over_last_year', 'target_growth_2024_nb_premium_over_last_year', 'COLUMN';
+IF EXISTS (
+    SELECT  1
+    FROM    INFORMATION_SCHEMA.COLUMNS
+    WHERE   TABLE_SCHEMA='edw_stage'
+    AND     TABLE_NAME = 'hubspot_company_goals'
+    AND     COLUMN_NAME = 'target_2024_policy_inforce_renewal_rentention_pct'
+) BEGIN EXEC sp_rename 'edw_stage.hubspot_company_goals.target_2024_policy_inforce_renewal_rentention_pct', 'target_2024_policy_inforce_renewal_retention__', 'COLUMN';
+ END;  
+
+IF EXISTS (
+    SELECT  1
+    FROM    INFORMATION_SCHEMA.COLUMNS
+    WHERE   TABLE_SCHEMA='edw_stage'
+    AND     TABLE_NAME = 'hubspot_company_goals'
+    AND     COLUMN_NAME = 'target_monthly_nb_quote_commitment_$'
+) BEGIN EXEC sp_rename 'edw_stage.hubspot_company_goals.target_monthly_nb_quote_commitment_$', 'target_monthly_nb_quote_commitment__', 'COLUMN';
+ END;  
+
+IF EXISTS (
+    SELECT  1
+    FROM    INFORMATION_SCHEMA.COLUMNS
+    WHERE   TABLE_SCHEMA='edw_stage'
+    AND     TABLE_NAME = 'hubspot_company_goals'
+    AND     COLUMN_NAME = 'Pct_target_growth_2024_inforce_premium_over_last_year'
+) BEGIN EXEC sp_rename 'edw_stage.hubspot_company_goals.Pct_target_growth_2024_inforce_premium_over_last_year', 'target_growth_2024_inforce_premium_over_last_year', 'COLUMN';
+ END;  
+ 
+IF EXISTS (
+    SELECT  1
+    FROM    INFORMATION_SCHEMA.COLUMNS
+    WHERE   TABLE_SCHEMA='edw_stage'
+    AND     TABLE_NAME = 'hubspot_company_goals'
+    AND     COLUMN_NAME = 'Pct_target_growth_2024_nb_premium_over_last_year'
+) BEGIN EXEC sp_rename 'edw_stage.hubspot_company_goals.Pct_target_growth_2024_nb_premium_over_last_year', 'target_growth_2024_nb_premium_over_last_year', 'COLUMN';
+ END;   
