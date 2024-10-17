0e1b73f5eb831355f39d9670660cb52dd44ec8b1
Architha Gudimalla <architha.gudimalla@vault.insurance>
2024-10-17 13:23:33 -0400

    Updated_IsReportFromCache

--- SQL/DDL/436_Alter_Table_STAGE_tvendor_report_field_data.sql
+++ SQL/DDL/436_Alter_Table_STAGE_tvendor_report_field_data.sql
@@ -1,2 +1,7 @@
-Alter TABLE [edw_stage].[tvendor_report_field_data]
-add IsReportFromCache bit
+IF NOT EXISTS (
+    SELECT  1
+    FROM    INFORMATION_SCHEMA.COLUMNS
+    WHERE   TABLE_SCHEMA='edw_stage'
+    AND     TABLE_NAME = 'tvendor_report_field_data'
+    AND     COLUMN_NAME = 'IsReportFromCache'
+) BEGIN ALTER TABLE edw_stage.tvendor_report_field_data ADD IsReportFromCache varchar(255) END;  
