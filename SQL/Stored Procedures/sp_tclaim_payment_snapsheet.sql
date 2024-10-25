5636c532ed3d8b438bfe34e9fb645fe1e0600c0d
Architha Gudimalla <architha.gudimalla@vault.insurance>
2024-10-25 12:37:16 -0400

    created_the_proc

---
+++ SQL/Stored Procedures/sp_tclaim_payment_snapsheet.sql
@@ -0,0 +1,138 @@
+﻿-- =================================================================================================
+-- Description: This procedures inserts and updates claim payment data
+-----------------------------------------------------------------------------------------------------------
+-- Change date |	Author					|	Change Description
+-----------------------------------------------------------------------------------------------------------
+-- 07/28/23		Architha Gudimalla			1. Created this procedure 
+-- ======================================================================================================== 
+CREATE OR ALTER PROCEDURE [edw_core].[sp_tclaim_payment_snapsheet]
+
+AS
+BEGIN
+	DECLARE @ProcedureName NVARCHAR(120)
+    SET @ProcedureName = OBJECT_NAME(@@PROCID)
+    -- SET NOCOUNT ON added to prevent extra result sets from
+    -- interfering with SELECT statements.
+    SET NOCOUNT ON
+
+	BEGIN TRY
+		DECLARE @last_source_extract_ts DATETIME2(7)
+		DECLARE @etl_audit_sk INT
+		DECLARE @new_last_source_extract_ts DATETIME2(7)
+		DECLARE @rows_affected INT
+		DECLARE @process_nm VARCHAR(255)=@ProcedureName
+		DECLARE @current_date DATETIME=GETDATE()
+		DECLARE @parameter_desc VARCHAR(255)
+
+		-- Get last source extract date
+		SELECT @last_source_extract_ts = edw_core.fn_get_last_source_extract_ts(@process_nm);
+		EXEC edw_core.sp_ins_tetl_audit @process_nm,@current_date,@etl_audit_sk=@etl_audit_sk OUTPUT;
+
+		DROP TABLE IF exists edw_temp.tclaim_payment_temp1
+
+		SELECT	c.claim_number,
+				tc.claim_sk,
+				tf.claim_feature_sk,
+				concat(fpi.financial_transaction_id , "-" , fpi.exposure_id) AS payment_sequence_no,
+				settle_payee.stage AS payment_status,
+				null AS payment_no,
+				fpi.cost_type AS claim_type_cd,
+				null as settle_payee_id,
+				null as payee_id,
+				concat(cp.first_name,' ',cp.last_name) AS payee_nm,
+				fpd.party_type AS party_role_nm, 
+				ISNULL(fpi.amount,0) AS paid_amt,
+				concat( fpi.address_address_1,', ',
+						fpi.address_address_2,', ', 
+						fpi.address_city,', ', 
+						fpi.address_region,', ', 
+						fpi.address_postal_code,', ', 
+						fpi.address_country
+				) AS payee_address,
+				fpd.note_body AS remark, 
+				u.name AS payment_submitter_nm,
+				null as payment_approver_nm, --ISNULL(tpu1.REAL_NAME,tpu2.REAL_NAME) AS payment_approver_nm, --pending
+				ft.created_at AS payment_submitted_dt,
+				ft.approved_at AS payment_approver_dt,
+				ft.financial_transaction_type as payment_category_nm,--(CASE WHEN settle.claim_type = 'LOS' THEN 'Payment' ELSE 'Recovery' END) AS payment_category_nm,
+				fpi.payment_type as partial_final_payment_desc,--(CASE WHEN fpi.pay_final = 4 THEN 'Final' ELSE 'Partial' END) AS partial_final_payment_desc,
+				null as expert_subtype_role, --party.expert_subtype_role, --pending
+				5 AS source_system_sk
+
+		INTO 	edw_temp.tclaim_payment_temp1 
+
+		FROM 		edw_stage_snapsheet.claims c
+		inner join 	edw_stage_snapsheet.financial_payment_items AS fpi on fpi.claim_id = c.id
+		left join 	edw_stage_snapsheet.claim_parties cp on a.party_id = cp.id 
+		left join 	edw_stage_snapsheet.financial_payment_details fpd on fpd.id = financial_payment_items.financial_transaction_id
+		left join 	edw_stage_snapsheet.financial_transactions ft on fpi.financial_transaction_id = ft.id
+		left join 	edw_stage.users u on ft.creator_user_id = u.id 
+		INNER JOIN 	edw_core.tclaim tc ON tc.claim_no=c.claim_number
+		INNER JOIN 	edw_core.tclaim_feature tf ON tf.claim_no=tc.claim_no AND tf.subclaim_seq_no = obj.seq_no -- need to check join
+		WHERE		greatest(fpi.created_at,fpi.updated_at) > @last_source_extract_ts;   
+
+		MERGE edw_core.tclaim_payment  AS Target
+		USING edw_temp.tclaim_payment_temp1 AS Source
+		ON  Source.claim_feature_sk=Target.claim_feature_sk AND 
+			Source.payment_no=Target.payment_no AND 
+			Source.payment_sequence_no=Target.payment_sequence_no
+		-- For Inserts
+		WHEN NOT MATCHED BY Target THEN
+		INSERT (
+					claim_no,claim_sk,claim_feature_sk,payment_sequence_no,payment_no,payment_status,
+					claim_type_cd,settle_payee_id,payee_id,payee_nm,party_role_nm,paid_amt,payee_address,
+					remark,payment_submitter_nm,payment_approver_nm,payment_submitted_dt,payment_approver_dt,
+					payment_category_nm,partial_final_payment_desc,party_subtype_role_nm,source_system_sk,create_ts,update_ts,etl_audit_sk
+			)
+		VALUES
+			(
+					claim_no,claim_sk,claim_feature_sk,payment_sequence_no,payment_no,payment_status,
+					claim_type_cd,settle_payee_id,payee_id,payee_nm,party_role_nm,paid_amt,payee_address,
+					remark,payment_submitter_nm,payment_approver_nm,payment_submitted_dt,payment_approver_dt,
+					payment_category_nm,partial_final_payment_desc,expert_subtype_role,3,@current_date,@current_date,@etl_audit_sk
+			)
+		-- For Updates
+		WHEN MATCHED THEN UPDATE 
+		SET
+			Target.payment_status=Source.payment_status,
+			Target.claim_type_cd=Source.claim_type_cd,
+			Target.settle_payee_id=Source.settle_payee_id,
+			Target.payee_id=Source.payee_id,
+			Target.payee_nm=Source.payee_nm,
+			Target.party_role_nm=Source.party_role_nm,
+			Target.paid_amt=Source.paid_amt,
+			Target.payee_address=Source.payee_address,
+			Target.remark=Source.remark,
+			Target.payment_submitter_nm=Source.payment_submitter_nm,
+			Target.payment_approver_nm=Source.payment_approver_nm,
+			Target.payment_submitted_dt=Source.payment_submitted_dt,
+			Target.payment_approver_dt=Source.payment_approver_dt,
+			Target.payment_category_nm=Source.payment_category_nm,
+			Target.partial_final_payment_desc=Source.partial_final_payment_desc,
+			Target.party_subtype_role_nm=Source.expert_subtype_role,
+			Target.update_ts=@current_date;
+
+		SET @rows_affected=@@ROWCOUNT;
+
+		-- Update control table
+		SET @new_last_source_extract_ts=COALESCE((SELECT MAX(payment_approver_dt) FROM edw_temp.tclaim_payment_temp1),@last_source_extract_ts)
+		EXEC edw_core.sp_upd_tetl_control @process_nm,@new_last_source_extract_ts;
+
+		-- Update audit table
+		SET @parameter_desc= @parameter_desc + ' AND last_source_extract_ts <=' + CAST(@new_last_source_extract_ts AS VARCHAR(200))
+		EXEC edw_core.sp_upd_tetl_audit @etl_audit_sk,@rows_affected,@parameter_desc;
+
+		
+		-- Drop temp table
+		DROP TABLE IF EXISTS edw_temp.tclaim_payment_temp1
+	END TRY
+	BEGIN CATCH
+		DECLARE @error_message nvarchar(4000)
+		SET @error_message = 'Error Number:' + CAST(ERROR_NUMBER() AS NVARCHAR(100)) + ' Error State:' + CAST(ERROR_STATE() AS NVARCHAR(100))
+							+ ' Error Severity:' + CAST(ERROR_SEVERITY() AS NVARCHAR(100)) +
+							CHAR(13) + 'Error Procedure:' + ERROR_PROCEDURE() + ' Error Line:' +CAST(ERROR_LINE() AS NVARCHAR(100)) +
+							CHAR(13) + 'Error Message:' + ERROR_MESSAGE()
+		EXEC edw_core.sp_upd_error_tetl_audit @etl_audit_sk,@error_message;
+		THROW 99001,'Error occured: see tetl_audit table for more info', 1;
+	END CATCH
+END
