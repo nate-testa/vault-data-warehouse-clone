ac96d69354ca21742401e0577965658c375ed647
Tuba Mohsin <tuba.mohsin@vault.insurance>
2024-11-06 10:22:10 -0500

    Task-7618

---
+++ SQL/DDL/456_Create_Table_STAGE_tico_place_code.sql
@@ -0,0 +1,11 @@
+create table edw_stage.tico_place_code (
+community VARCHAR(255),
+place_code VARCHAR(255),
+county VARCHAR(255),
+tdi_no VARCHAR(255), 
+zone VARCHAR(255), 
+terr VARCHAR(255), 
+zip_code VARCHAR(255) ,
+create_ts datetime not null,
+) ;
+
