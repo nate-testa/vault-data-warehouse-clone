81ffd9d1fb7d79a9258731a453d2205695b067c5
Architha Gudimalla <architha.gudimalla@vault.insurance>
2024-11-07 14:30:27 -0500

    updated_retention_logic

---
+++ SQL/DDL/457_Alter_Table_tbroker_summary.sql
@@ -0,0 +1,10 @@
+ALTER TABLE edw_core.tbroker_summary
+ADD 
+	ytd_policy_expiring_ct int,
+	ytd_policy_renewal_ct int,
+	ytd_policy_renewal_offered_ct int,
+	ytd_policy_renewal_offered_over_50k_ct int,
+	ytd_policy_renewal_offered_premiumm_amt decimal(10,4),
+	ytd_policy_renewal_offered_expiring_premium_amt decimal(10,4),
+	ytd_policy_expiring_premium_amt decimal(10,4),
+	ytd_policy_renewal_premium_amt decimarl(10,4);
