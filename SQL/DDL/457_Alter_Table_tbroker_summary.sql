4850093cc87d5e79e4c7b691a6b227dd900b1b54
Architha Gudimalla <architha.gudimalla@vault.insurance>
2024-11-07 16:43:21 -0500

    updated_the_data_type

--- SQL/DDL/457_Alter_Table_tbroker_summary.sql
+++ SQL/DDL/457_Alter_Table_tbroker_summary.sql
@@ -7,4 +7,4 @@ ADD
 	ytd_policy_renewal_offered_premiumm_amt decimal(10,4),
 	ytd_policy_renewal_offered_expiring_premium_amt decimal(10,4),
 	ytd_policy_expiring_premium_amt decimal(10,4),
-	ytd_policy_renewal_premium_amt decimarl(10,4);
+	ytd_policy_renewal_premium_amt decimal(10,4);
