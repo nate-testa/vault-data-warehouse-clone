e4573480832cd8bef916aa5c7f603ae8464f0b4b
Architha Gudimalla <architha.gudimalla@vault.insurance>
2024-11-14 12:13:52 -0500

    Add_lifetime_claim_ct_and_amt

---
+++ SQL/DDL/468_Alter_Table_tpolicy.sql
@@ -0,0 +1,15 @@
+IF NOT EXISTS (
+    SELECT  1
+    FROM    INFORMATION_SCHEMA.COLUMNS
+    WHERE   TABLE_SCHEMA='edw_core'
+    AND     TABLE_NAME = 'tpolicy'
+    AND     COLUMN_NAME = 'lifetime_claim_ct'
+) BEGIN ALTER TABLE edw_core.tpolicy ADD lifetime_claim_ct int END; 
+
+IF NOT EXISTS (
+    SELECT  1
+    FROM    INFORMATION_SCHEMA.COLUMNS
+    WHERE   TABLE_SCHEMA='edw_core'
+    AND     TABLE_NAME = 'tpolicy'
+    AND     COLUMN_NAME = 'lifetime_incurred_amt'
+) BEGIN ALTER TABLE edw_core.tpolicy ADD lifetime_incurred_amt decimal(15,2) END; 
---
+++ SQL/Stored Procedures/sp_tquote_update_lifetime_claims.sql
@@ -0,0 +1,70 @@
+-- ==========================================================================================================================================
+-- Description: This procedures updates tquote lifetime_claims
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- Change date |Author						|	Change Description
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- 11/14/23		Architha Gudimalla		    1. VI34680|AD7653 - Created this procedure 
+-- ========================================================================================================================================== 
+
+CREATE OR ALTER PROCEDURE [edw_core].[sp_tquote_update_lifetime_claims]
+
+AS 
+BEGIN
+    -- SET NOCOUNT ON added to prevent extra result sets from
+    -- interfering with SELECT statements.
+    SET NOCOUNT ON
+
+	BEGIN TRY
+		DECLARE @last_source_extract_ts DATETIME2(7)
+		DECLARE @etl_audit_sk INT
+		DECLARE @new_last_source_extract_ts DATETIME2(7)
+		DECLARE @rows_affected INT
+		DECLARE @process_nm VARCHAR(255)=OBJECT_NAME(@@PROCID)
+		DECLARE @CU DATETIME=GETDATE()
+		
+        -- Get last source extract date
+		SELECT @last_source_extract_ts = edw_core.fn_get_last_source_extract_ts(@process_nm);
+		EXEC edw_core.sp_ins_tetl_audit @process_nm,@CU,@etl_audit_sk=@etl_audit_sk OUTPUT;
+	
+		DECLARE @parameter_desc VARCHAR(255)
+		SET @parameter_desc= 'last_source_extract_ts >' + CAST(@last_source_extract_ts AS VARCHAR(200))   
+
+
+		update q
+		set lifetime_claim_ct = isnull(pol.lifetime_claim_ct,0),
+			lifetime_incurred_amt = isnull(pol.lifetime_incurred_amt,0)
+		from tquote q
+		left join edw_core.tpolicy pol on pol.policy_sk = q.policy_sk ; 
+
+		
+         
+        -- Update control table
+		SET @new_last_source_extract_ts = '2017-01-01'
+		EXEC edw_core.sp_upd_tetl_control @process_nm,@new_last_source_extract_ts; 
+
+		
+		SET @rows_affected=@@ROWCOUNT;
+
+        -- Update control table
+		SET @new_last_source_extract_ts = '2017-01-01'
+		EXEC edw_core.sp_upd_tetl_control @process_nm,@new_last_source_extract_ts; 
+	
+		-- Update audit table
+		SET @parameter_desc= @parameter_desc + ' AND last_source_extract_ts <=' + CAST(@new_last_source_extract_ts AS VARCHAR(200))
+		EXEC edw_core.sp_upd_tetl_audit @etl_audit_sk,@rows_affected,@parameter_desc; 
+
+	END TRY
+	BEGIN CATCH
+		DECLARE @error_message nvarchar(4000)
+		SET @error_message = 'Error Number:' + ISNULL(CAST(ERROR_NUMBER() AS NVARCHAR(100)),'') + 
+						     ' Error State:' + ISNULL(CAST(ERROR_STATE() AS NVARCHAR(100)),'')  + 
+						  ' Error Severity:' + ISNULL(CAST(ERROR_SEVERITY() AS NVARCHAR(100)),'') + CHAR(13) + 
+					      'Error Procedure:' + ISNULL(ERROR_PROCEDURE(),'') + 
+						      ' Error Line:' + ISNULL(CAST(ERROR_LINE() AS NVARCHAR(100)),'') + CHAR(13) + 
+						    'Error Message:' + ISNULL(ERROR_MESSAGE(),'')
+	
+		EXEC edw_core.sp_upd_error_tetl_audit @etl_audit_sk,@error_message;
+		THROW 99001,'Error occured: see tetl_audit table for more info', 1;
+	END CATCH
+END
+
