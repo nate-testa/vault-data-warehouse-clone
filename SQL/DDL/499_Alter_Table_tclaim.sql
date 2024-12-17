5d31362f777429c95e5ac120adad33d9331fc1fe
Rushin Shah <rushin.shah@vault.insurance>
2024-12-17 17:11:57 -0500

    Updates to tclaim column

--- SQL/DDL/499_Alter_Table_tclaim.sql
+++ SQL/DDL/499_Alter_Table_tclaim.sql
@@ -1,2 +1,4 @@
-ALTER TABLE edw_core.tclaim ADD source_of_loss varchar(255);
+
+ALTER TABLE edw_core.tclaim ADD source_of_fire varchar(255);
+ALTER TABLE edw_core.tclaim ADD source_of_water varchar(255);
 ALTER TABLE edw_core.tclaim ADD first_party_driver_nm varchar(255);
