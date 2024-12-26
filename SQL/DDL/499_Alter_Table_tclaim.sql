cff13b7d9bc2cf0d955ea24bbbcee064fe816e0e
Rushin Shah <rushin.shah@vault.insurance>
2024-12-26 12:59:23 -0500

    Addition of liability fields

--- SQL/DDL/499_Alter_Table_tclaim.sql
+++ SQL/DDL/499_Alter_Table_tclaim.sql
@@ -2,3 +2,7 @@
 ALTER TABLE edw_core.tclaim ADD source_of_fire varchar(255);
 ALTER TABLE edw_core.tclaim ADD source_of_water varchar(255);
 ALTER TABLE edw_core.tclaim ADD first_party_driver_nm varchar(255);
+
+ALTER TABLE edw_core.tclaim ADD fault_decision varchar(255);
+ALTER TABLE edw_core.tclaim ADD responsible_party varchar(255);
+ALTER TABLE edw_core.tclaim ADD at_fault_pct varchar(255);
--- SQL/DDL/505_Alter_Table_claim_renewal_rating_auto_pel_api.sql
+++ SQL/DDL/505_Alter_Table_claim_renewal_rating_auto_pel_api.sql
@@ -1,1 +1,5 @@
 ALTER TABLE edw_integration.claim_renewal_rating_auto_pel_api ADD FirstPartyDriverName varchar(255);
+
+ALTER TABLE edw_integration.claim_renewal_rating_auto_pel_api ADD FaultDecision varchar(255);
+ALTER TABLE edw_integration.claim_renewal_rating_auto_pel_api ADD ResponsibleParty varchar(255);
+ALTER TABLE edw_integration.claim_renewal_rating_auto_pel_api ADD AtFaultPercent varchar(255);
