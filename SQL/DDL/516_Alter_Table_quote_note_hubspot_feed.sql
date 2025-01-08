9ab014d53021824954ae70a2f5ff7e3e486aa448
Architha Gudimalla <architha.gudimalla@vault.insurance>
2025-01-08 11:10:52 -0500

    updated_to_note_user_nm

--- SQL/DDL/516_Alter_Table_quote_note_hubspot_feed.sql
+++ SQL/DDL/516_Alter_Table_quote_note_hubspot_feed.sql
@@ -1,2 +1,7 @@
-ALTER TABLE edw_integration.quote_note_hubspot_feed
-add  note_user_id varchar(255) null; 	
+IF NOT EXISTS (
+    SELECT  1
+    FROM    INFORMATION_SCHEMA.COLUMNS
+    WHERE   TABLE_SCHEMA='edw_integration'
+    AND     TABLE_NAME = 'quote_note_hubspot_feed'
+    AND     COLUMN_NAME = 'note_user_nm'
+) BEGIN ALTER TABLE edw_integration.quote_note_hubspot_feed ADD note_user_nm varchar(255) END; 
