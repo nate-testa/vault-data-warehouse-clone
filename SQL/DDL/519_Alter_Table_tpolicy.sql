39f9f2cf3a8bf5b4385087c328e5e46a9cd9b2db
Architha Gudimalla <Architha.Gudimalla@vault.insurance>
2025-01-10 21:38:14 +0000

    Merge pull request 8140 from Task-7636_tpolicy_add_uw_orig_eff_dt into Development

---
+++ SQL/DDL/519_Alter_Table_tpolicy.sql
@@ -0,0 +1,7 @@
+IF NOT EXISTS (
+    SELECT  1
+    FROM    INFORMATION_SCHEMA.COLUMNS
+    WHERE   TABLE_SCHEMA='edw_core'
+    AND     TABLE_NAME = 'tpolicy'
+    AND     COLUMN_NAME = 'uw_company_original_policy_effective_dt'
+) BEGIN ALTER TABLE edw_core.tpolicy ADD uw_company_original_policy_effective_dt date END; 
