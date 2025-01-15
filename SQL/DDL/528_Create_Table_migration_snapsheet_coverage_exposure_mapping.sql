a4cdbffe7d5b9144dfe1a0d33992a35d98e1766a
Yunus Mohammed <yunus.mohammed@vault.insurance>
2025-01-15 08:42:22 -0500

    snapsheet exposure and cov mapping

---
+++ SQL/DDL/528_Create_Table_migration_snapsheet_coverage_exposure_mapping.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [edw_temp].[migration_snapsheet_coverage_exposure_mapping]
+(
+product_cd VARChar(255),
+subclaim_type_name VARChar(255),
+coverage_name VARChar(255),
+snapsheet_exposure_type VARChar(255),
+snapsheet_coverage_cd VARChar(255),
+snapsheet_coverage_nm  VARChar(255)
+)
