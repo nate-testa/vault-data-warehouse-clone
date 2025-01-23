edcd361018382416c0f389301431adb0816cb6f8
Tuba Mohsin <tuba.mohsin@vault.insurance>
2025-01-23 12:20:55 -0500

    Updated PR with check to see if column exists

--- SQL/DDL/535_Alter_Table_tquote_collection_coverage.sql
+++ SQL/DDL/535_Alter_Table_tquote_collection_coverage.sql
@@ -1,1 +1,8 @@
-ALTER TABLE edw_core.tquote_collection_coverage ADD theft_or_loss_general_conditions_endorsement_in varchar(255);
+
+IF NOT EXISTS (
+    SELECT  1
+    FROM    INFORMATION_SCHEMA.COLUMNS
+    WHERE   TABLE_SCHEMA='edw_core'
+    AND     TABLE_NAME = 'tquote_collection_coverage'
+    AND     COLUMN_NAME = 'theft_or_loss_general_conditions_endorsement_in'
+) BEGIN ALTER TABLE edw_core.tquote_collection_coverage ADD theft_or_loss_general_conditions_endorsement_in varchar(255);
