e898a17961dab4f3e8e2036f9e406cbf0d085549
Hernando Gonzalez Garcia <Hernando.Gonzalez.Garcia@Vault.Insurance>
2025-01-24 10:29:40 -0500

    Updated PR with check to see if column exists

--- SQL/DDL/530_Alter_Table_thome_coverage.sql
+++ SQL/DDL/530_Alter_Table_thome_coverage.sql
@@ -5,4 +5,4 @@ IF NOT EXISTS (
     WHERE   TABLE_SCHEMA='edw_core'
     AND     TABLE_NAME = 'thome_coverage'
     AND     COLUMN_NAME = 'fenced_pool_in'
-) BEGIN ALTER TABLE edw_core.thome_coverage ADD fenced_pool_in varchar(255);
+) BEGIN ALTER TABLE edw_core.thome_coverage ADD fenced_pool_in varchar(255) END;
