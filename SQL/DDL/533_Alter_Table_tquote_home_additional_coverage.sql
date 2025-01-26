64665d02600747e248ce8c254867a6bf71ce4648
Sandeep Gundreddy <sandeep.gundreddy@vault.insurance>
2025-01-26 07:51:18 -0500

    fixed the script to add missing column

--- SQL/DDL/533_Alter_Table_tquote_home_additional_coverage.sql
+++ SQL/DDL/533_Alter_Table_tquote_home_additional_coverage.sql
@@ -13,5 +13,5 @@ IF NOT EXISTS (
     FROM    INFORMATION_SCHEMA.COLUMNS
     WHERE   TABLE_SCHEMA='edw_core'
     AND     TABLE_NAME = 'tquote_home_additional_coverage'
-    AND     COLUMN_NAME = 'theft_or_loss_general_conditions_endorsement_in'
-) BEGIN ALTER TABLE edw_core.tquote_home_additional_coverage ADD theft_or_loss_general_conditions_endorsement_in varchar(255) END;
+    AND     COLUMN_NAME = 'animal_related_liability_endorsement_in'
+) BEGIN ALTER TABLE edw_core.tquote_home_additional_coverage ADD animal_related_liability_endorsement_in varchar(255) END;
