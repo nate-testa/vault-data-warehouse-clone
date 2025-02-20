4a24ba68931060ae46e3393c005b33a21b6813d0
Yunus Mohammed <yunus.mohammed@vault.insurance>
2025-02-20 07:32:27 -0500

    Workday claim procs updates

--- SQL/Stored Procedures/sp_claim_workday_reserve_feed.sql
+++ SQL/Stored Procedures/sp_claim_workday_reserve_feed.sql
@@ -10,6 +10,7 @@
 -- 11/30/23		Yunus Mohammed				3. Updated insured name for NFP
 -- 09/18/24		Yunus Mohammed				4. Added Throw in catch block
 -- 11/26/24		Yunus Mohammed				5. Updated "Marine Boat & Yacht" to "Marine_Boat&Yacht"
+-- 02/19/25		Yunus Mohammed				7. Updated to use new columns after Snapsheet implementation
 -- ================================================================================================= 
 CREATE OR ALTER PROCEDURE [edw_core].[sp_claim_workday_reserve_feed]
 
@@ -85,35 +86,44 @@ BEGIN
 				ELSE tprd.product_nm END AS product,
 				tcf.claim_coverage_desc AS policycoveragetype,
 				CASE
-					WHEN tcr.loss_reserve_amt!=0 THEN 'Loss (Indemnity)'
-					WHEN tcr.expense_reserve_amt!=0 THEN 'Loss (Expense - A&O)'
-					WHEN tcr.adjusting_other_reserve_amt!=0 THEN 'ALAE'
-					WHEN tcr.subro_reserve_amt!=0 THEN 'Subrogation'
-					WHEN tcr.salvage_reserve_amt!=0 THEN 'Salvage'
-					WHEN tcr.salvage_expense_reserve_amt!=0 THEN 'Salvage Expenses' 
-					WHEN tcr.subro_expense_reserve_amt!=0 THEN 'Subrogation Expense'
+					WHEN
+						(tcr.loss_reserve_amt + tcr.deductible_recovery_reserve_amt+ tcr.overpayment_recovery_reserve_amt) != 0
+					THEN 'Loss (Indemnity)'
+					
+					WHEN (tcr.deductible_recovery_expense_reserve_amt + tcr.expense_reserve_amt + tcr.overpayment_recovery_expense_reserve_amt) !=0
+					THEN 'Loss (Expense - A&O)'
+
+					WHEN (tcr.deductible_recovery_defense_reserve_amt + tcr.defense_reserve_amt + tcr.overpayment_recovery_defense_reserve_amt) !=0
+					THEN 'Loss (Expense - DCC)'
+
+					WHEN (tcr.salvage_recovery_reserve_amt + tcr.salvage_recovery_defense_reserve_amt) !=0 THEN 'Salvage'
+
+					WHEN tcr.salvage_recovery_expense_reserve_amt !=0 THEN 'Salvage Expenses'
+
+					WHEN (tcr.subrogation_recovery_reserve_amt + tcr.subrogation_recovery_defense_reserve_amt) != 0 THEN 'Subrogation'
+
+					WHEN tcr.subrogation_recovery_expense_reserve_amt !=0 THEN 'Subrogation Expense'
 				END AS reserve_type,
 				(
-					tcr.loss_reserve_amt+tcr.expense_reserve_amt+tcr.adjusting_other_reserve_amt+
-					tcr.subro_reserve_amt+tcr.salvage_reserve_amt+tcr.salvage_expense_reserve_amt+tcr.subro_expense_reserve_amt
+					tcr.deductible_recovery_expense_reserve_amt + tcr.expense_reserve_amt +
+					tcr.overpayment_recovery_expense_reserve_amt + tcr.deductible_recovery_defense_reserve_amt +
+					tcr.defense_reserve_amt + tcr.overpayment_recovery_defense_reserve_amt +
+					tcr.loss_reserve_amt + tcr.deductible_recovery_reserve_amt + tcr.overpayment_recovery_reserve_amt +
+					tcr.salvage_recovery_reserve_amt + tcr.salvage_recovery_defense_reserve_amt + tcr.salvage_recovery_expense_reserve_amt +
+					tcr.subrogation_recovery_expense_reserve_amt + tcr.subrogation_recovery_reserve_amt +tcr.subrogation_recovery_defense_reserve_amt
 				) AS reserve_amount,
 				YEAR(tc.loss_dt) AS accident_year,
-				CASE WHEN tc.policy_no LIKE 'NFP%' THEN np.risk_state ELSE COALESCE(st.state_cd,tp.risk_state_cd) END AS risk_state,
-				CAST(tasl.aslob_cd AS INT) AS aslob,
+				CASE WHEN tc.policy_no LIKE 'NFP%' THEN np.risk_state ELSE COALESCE(st.state_cd,tp.risk_state_cd) END AS risk_state,				
 				tcr.claim_transaction_sk AS transaction_id,
 				@end_dt AS monthend,
-				CASE WHEN tc.policy_no like 'NFP%' THEN np.insured_nm ELSE tp.insured_nm END AS insured_nm,
-				tscl.sub_cause_of_loss_desc AS sub_cause_of_loss_code,
-				tscl.sub_cause_of_loss_desc AS sub_cause_of_loss_name,
+				CASE WHEN tc.policy_no like 'NFP%' THEN np.insured_nm ELSE tp.insured_nm END AS insured_nm,				
 				tc.claim_status AS claim_status,
 				tcf.claim_feature_status AS loss_status
 				FROM
 					edw_core.tclaim tc
 					LEFT JOIN edw_core.tcause_of_loss tcl ON tcl.cause_of_loss_sk=tc.cause_of_loss_sk
-					LEFT JOIN edw_core.tcatastrophe tcat ON tcat.catastrophe_sk=tc.catastrophe_sk
-					LEFT JOIN edw_core.tsub_cause_of_loss tscl ON tscl.sub_cause_of_loss_sk=tc.sub_cause_of_loss_sk
-					INNER JOIN edw_core.tclaim_feature tcf ON tc.claim_sk=tcf.claim_sk
-					LEFT JOIN edw_core.taslob tasl ON tasl.aslob_sk=tcf.aslob_sk
+					LEFT JOIN edw_core.tcatastrophe tcat ON tcat.catastrophe_sk=tc.catastrophe_sk					
+					INNER JOIN edw_core.tclaim_feature tcf ON tc.claim_sk=tcf.claim_sk					
 					INNER JOIN edw_core.tproduct tprd ON tprd.product_sk=tc.product_sk
 					INNER JOIN edw_core.tclaim_transaction tcr ON tcr.claim_feature_sk=tcf.claim_feature_sk
 					LEFT JOIN edw_core.tclaim_payment tpay ON tpay.claim_feature_sk=tcf.claim_feature_sk  AND tcr.claim_payment_sk=tpay.claim_payment_sk
@@ -137,13 +147,13 @@ BEGIN
 			(
 			company,claim_no,policy_no,transaction_date,policyeffectivedate,claimlossdate,claimreporteddate,[address],city,[state],
 			zip,causeofloss,catastrophecode,catastrophename,product,policycoveragetype,reserve_type,reserve_amount,accident_year,
-			risk_state,aslob,transaction_id,monthend,insuredname,sub_cause_of_loss_code,sub_cause_of_loss_name,claim_status,
+			risk_state,transaction_id,monthend,insuredname,claim_status,
 			loss_status,create_ts,update_ts,etl_audit_sk
 			)
 			SELECT
 				company,claim_no,policy_no,transaction_date,policyeffectivedate,claimlossdate,claimreporteddate,[address],city,[state],
 				zip,causeofloss,catastrophecode,catastrophename,product,policycoveragetype,reserve_type,reserve_amount,accident_year,
-				risk_state,aslob,transaction_id,monthend,insured_nm,sub_cause_of_loss_code,sub_cause_of_loss_name,claim_status,
+				risk_state,transaction_id,monthend,insured_nm,claim_status,
 				loss_status,GETDATE() AS create_ts,GETDATE() AS update_ts, @etl_audit_sk AS etl_audit_sk
 			FROM
 				claim_reserve_feed_temp
