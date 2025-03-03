778f06607de8af652205defdbbcef115f28c276a
Architha Gudimalla <architha.gudimalla@vault.insurance>
2025-03-03 03:54:30 -0500

    Added_new_quote_close_reasons

---
+++ SQL/DDL/549_Alter_Table_tquote.sql
@@ -0,0 +1,15 @@
+ IF NOT EXISTS (					
+SELECT 1					
+FROM INFORMATION_SCHEMA.COLUMNS					
+WHERE TABLE_SCHEMA='edw_core'					
+AND TABLE_NAME = 'tquote'					
+AND COLUMN_NAME = 'competitor_carrier_nm'					
+) BEGIN ALTER TABLE edw_core.tquote ADD competitor_carrier_nm varchar(255) END 					
+					
+IF NOT EXISTS (					
+SELECT 1					
+FROM INFORMATION_SCHEMA.COLUMNS					
+WHERE TABLE_SCHEMA='edw_core'					
+AND TABLE_NAME = 'tquote'					
+AND COLUMN_NAME = 'close_reason_other_desc'					
+) BEGIN ALTER TABLE edw_core.tquote ADD close_reason_other_desc nvarchar(3000) END	 
