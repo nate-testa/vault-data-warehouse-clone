493bb8e4890b4846b9f720795cdd328f94b1bf53
Yunus Mohammed <yunus.mohammed@vault.insurance>
2025-03-07 14:01:20 -0500

    Column name added

--- SQL/DML/129_Insert_taslob.sql
+++ SQL/DML/129_Insert_taslob.sql
@@ -2,4 +2,4 @@ insert into edw_core.taslob
 (aslob_cd,aslob_desc,product_cd,coverage_cd,update_ts)
 select '091' as aslob_cd,'Inland Marine' as aslob_desc,'Homeowners' as product_cd,'HO Collections - Blanket' as coverage_cd,getdate() as update_ts
 union 
-select '091' as aslob_cd,'Inland Marine' as aslob_desc,'Collections' as product_cd,'Collections - Blanket',getdate() as update_ts
+select '091' as aslob_cd,'Inland Marine' as aslob_desc,'Collections' as product_cd,'Collections - Blanket' as coverage_cd,getdate() as update_ts
