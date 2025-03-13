b28a72c7d3efa54eb99e414993af6ff2011d82ba
Hernando Gonzalez Garcia <Hernando.Gonzalez.Garcia@Vault.Insurance>
2025-03-13 09:24:17 -0500

    Update

--- SQL/DDL/561_Alter_Table_STAGE_AccountTransactionVersionPremiumRaterReference.sql
+++ SQL/DDL/561_Alter_Table_STAGE_AccountTransactionVersionPremiumRaterReference.sql
@@ -4,7 +4,7 @@ FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA='edw_stage'					
 AND TABLE_NAME = 'AccountTransactionVersionPremiumRaterReference'					
 AND COLUMN_NAME = 'ExternalVersionId'					
-) BEGIN ALTER TABLE edw_stage.AccountRaterRefeAccountTransactionVersionPremiumRaterReferencerence ADD ExternalVersionId nvarchar(100) END
+) BEGIN ALTER TABLE edw_stage.AccountTransactionVersionPremiumRaterReference ADD ExternalVersionId nvarchar(100) END
 
 IF NOT EXISTS (					
 SELECT 1					
