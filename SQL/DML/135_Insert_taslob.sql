6354205180333d8119ae372dcc749ddac82e716e
Yunus Mohammed <yunus.mohammed@vault.insurance>
2025-03-14 12:35:40 -0400

    PR comment resolved

--- SQL/DML/135_Insert_taslob.sql
+++ SQL/DML/135_Insert_taslob.sql
@@ -4,10 +4,8 @@ select '192' as aslob_cd,'Auto Other Liability' as aslob_desc,'Auto' as product_
 union
 select '192' as aslob_cd,'Auto Other Liability' as aslob_desc,'Auto' as product_cd,'Underinsured Motorist Liablity' as coverage_cd,getdate() as update_ts
 union
-select '040' as aslob_cd,'Homeowners' as aslob_desc,'Home/Condo' as product_cd,	'Screen Enclosure' as coverage_cd,getdate() as update_ts
+select '040' as aslob_cd,'Homeowners' as aslob_desc,'Homeowners' as product_cd,	'Screen Enclosure' as coverage_cd,getdate() as update_ts
 union
-select '040' as aslob_cd,'Homeowners' as aslob_desc,'Home/Condo' as product_cd,	'Water Damage limitation' as coverage_cd,getdate() as update_ts
+select '040' as aslob_cd,'Homeowners' as aslob_desc,'Homeowners' as product_cd,	'Water Damage limitation' as coverage_cd,getdate() as update_ts
 union
-select '171' as aslob_cd,'Other Liability' as aslob_desc,'NFP' as product_cd,	'Excess Liability' as coverage_cd,getdate() as update_ts
-union
-select '171' as aslob_cd,'Other Liability' as aslob_desc,'NFP' as product_cd,	'UM/UIM Motorist Liability' as coverage_cd,getdate() as update_ts
+select '171' as aslob_cd,'Other Liability' as aslob_desc,'Excess Liability' as product_cd,	'Excess Liability' as coverage_cd,getdate() as update_ts
