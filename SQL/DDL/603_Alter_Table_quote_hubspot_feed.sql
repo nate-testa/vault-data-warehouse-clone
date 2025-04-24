200ee0ce092029298e822be61203421d09d1920b
Architha Gudimalla <architha.gudimalla@vault.insurance>
2025-04-24 16:40:00 -0400

    Added_monoline

--- SQL/DDL/603_Alter_Table_quote_hubspot_feed.sql
+++ SQL/DDL/603_Alter_Table_quote_hubspot_feed.sql
@@ -4,6 +4,7 @@ FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA='edw_integration'					
 AND TABLE_NAME = 'quote_hubspot_feed'					
 AND COLUMN_NAME = 'monoline_in'					
-) BEGIN ALTER TABLE edw_integration.quote_hubspot_feed ADD monoline_in varchar(255) END  
+) 
+BEGIN ALTER TABLE edw_integration.quote_hubspot_feed ADD monoline_in varchar(255) END  
  
 
--- SQL/DDL/604_Alter_Table_customer_hubspot_feed.sql
+++ SQL/DDL/604_Alter_Table_customer_hubspot_feed.sql
@@ -4,6 +4,7 @@ FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA='edw_integration'					
 AND TABLE_NAME = 'customer_hubspot_feed'					
 AND COLUMN_NAME = 'monoline_in'					
-) BEGIN ALTER TABLE edw_integration.customer_hubspot_feed ADD monoline_in varchar(255) END  
+) 
+BEGIN ALTER TABLE edw_integration.customer_hubspot_feed ADD monoline_in varchar(255) END  
  
 
