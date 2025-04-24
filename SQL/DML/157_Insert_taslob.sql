4162d1458ea01482aac9475cff6dc7df185f9257
Rushin Shah <rushin.shah@vault.insurance>
2025-04-24 16:55:37 -0400

    Add new PEL coverage to taslob table

---
+++ SQL/DML/157_Insert_taslob.sql
@@ -0,0 +1,3 @@
+--Manually deployed
+--insert into edw_core.taslob (aslob_cd,aslob_desc,product_cd,coverage_cd,update_ts) 
+--select '171' as aslob_cd,'Other Liability' as aslob_desc,'Excess Liability' as product_cd,'Coverage' as coverage_cd,getdate() as update_ts
