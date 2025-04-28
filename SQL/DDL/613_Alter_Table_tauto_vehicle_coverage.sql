d746e97274f197e55a245b610f05c3159c5ee526
Tuba Mohsin <tuba.mohsin@vault.insurance>
2025-04-28 16:32:09 -0400

    PR for 9286 _metal_statefilings_05_03

---
+++ SQL/DDL/613_Alter_Table_tauto_vehicle_coverage.sql
@@ -0,0 +1,18 @@
+IF NOT EXISTS (					
+SELECT 1					
+FROM INFORMATION_SCHEMA.COLUMNS					
+WHERE TABLE_SCHEMA='edw_core'					
+AND TABLE_NAME = 'tauto_vehicle_coverage'					
+AND COLUMN_NAME = 'agreed_value_coverage_in'					
+) BEGIN ALTER TABLE edw_core.tauto_vehicle_coverage ADD agreed_value_coverage_in VARCHAR(255) END
+;
+
+
+IF NOT EXISTS (					
+SELECT 1					
+FROM INFORMATION_SCHEMA.COLUMNS					
+WHERE TABLE_SCHEMA='edw_core'					
+AND TABLE_NAME = 'tauto_vehicle_coverage'					
+AND COLUMN_NAME = 'flood_deductible_pc'					
+) BEGIN ALTER TABLE edw_core.tauto_vehicle_coverage ADD flood_deductible_pc VARCHAR(255) END
+;
