323dfc5af6fcf80832a339e707c5dabc4673fc07
Bhaskar Muthyala <bhaskar.muthyala@vault.insurance>
2025-05-08 11:31:50 -0400

    Added new columns for renewal rating

---
+++ SQL/DDL/623_Alter_Table_claim_renewal_rating_auto_pel_api.sql
@@ -0,0 +1,15 @@
+IF NOT EXISTS (                
+SELECT 1                    
+FROM INFORMATION_SCHEMA.COLUMNS                
+WHERE TABLE_SCHEMA='edw_integration'                
+AND TABLE_NAME = 'claim_renewal_rating_auto_pel_api '                  
+AND COLUMN_NAME = 'AdjusterName'                    
+) BEGIN ALTER TABLE edw_integration.claim_renewal_rating_auto_pel_api ADD AdjusterName VARCHAR(255) NULL END ; 
+
+IF NOT EXISTS (                
+SELECT 1                    
+FROM INFORMATION_SCHEMA.COLUMNS                
+WHERE TABLE_SCHEMA='edw_integration'                
+AND TABLE_NAME = 'claim_renewal_rating_auto_pel_api '                  
+AND COLUMN_NAME = 'FirstPartyDriverRelationshipToInsured'                    
+) BEGIN ALTER TABLE edw_integration.claim_renewal_rating_auto_pel_api ADD FirstPartyDriverRelationshipToInsured VARCHAR(255) NULL END ; 
