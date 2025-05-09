437745cc86727419070b6c7f19173151b523ea0d
Architha Gudimalla <architha.gudimalla@vault.insurance>
2025-05-08 20:27:32 -0400

    TAP_team_added

---
+++ SQL/Stored Procedures/sp_tbroker_servicing_team_member.sql
@@ -0,0 +1,101 @@
+﻿SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =================================================================================================
+-- Description: This procedures inserts data into tbroker_servicing_team_member
+---------------------------------------------------------------------------------------------------
+-- Change date |Author						|	Change Description
+---------------------------------------------------------------------------------------------------
+-- 05/08/05		Architha Gudimalla				1. Created the proc
+-- ================================================================================================= 
+
+CREATE OR ALTER PROCEDURE edw_core.sp_tbroker_servicing_team_member
+
+AS
+BEGIN
+    -- SET NOCOUNT ON added to prevent extra result sets from
+    -- interfering with SELECT statements.
+    SET NOCOUNT ON
+
+	BEGIN TRY
+		DECLARE @last_source_extract_ts DATETIME2(7)
+		DECLARE @etl_audit_sk INT
+		DECLARE @new_last_source_extract_ts DATETIME2(7)
+		DECLARE @rows_affected INT
+		DECLARE @process_nm VARCHAR(255)=OBJECT_NAME(@@PROCID)
+		DECLARE @current_date DATETIME=GETDATE()
+		DECLARE @parameter_desc VARCHAR(255)
+
+		-- Get last source extract date
+		SELECT @last_source_extract_ts = edw_core.fn_get_last_source_extract_ts(@process_nm);
+		EXEC edw_core.sp_ins_tetl_audit @process_nm,@current_date,@etl_audit_sk=@etl_audit_sk OUTPUT;
+		SET @parameter_desc= 'last_source_extract_ts >' + CAST(@last_source_extract_ts AS VARCHAR(200)) 
+
+		-- Create temp table with name as tbroker_license_temp
+		DROP TABLE IF EXISTS edw_temp.tbroker_servicing_team_member_temp
+
+		SELECT 
+				a.broker_servicing_team_sk,
+				NULLIF(TRIM(u.name),'') full_nm, 
+				NULLIF(TRIM(u.FirstName),'') first_nm,  
+				NULLIF(TRIM(u.LastName),'') last_nm  ,
+				NULLIF(TRIM(u.Email),'') Email,  
+				NULLIF(TRIM(u.MobilePhone),'') last_nm
+		INTO edw_temp.tbroker_servicing_team_member_temp
+		FROM edw_stage.BrokerageServicingTeamMember as bstm
+		inner join edw_stage.BrokerageServicingTeam as bst on bstm.BrokerageServicingTeamId=bst.id 
+		inner join edw_core.tbroker_servicing_team as a on bst.name=a.name 
+		left join edw_stage.User u on bstm.UserId=u.id 
+		WHERE
+			GREATEST(ctm.CreatedDate,ctm.UpdatedDate) > @last_source_extract_ts 
+
+		-- Delete from tbroker_license table
+		DELETE FROM edw_core.tbroker_servicing_team_member;
+		
+		-- Reset identity column
+		DBCC CHECKIDENT('edw_core.tbroker_servicing_team_member',RESEED,0);
+
+		INSERT INTO edw_core.tbroker_servicing_team_member
+		(			
+			broker_servicing_team_sk, 
+			full_nm, first_nm , last_nm, email, last_nm, 
+			create_ts, update_ts, etl_audit_sk 
+		)
+		SELECT
+			broker_servicing_team_sk,
+			full_nm, first_nm , last_nm, email, last_nm,
+			@current_date AS create_ts,@current_date AS update_ts,@etl_audit_sk,product_sk
+		FROM
+			edw_temp.tbroker_servicing_team_member_temp
+
+		
+		SET @rows_affected=@@ROWCOUNT;
+		
+		-- Update control table
+		SET @new_last_source_extract_ts = '2017-01-01'
+		
+		-- Update audit table
+		SET @parameter_desc= @parameter_desc + ' AND last_source_extract_ts <=' + CAST(@new_last_source_extract_ts AS VARCHAR(200))
+		EXEC edw_core.sp_upd_tetl_audit @etl_audit_sk,@rows_affected,@parameter_desc;
+
+		-- Drop temp table
+		DROP TABLE IF EXISTS edw_temp.tbroker_servicing_team_member_temp
+	END TRY
+	BEGIN CATCH
+		DECLARE @error_message nvarchar(4000)
+		SET @error_message = 'Error Number:' + ISNULL(CAST(ERROR_NUMBER() AS NVARCHAR(100)),'') + 
+						     ' Error State:' + ISNULL(CAST(ERROR_STATE() AS NVARCHAR(100)),'')  + 
+						  ' Error Severity:' + ISNULL(CAST(ERROR_SEVERITY() AS NVARCHAR(100)),'') + CHAR(13) + 
+					      'Error Procedure:' + ISNULL(ERROR_PROCEDURE(),'') + 
+						      ' Error Line:' + ISNULL(CAST(ERROR_LINE() AS NVARCHAR(100)),'') + CHAR(13) + 
+						    'Error Message:' + ISNULL(ERROR_MESSAGE(),'')
+	
+		EXEC edw_core.sp_upd_error_tetl_audit @etl_audit_sk,@error_message;
+		THROW 99001,'Error occured: see tetl_audit table for more info', 1;
+	END CATCH
+END
+
+GO
