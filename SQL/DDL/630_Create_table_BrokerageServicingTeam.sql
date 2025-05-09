32a74b37ccba9c5f7a04e67360add1cd154f5d0e
Bhaskar Muthyala <bhaskar.muthyala@vault.insurance>
2025-05-08 21:40:16 -0400

    Updated

--- SQL/DDL/630_Create_table_BrokerageServicingTeam.sql
+++ SQL/DDL/630_Create_table_BrokerageServicingTeam.sql
@@ -4,11 +4,10 @@ WHERE TABLE_SCHEMA = 'edw_stage'
 AND TABLE_name = 'BrokerageServicingTeam')
 BEGIN
 CREATE TABLE edw_stage.BrokerageServicingTeam (
-	Id uniqueidentifier NOT NULL,
+	Id uniqueidentifier NULL,
 	Name nvarchar(200) NULL,
 	ExternalSourceId nvarchar(2000) NULL,
-	CreatedDate datetime2 NOT NULL,
-	UpdatedDate datetime2 NOT NULL,
-	CONSTRAINT PK_BrokerageServicingTeam PRIMARY KEY (Id)
+	CreatedDate datetime2 NULL,
+	UpdatedDate datetime2 NULL
 ) 
 END ; 
--- SQL/DDL/631_Create_Table_BrokerageServicingTeamMember.sql
+++ SQL/DDL/631_Create_Table_BrokerageServicingTeamMember.sql
@@ -4,14 +4,12 @@ WHERE TABLE_SCHEMA = 'edw_stage'
 AND TABLE_name = 'BrokerageServicingTeamMember')
 BEGIN
 CREATE TABLE edw_stage.BrokerageServicingTeamMember (
-	Id int IDENTITY(1,1) NOT NULL,
-	BrokerageServicingTeamId uniqueidentifier NOT NULL,
+	Id int NULL,
+	BrokerageServicingTeamId uniqueidentifier NULL,
 	UserId nvarchar(MAX) NULL,
 	ExternalSourceId nvarchar(2000) NULL,
 	ExternalSourceUniqueId nvarchar(2000) NULL,
-	CreatedDate datetime2 NOT NULL,
-	UpdatedDate datetime2 NULL,
-	CONSTRAINT PK_BrokerageServicingTeamMember PRIMARY KEY (Id),
-	CONSTRAINT FK_BrokerageServicingTeamMember_BrokerageServicingTeam_BrokerageServicingTeamId FOREIGN KEY (BrokerageServicingTeamId) REFERENCES edw_stage.BrokerageServicingTeam(Id) ON DELETE CASCADE
+	CreatedDate datetime2 NULL,
+	UpdatedDate datetime2 NULL
 )
 END ; 
