37a6e96110b72132d8a01a4755cf792043626277
Bhaskar Muthyala <bhaskar.muthyala@vault.insurance>
2025-05-12 14:13:59 -0400

    Renamed column from id to broker_servicing_team_id

--- SQL/DDL/643_Alter_Table_tbroker_servicing_team.sql
+++ SQL/DDL/643_Alter_Table_tbroker_servicing_team.sql
@@ -3,8 +3,8 @@ SELECT 1
 FROM INFORMATION_SCHEMA.COLUMNS					
 WHERE TABLE_SCHEMA='edw_core'					
 AND TABLE_NAME = 'tbroker_servicing_team'					
-AND COLUMN_NAME = 'id'					
+AND COLUMN_NAME = 'broker_servicing_team_id'					
 ) 
 BEGIN 
-ALTER TABLE edw_core.tbroker_servicing_team ADD id uniqueidentifier NOT NULL 
+ALTER TABLE edw_core.tbroker_servicing_team ADD broker_servicing_team_id uniqueidentifier NOT NULL 
 END ; 
