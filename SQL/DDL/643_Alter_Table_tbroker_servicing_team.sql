3e535b501b5251ef1216d301a4255f1db90a7b8a
Bhaskar Muthyala <bhaskar.muthyala@vault.insurance>
2025-05-12 14:53:35 -0400

    Added unique constraint

--- SQL/DDL/643_Alter_Table_tbroker_servicing_team.sql
+++ SQL/DDL/643_Alter_Table_tbroker_servicing_team.sql
@@ -6,5 +6,6 @@ AND TABLE_NAME = 'tbroker_servicing_team'
 AND COLUMN_NAME = 'broker_servicing_team_id'					
 ) 
 BEGIN 
-ALTER TABLE edw_core.tbroker_servicing_team ADD broker_servicing_team_id varchar(255) NOT NULL 
+ALTER TABLE edw_core.tbroker_servicing_team ADD broker_servicing_team_id varchar(255) NOT NULL, 
+CONSTRAINT uidx_broker_servicing_team_id UNIQUE (broker_servicing_team_id)
 END ; 
