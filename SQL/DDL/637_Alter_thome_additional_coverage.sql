f88ddf1cc5b84a8aba40b4a6d23e2eb551b62169
Tuba Mohsin <tuba.mohsin@vault.insurance>
2025-05-13 12:28:59 -0400

    Changes as per sandeeps comments

--- SQL/DDL/637_Alter_thome_additional_coverage.sql
+++ SQL/DDL/637_Alter_thome_additional_coverage.sql
@@ -3,5 +3,13 @@ SELECT 1
 FROM INFORMATION_SCHEMA.COLUMNS					
 WHERE TABLE_SCHEMA='edw_core'					
 AND TABLE_NAME = 'thome_additional_coverage'					
-AND COLUMN_NAME = 'all_peril_roof_covering_coverage_CW_in'					
-) BEGIN ALTER TABLE edw_core.thome_additional_coverage ADD all_peril_roof_covering_coverage_CW_in VARCHAR(255) END ; 
+AND COLUMN_NAME = 'all_peril_roof_covering_coverage_cw_in'					
+) BEGIN ALTER TABLE edw_core.thome_additional_coverage ADD all_peril_roof_covering_coverage_cw_in VARCHAR(255) END ; 
+
+IF NOT EXISTS (					
+SELECT 1					
+FROM INFORMATION_SCHEMA.COLUMNS					
+WHERE TABLE_SCHEMA='edw_core'					
+AND TABLE_NAME = 'thome_additional_coverage'					
+AND COLUMN_NAME = 'gate_entry_code_required_in'					
+) BEGIN ALTER TABLE edw_core.thome_additional_coverage ADD gate_entry_code_required_in VARCHAR(255) END ; 
