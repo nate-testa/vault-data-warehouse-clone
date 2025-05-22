ab7ed38ea7b28b27bb3bb1650309fea71c20ce03
Yunus Mohammed <yunus.mohammed@vault.insurance>
2025-05-22 13:36:46 -0400

    Validation updated

--- SQL/DML/166_Insert_tvalidation_sql.sql
+++ SQL/DML/166_Insert_tvalidation_sql.sql
@@ -1,20 +1,8 @@
--- select * from edw_core.tvalidation_sql order by 1 desc
--- INSERT INTO edw_core.tvalidation_sql (validation_sql_desc , source_sql , target_sql , active_in , frequency_desc , create_ts , update_ts)
+INSERT INTO edw_core.tvalidation_sql (validation_sql_desc , source_sql , target_sql , active_in , frequency_desc , create_ts , update_ts)
 SELECT
 	'Metal EDW Validation - AccountTransaction - Compare rows between Metal and EDW stage ' AS validation_sql_desc ,
        'select count(*) from dbo.AccountTransaction where CreatedDate=''var_actual_dt''' AS source_sql ,
        'select count(*) from edw_stage.AccountTransaction where CreatedDate=''var_actual_dt''' AS target_sql ,
-       'Y' AS active_in ,
-       'Daily' AS frequency_desc ,
-       getdate() AS create_ts ,
-       getdate() AS update_ts;
-
---
-INSERT INTO edw_core.tvalidation_sql (validation_sql_desc , source_sql , target_sql , active_in , frequency_desc , create_ts , update_ts)
-SELECT
-	'Metal EDW Validation - Account - Compare rows between Metal and EDW stage ' AS validation_sql_desc ,
-       'select count(*) from dbo.Account where CreatedDate=''var_actual_dt''' AS source_sql ,
-       'select count(*) from edw_stage.Account where CreatedDate=''var_actual_dt''' AS target_sql ,
        'Y' AS active_in ,
        'Daily' AS frequency_desc ,
        getdate() AS create_ts ,
