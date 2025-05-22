6a3c917af759db81f5b236d2de53f0a366e052a4
Architha Gudimalla <architha.gudimalla@vault.insurance>
2025-05-22 14:51:39 -0400

    updated

--- SQL/DML/167_Insert_tvalidation_sql.sql
+++ SQL/DML/167_Insert_tvalidation_sql.sql
@@ -1,7 +1,7 @@
 INSERT INTO edw_core.tvalidation_sql (validation_sql_desc , source_sql , target_sql , active_in , frequency_desc , create_ts , update_ts)
 SELECT
 'tpolicy - Incorrect cancelled policy status' AS validation_sql_desc ,
-       'select count(*) from edw_core.tpolicy where cancellation_effective_dt > getdate()' AS source_sql ,
+       'select count(*) from edw_core.tpolicy where cancellation_effective_dt > getdate() and policy_status = 'Cancelled'' AS source_sql ,
        'select 0' AS target_sql ,
        'Y' AS active_in ,
        'Daily' AS frequency_desc ,
