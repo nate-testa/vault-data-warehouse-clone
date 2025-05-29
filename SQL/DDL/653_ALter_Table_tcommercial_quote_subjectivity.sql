cdd6731c0ed3f9eeb6a5a56d0324c882b145aaa2
Yunus Mohammed <yunus.mohammed@vault.insurance>
2025-05-29 13:12:55 -0400

    ddl updated

--- SQL/DDL/653_ALter_Table_tcommercial_quote_subjectivity.sql
+++ SQL/DDL/653_ALter_Table_tcommercial_quote_subjectivity.sql
@@ -3,6 +3,7 @@ from information_schema.table_constraints
 where table_name = 'tcommercial_quote_subjectivity'
 and table_schema = 'edw_commercial'
 and constraint_type = 'UNIQUE'
+and constraint_name = 'uidx_tcommercial_quote_subjectivity_quote_no_effective_dt' 
 )
 BEGIN
 Alter table edw_commercial.tcommercial_quote_subjectivity
