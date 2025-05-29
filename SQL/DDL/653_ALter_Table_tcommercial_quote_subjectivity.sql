ddef0e71310e3eaa8997fcffd7d5bbf49cf0c135
Yunus Mohammed <yunus.mohammed@vault.insurance>
2025-05-29 13:05:51 -0400

    Constraint added to tcommercial_quote_subjectivity

---
+++ SQL/DDL/653_ALter_Table_tcommercial_quote_subjectivity.sql
@@ -0,0 +1,11 @@
+if not exists(select *
+from information_schema.table_constraints
+where table_name = 'tcommercial_quote_subjectivity'
+and table_schema = 'edw_commercial'
+and constraint_type = 'UNIQUE'
+)
+BEGIN
+Alter table edw_commercial.tcommercial_quote_subjectivity
+add constraint uidx_tcommercial_quote_subjectivity_quote_no_effective_dt
+unique(quote_no, effective_dt)
+END
