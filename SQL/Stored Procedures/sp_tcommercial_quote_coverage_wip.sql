58bd0d6096bc6c011ac6ba82e3919828abc652eb
Yunus Mohammed <yunus.mohammed@vault.insurance>
2025-05-29 15:48:40 -0400

    Added new columns

--- SQL/Stored Procedures/sp_tcommercial_quote_coverage_wip.sql
+++ SQL/Stored Procedures/sp_tcommercial_quote_coverage_wip.sql
@@ -5,7 +5,8 @@
 -- Change date          |Author						        |	Change Description
 -----------------------------------------------------------------------------------------------------------------------
 -- 03/28/25		          Yunus Mohammed		1.Procedure created
--- 22/04/2025             Alberto Almario		2.Change PolicyNumber to Number from Account table
+-- 04/22/25              Alberto Almario		2.Change PolicyNumber to Number from Account table
+-- 05/29/25				  Yunus Mohammed		3. AD-9660 Added new columns
 -- ===================================================================================================================== 
 CREATE OR ALTER PROCEDURE [edw_core].[sp_tcommercial_quote_coverage_wip]
 
@@ -47,7 +48,12 @@ BEGIN
 		CreatedDate,UpdatedDate,commercial_quote_history_sk,product_name,
         CoverageType as coverage_type,CoverageTypeB as coverage_type_b,Revenue as revenue_amt,
         MemorandumOfInsurance as memorandum_of_insurance_in,NumberOfFTEAttorneys as employee_ct,
-        coalesce(ClaimsActivity,ClaimsHistory) as claim_history,getdate() as create_ts,getdate() as update_ts,@etl_audit_sk as etl_audit_sk
+        coalesce(ClaimsActivity,ClaimsHistory) as claim_history,RetroactiveDate as retroactive_date_desc,PriorOrPendingDate as prior_or_pending_date_desc,
+		case
+				when SingleRoundTheClockReinstatement = 'true' then 'Yes' 
+				when SingleRoundTheClockReinstatement = 'false' then 'No'
+			end as  single_round_the_clock_resinstatement_in,
+		getdate() as create_ts,getdate() as update_ts,@etl_audit_sk as etl_audit_sk
 		into edw_temp.tcommercial_quote_coverage_wip_temp2
 			from
 			(
@@ -64,7 +70,7 @@ BEGIN
 						and cph.transaction_seq_no = 0				
 			where				
                 accof.Field in ('CoverageType','CoverageTypeB','Revenue','MemorandumOfInsurance','NumberOfFTEAttorneys',
-                'ClaimsActivity','ClaimsHistory'
+                'ClaimsActivity','ClaimsHistory','RetroactiveDate','PriorOrPendingDate','SingleRoundTheClockReinstatement'
                 )
 				
 			) as t
@@ -72,7 +78,8 @@ BEGIN
 			(
 				max(Value) FOR Field IN
                  (
-                    CoverageType,CoverageTypeB,Revenue,MemorandumOfInsurance,NumberOfFTEAttorneys, ClaimsActivity,ClaimsHistory
+                    CoverageType,CoverageTypeB,Revenue,MemorandumOfInsurance,NumberOfFTEAttorneys, ClaimsActivity,ClaimsHistory,
+					RetroactiveDate,PriorOrPendingDate,SingleRoundTheClockReinstatement
                 )
 			) as pivottable
 
@@ -86,13 +93,15 @@ BEGIN
             (
             quote_no,effective_dt,expiration_dt,transaction_seq_no,
             commercial_quote_history_sk,coverage_type,coverage_type_b,revenue_amt,memorandum_of_insurance_in,
-            employee_ct,claim_history,source_system_sk,create_ts,update_ts,etl_audit_sk
+            employee_ct,claim_history,retroactive_date_desc,prior_or_pending_date_desc,single_round_the_clock_resinstatement_in,
+			source_system_sk,create_ts,update_ts,etl_audit_sk
             )
             VALUES
             (
             quote_no,effective_dt,expiration_dt,transaction_seq_no,
             commercial_quote_history_sk,coverage_type,coverage_type_b,revenue_amt,memorandum_of_insurance_in,
-            employee_ct,claim_history,source_system_sk,create_ts,update_ts,etl_audit_sk
+            employee_ct,claim_history,retroactive_date_desc,prior_or_pending_date_desc,single_round_the_clock_resinstatement_in,
+			source_system_sk,create_ts,update_ts,etl_audit_sk
             )
             WHEN MATCHED THEN UPDATE
             SET
@@ -105,6 +114,9 @@ BEGIN
             [target].memorandum_of_insurance_in = [source].memorandum_of_insurance_in,
             [target].employee_ct = [source].employee_ct,
             [target].claim_history = [source].claim_history,
+			[target].retroactive_date_desc = [source].retroactive_date_desc,
+			[target].prior_or_pending_date_desc = [source].prior_or_pending_date_desc,
+			[target].single_round_the_clock_resinstatement_in = [source]. single_round_the_clock_resinstatement_in,
             [target].update_ts = GETDATE();
 
 		SET @rows_affected=@@ROWCOUNT;
