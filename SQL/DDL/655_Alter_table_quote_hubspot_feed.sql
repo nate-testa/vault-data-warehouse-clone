a893a64d13ee4858a0a130015d20f7ba470cb625
Architha Gudimalla <architha.gudimalla@vault.insurance>
2025-05-30 16:38:06 -0400

    updated

--- SQL/DDL/655_Alter_table_quote_hubspot_feed.sql
+++ SQL/DDL/655_Alter_table_quote_hubspot_feed.sql
@@ -51,8 +51,8 @@ SELECT 1
 FROM INFORMATION_SCHEMA.COLUMNS					
 WHERE TABLE_SCHEMA='edw_integration'					
 AND TABLE_NAME = 'quote_hubspot_feed'					
-AND COLUMN_NAME = 'threafter_retention'		
-) BEGIN ALTER TABLE edw_integration.quote_hubspot_feed ADD threafter_retention int END			 
+AND COLUMN_NAME = 'thereafter_retention'		
+) BEGIN ALTER TABLE edw_integration.quote_hubspot_feed ADD thereafter_retention int END			 
 ; 
 IF NOT EXISTS (					
 SELECT 1					
