ea761e4ac5be743263a94f5ecf66a288719074c2
Bhaskar Muthyala <bhaskar.muthyala@vault.insurance>
2025-06-06 15:38:53 -0400

    Added new column litigation_in

---
+++ SQL/DDL/660_Alter_Table_tclaim.sql
@@ -0,0 +1,7 @@
+IF NOT EXISTS (                
+SELECT 1                    
+FROM INFORMATION_SCHEMA.COLUMNS                
+WHERE TABLE_SCHEMA='edw_core'                
+AND TABLE_NAME = 'tclaim '                  
+AND COLUMN_NAME = 'litigation_in'                    
+) BEGIN ALTER TABLE edw_core.tclaim ADD litigation_in VARCHAR(255) NULL END ; 
