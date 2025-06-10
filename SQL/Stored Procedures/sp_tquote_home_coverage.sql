a656ba1d8f7d9eea2ef28077733de19af219085a
dinesh.bobbili <dinesh.bobbili@vault.insurance>
2025-06-10 12:01:46 -0400

    Added new fields wildfire_suppression_system,wildfire_decks_balconies_porches_stairs

--- SQL/Stored Procedures/sp_tquote_home_coverage.sql
+++ SQL/Stored Procedures/sp_tquote_home_coverage.sql
@@ -22,7 +22,8 @@
 -- 01/22/25				Alberto Almario					16. Added new column fenced_pool_in
 -- 03/19/25				Hernando Gonzalez			17. Added new columns wildfire_risk_score, wildfire_risk_class
 -- 04/02/25				Yunus Mohammed				18. AD-8973 roof_deck_attachment value logic updated
--- 04/16/25		Yunus Mohammed						19. AD-9121 Corrected null values for premium mods
+-- 04/16/25				Yunus Mohammed				19. AD-9121 Corrected null values for premium mods
+-- 06/10/22				Dinesh Bobbili				20. Added new fields wildfire_suppression_system,wildfire_decks_balconies_porches_stairs
 -- =========================================================================================================================== 
 CREATE OR ALTER  PROCEDURE [edw_core].[sp_tquote_home_coverage]
 
@@ -196,6 +197,7 @@ BEGIN
 				facultative_ceded_premium_amt, facultative_reinsurer_nm, coverage_layer, coverage_layer_placed_pc, coverage_layer_limit_amt, 
 				newly_purchased_home_in, target_closing_dt, current_policy_anniversary_dt, current_underlying_company_nm, new_client_for_agency_in,
 				no_of_bathrooms,no_of_fireplaces,foundation_type,waived_inflation_factor_in,fenced_pool_in,wildfire_risk_score,wildfire_risk_class,
+				wildfire_suppression_system,wildfire_decks_balconies_porches_stairs,
 				source_system_sk,create_ts,update_ts,etl_audit_sk
 			)			
 			SELECT
@@ -350,6 +352,8 @@ BEGIN
 				tthc.FencedPool as fenced_pool_in,
 				tthc.WildfireRiskScore as wildfire_risk_score,
 				tthc.WildfireRiskClass as wildfire_risk_class,
+				tthc.WildfireSuppressionSystem as wildfire_suppression_system,
+				tthc.WildfireDecksBalconiesPorchesStairs as wildfire_decks_balconies_porches_stairs,
 				source_system_sk,getdate() AS create_ts,getdate() AS update_ts,@etl_audit_sk AS etl_audit_sk
 			FROM
 				edw_temp.tquote_home_coverage_temp2 AS tthc
