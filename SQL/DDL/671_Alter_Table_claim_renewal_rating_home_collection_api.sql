21c332c78b47e991fbc8e4a9a8595c06c4b9ca4e
Bhaskar Muthyala <bhaskar.muthyala@vault.insurance>
2025-06-11 08:46:14 -0400

    Renamed column names

--- SQL/DDL/671_Alter_Table_claim_renewal_rating_home_collection_api.sql
+++ SQL/DDL/671_Alter_Table_claim_renewal_rating_home_collection_api.sql
@@ -3,13 +3,13 @@ SELECT 1
 FROM INFORMATION_SCHEMA.COLUMNS                
 WHERE TABLE_SCHEMA='edw_integration'                
 AND TABLE_NAME = 'claim_renewal_rating_home_collection_api'                  
-AND COLUMN_NAME = 'Litigation_In'                    
-) BEGIN ALTER TABLE edw_integration.claim_renewal_rating_home_collection_api ADD Litigation_In VARCHAR(255) NULL END ; 
+AND COLUMN_NAME = 'LitigationIn'                    
+) BEGIN ALTER TABLE edw_integration.claim_renewal_rating_home_collection_api ADD LitigationIn VARCHAR(255) NULL END ; 
 
 IF NOT EXISTS (                
 SELECT 1                    
 FROM INFORMATION_SCHEMA.COLUMNS                
 WHERE TABLE_SCHEMA='edw_integration'                
 AND TABLE_NAME = 'claim_renewal_rating_home_collection_api'                  
-AND COLUMN_NAME = 'Litigation_Complete_In'                    
-) BEGIN ALTER TABLE edw_integration.claim_renewal_rating_home_collection_api ADD Litigation_Complete_In VARCHAR(255) NULL END ; 
+AND COLUMN_NAME = 'LitigationCompleteIn'                    
+) BEGIN ALTER TABLE edw_integration.claim_renewal_rating_home_collection_api ADD LitigationCompleteIn VARCHAR(255) NULL END ; 
