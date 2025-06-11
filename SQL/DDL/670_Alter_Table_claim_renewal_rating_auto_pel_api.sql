8638b347f07f32f752719dd42a20d327c976b898
Bhaskar Muthyala <bhaskar.muthyala@vault.insurance>
2025-06-11 08:51:16 -0400

    Column renamed

--- SQL/DDL/670_Alter_Table_claim_renewal_rating_auto_pel_api.sql
+++ SQL/DDL/670_Alter_Table_claim_renewal_rating_auto_pel_api.sql
@@ -3,13 +3,13 @@ SELECT 1
 FROM INFORMATION_SCHEMA.COLUMNS                
 WHERE TABLE_SCHEMA='edw_integration'                
 AND TABLE_NAME = 'claim_renewal_rating_auto_pel_api'                  
-AND COLUMN_NAME = 'LitigationIn'                    
-) BEGIN ALTER TABLE edw_integration.claim_renewal_rating_auto_pel_api ADD LitigationIn VARCHAR(255) NULL END ; 
+AND COLUMN_NAME = 'Litigation'                    
+) BEGIN ALTER TABLE edw_integration.claim_renewal_rating_auto_pel_api ADD Litigation VARCHAR(255) NULL END ; 
 
 IF NOT EXISTS (                
 SELECT 1                    
 FROM INFORMATION_SCHEMA.COLUMNS                
 WHERE TABLE_SCHEMA='edw_integration'                
 AND TABLE_NAME = 'claim_renewal_rating_auto_pel_api'                  
-AND COLUMN_NAME = 'LitigationCompleteIn'                    
-) BEGIN ALTER TABLE edw_integration.claim_renewal_rating_auto_pel_api ADD LitigationCompleteIn VARCHAR(255) NULL END ; 
+AND COLUMN_NAME = 'LitigationComplete'                    
+) BEGIN ALTER TABLE edw_integration.claim_renewal_rating_auto_pel_api ADD LitigationComplete VARCHAR(255) NULL END ; 
