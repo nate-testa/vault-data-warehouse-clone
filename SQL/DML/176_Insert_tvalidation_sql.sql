2f5accea2a7495b43e28a1579204c1a9909b1b95
Yunus Mohammed <yunus.mohammed@vault.insurance>
2025-06-13 09:48:54 -0400

    Updated target SQL

--- SQL/DML/176_Insert_tvalidation_sql.sql
+++ SQL/DML/176_Insert_tvalidation_sql.sql
@@ -2,8 +2,8 @@ INSERT INTO edw_core.tvalidation_sql (validation_sql_desc , source_sql , target_
 SELECT
 	'Metal EDW Validation - AccountTransactionTaxAndFee (name=Clearinghouse fee and Type = Fee) - Compare rows between Metal and EDW stage ' AS validation_sql_desc ,
        'select count(*) from dbo.AccountTransactionTaxAndFee where [name]=''Clearinghouse fee'' and Type = ''Fee''' AS source_sql ,
-       'select count(*) from edw_stage.AccountTransactionTaxAndFee where [name]=''Clearinghouse fee'' and Type = ''Fee''' AS target_sql ,
-       'N' AS active_in ,
+       'select 0' AS target_sql ,
+       'Y' AS active_in ,
        'Daily' AS frequency_desc ,
        getdate() AS create_ts ,
        getdate() AS update_ts
@@ -11,7 +11,7 @@ UNION
 SELECT
 	'Metal EDW Validation - AccountTransactionTaxAndFee (name=EMPA Surcharge and Type = Surcharge) - Compare rows between Metal and EDW stage ' AS validation_sql_desc ,
        'select count(*) from dbo.AccountTransactionTaxAndFee where [name]=''EMPA Surcharge'' and Type = ''Surcharge''' AS source_sql ,
-       'select count(*) from edw_stage.AccountTransactionTaxAndFee where [name]=''EMPA Surcharge'' and Type = ''Surcharge''' AS target_sql ,
+       'select 0' AS target_sql ,
        'Y' AS active_in ,
        'Daily' AS frequency_desc ,
        getdate() AS create_ts ,
@@ -20,8 +20,8 @@ UNION
 SELECT
 	'Metal EDW Validation - AccountTransactionTaxAndFee (name=Surplus Lines Tax and Type = Tax) - Compare rows between Metal and EDW stage ' AS validation_sql_desc ,
        'select count(*) from dbo.AccountTransactionTaxAndFee where [name]=''Surplus Lines Tax'' and Type = ''Tax''' AS source_sql ,
-       'select count(*) from edw_stage.AccountTransactionTaxAndFee where [name]=''Surplus Lines Tax'' and Type = ''Tax''' AS target_sql ,
-       'N' AS active_in ,
+       'select 0' AS target_sql ,
+       'Y' AS active_in ,
        'Daily' AS frequency_desc ,
        getdate() AS create_ts ,
        getdate() AS update_ts
@@ -29,7 +29,7 @@ UNION
 SELECT
 	'Metal EDW Validation - AccountTransactionTaxAndFee (name=Healthy Homes Fund Surcharge and Type = Surcharge) - Compare rows between Metal and EDW stage ' AS validation_sql_desc ,
        'select count(*) from dbo.AccountTransactionTaxAndFee where [name]=''Healthy Homes Fund Surcharge'' and Type = ''Surcharge''' AS source_sql ,
-       'select count(*) from edw_stage.AccountTransactionTaxAndFee where [name]=''Healthy Homes Fund Surcharge'' and Type = ''Surcharge''' AS target_sql ,
+       'select 0' AS target_sql ,
        'Y' AS active_in ,
        'Daily' AS frequency_desc ,
        getdate() AS create_ts ,
