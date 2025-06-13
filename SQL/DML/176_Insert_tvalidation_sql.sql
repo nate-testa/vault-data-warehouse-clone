dd28c19d3c3808a8deccb445bb6a2873d05a3984
Sandeep Gundreddy <sandeep.gundreddy@vault.insurance>
2025-06-13 15:08:40 -0400

    modified sql description

--- SQL/DML/176_Insert_tvalidation_sql.sql
+++ SQL/DML/176_Insert_tvalidation_sql.sql
@@ -1,6 +1,6 @@
 INSERT INTO edw_core.tvalidation_sql (validation_sql_desc , source_sql , target_sql , active_in , frequency_desc , create_ts , update_ts)
 SELECT
-	'Clearinghouse fee transactions mapped to incorrect coverage category ' AS validation_sql_desc ,
+	'Metal validation - Clearinghouse fee transactions mapped to incorrect coverage category ' AS validation_sql_desc ,
        'select count(*) from edw_stage.AccountTransactionTaxAndFee where [name]=''Clearinghouse fee'' and Type = ''Fee''' AS source_sql ,
        'select 0' AS target_sql ,
        'Y' AS active_in ,
@@ -9,7 +9,7 @@ SELECT
        getdate() AS update_ts
 UNION
 SELECT
-	'EMPA Surcharge transactions mapped to incorrect coverage category' AS validation_sql_desc ,
+	'Metal validation - EMPA Surcharge transactions mapped to incorrect coverage category' AS validation_sql_desc ,
        'select count(*) from edw_stage.AccountTransactionTaxAndFee where [name]=''EMPA Surcharge'' and Type = ''Surcharge''' AS source_sql ,
        'select 0' AS target_sql ,
        'Y' AS active_in ,
@@ -18,7 +18,7 @@ SELECT
        getdate() AS update_ts
 UNION
 SELECT
-	'Surplus Lines Tax transactions mapped to incorrect coverage category ' AS validation_sql_desc ,
+	'Metal validation - Surplus Lines Tax transactions mapped to incorrect coverage category ' AS validation_sql_desc ,
        'select count(*) from edw_stage.AccountTransactionTaxAndFee where [name]=''Surplus Lines Tax'' and Type = ''Tax''' AS source_sql ,
        'select 0' AS target_sql ,
        'Y' AS active_in ,
@@ -27,7 +27,7 @@ SELECT
        getdate() AS update_ts
 UNION
 SELECT
-	'Healthy Homes Fund Surcharge transactions mapped to incorrect coverage category ' AS validation_sql_desc ,
+	'Metal validation - Healthy Homes Fund Surcharge transactions mapped to incorrect coverage category ' AS validation_sql_desc ,
        'select count(*) from edw_stage.AccountTransactionTaxAndFee where [name]=''Healthy Homes Fund Surcharge'' and Type = ''Surcharge''' AS source_sql ,
        'select 0' AS target_sql ,
        'Y' AS active_in ,
