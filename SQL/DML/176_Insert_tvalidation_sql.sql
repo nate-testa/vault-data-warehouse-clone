51d42072bc26bf612d84ab20a71c4134c29d6ca1
Yunus Mohammed <yunus.mohammed@vault.insurance>
2025-06-13 11:58:31 -0400

    Changes made

--- SQL/DML/176_Insert_tvalidation_sql.sql
+++ SQL/DML/176_Insert_tvalidation_sql.sql
@@ -1,7 +1,7 @@
 INSERT INTO edw_core.tvalidation_sql (validation_sql_desc , source_sql , target_sql , active_in , frequency_desc , create_ts , update_ts)
 SELECT
-	'Metal EDW Validation - AccountTransactionTaxAndFee (name=Clearinghouse fee and Type = Fee) - Compare rows between Metal and EDW stage ' AS validation_sql_desc ,
-       'select count(*) from dbo.AccountTransactionTaxAndFee where [name]=''Clearinghouse fee'' and Type = ''Fee''' AS source_sql ,
+	'Clearinghouse fee transactions mapped to incorrect coverage category ' AS validation_sql_desc ,
+       'select count(*) from edw_stage.AccountTransactionTaxAndFee where [name]=''Clearinghouse fee'' and Type = ''Fee''' AS source_sql ,
        'select 0' AS target_sql ,
        'Y' AS active_in ,
        'Daily' AS frequency_desc ,
@@ -9,8 +9,8 @@ SELECT
        getdate() AS update_ts
 UNION
 SELECT
-	'Metal EDW Validation - AccountTransactionTaxAndFee (name=EMPA Surcharge and Type = Surcharge) - Compare rows between Metal and EDW stage ' AS validation_sql_desc ,
-       'select count(*) from dbo.AccountTransactionTaxAndFee where [name]=''EMPA Surcharge'' and Type = ''Surcharge''' AS source_sql ,
+	'EMPA Surcharge transactions mapped to incorrect coverage category' AS validation_sql_desc ,
+       'select count(*) from edw_stage.AccountTransactionTaxAndFee where [name]=''EMPA Surcharge'' and Type = ''Surcharge''' AS source_sql ,
        'select 0' AS target_sql ,
        'Y' AS active_in ,
        'Daily' AS frequency_desc ,
@@ -18,8 +18,8 @@ SELECT
        getdate() AS update_ts
 UNION
 SELECT
-	'Metal EDW Validation - AccountTransactionTaxAndFee (name=Surplus Lines Tax and Type = Tax) - Compare rows between Metal and EDW stage ' AS validation_sql_desc ,
-       'select count(*) from dbo.AccountTransactionTaxAndFee where [name]=''Surplus Lines Tax'' and Type = ''Tax''' AS source_sql ,
+	'Surplus Lines Tax transactions mapped to incorrect coverage category ' AS validation_sql_desc ,
+       'select count(*) from edw_stage.AccountTransactionTaxAndFee where [name]=''Surplus Lines Tax'' and Type = ''Tax''' AS source_sql ,
        'select 0' AS target_sql ,
        'Y' AS active_in ,
        'Daily' AS frequency_desc ,
@@ -27,8 +27,8 @@ SELECT
        getdate() AS update_ts
 UNION
 SELECT
-	'Metal EDW Validation - AccountTransactionTaxAndFee (name=Healthy Homes Fund Surcharge and Type = Surcharge) - Compare rows between Metal and EDW stage ' AS validation_sql_desc ,
-       'select count(*) from dbo.AccountTransactionTaxAndFee where [name]=''Healthy Homes Fund Surcharge'' and Type = ''Surcharge''' AS source_sql ,
+	'Healthy Homes Fund Surcharge transactions mapped to incorrect coverage category ' AS validation_sql_desc ,
+       'select count(*) from edw_stage.AccountTransactionTaxAndFee where [name]=''Healthy Homes Fund Surcharge'' and Type = ''Surcharge''' AS source_sql ,
        'select 0' AS target_sql ,
        'Y' AS active_in ,
        'Daily' AS frequency_desc ,
