5e128461f0c67dbf3699dfc6830e08a79961ffc8
Tuba Mohsin <tuba.mohsin@vault.insurance>
2025-06-16 16:57:02 -0400

    changed data type for the staging table

--- SQL/DDL/673_Alter_Table_EDW_Stage_BrokerageLicense.sql
+++ SQL/DDL/673_Alter_Table_EDW_Stage_BrokerageLicense.sql
@@ -4,5 +4,5 @@ FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'edw_stage'                
 AND TABLE_NAME = 'BrokerageLicense'                  
 AND COLUMN_NAME = 'LicenseType'                    
-) BEGIN ALTER TABLE edw_stage.BrokerageLicense ADD LicenseType VARCHAR(255) NULL END ; 
+) BEGIN ALTER TABLE edw_stage.BrokerageLicense ADD LicenseType nvarchar(1000) NULL END ; 
 
