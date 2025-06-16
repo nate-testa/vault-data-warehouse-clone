a0126eac94efda4723943ea5563e4a310f067be5
Architha Gudimalla <architha.gudimalla@vault.insurance>
2025-06-16 12:54:34 -0400

    Added_product_nm

---
+++ SQL/DDL/677_Alter_Table_customer_hubspot_feed.sql
@@ -0,0 +1,8 @@
+IF NOT EXISTS (                
+SELECT 1                    
+FROM INFORMATION_SCHEMA.COLUMNS                
+WHERE TABLE_SCHEMA='edw_integration'                
+AND TABLE_NAME = 'customer_hubspot_feed'                  
+AND COLUMN_NAME = 'product_nm'                    
+) BEGIN ALTER TABLE edw_integration.customer_hubspot_feed ADD product_nm VARCHAR(255) NULL END ;   
+ 
