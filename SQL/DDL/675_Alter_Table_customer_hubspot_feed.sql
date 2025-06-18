2bd22c803162f8b0f9074ffaa8bd0b894450eb31
Architha Gudimalla <architha.gudimalla@vault.insurance>
2025-06-18 03:03:24 -0400

    updated

--- SQL/DDL/675_Alter_Table_customer_hubspot_feed.sql
+++ SQL/DDL/675_Alter_Table_customer_hubspot_feed.sql
@@ -3,6 +3,6 @@ SELECT 1
 FROM INFORMATION_SCHEMA.COLUMNS                
 WHERE TABLE_SCHEMA='edw_integration'                
 AND TABLE_NAME = 'customer_hubspot_feed'                  
-AND COLUMN_NAME = 'product_nm'                    
-) BEGIN ALTER TABLE edw_integration.customer_hubspot_feed ADD product_nm VARCHAR(255) NULL END ;   
+AND COLUMN_NAME = 'product_cd'                    
+) BEGIN ALTER TABLE edw_integration.customer_hubspot_feed ADD product_cd VARCHAR(255) NULL END ;  
  
