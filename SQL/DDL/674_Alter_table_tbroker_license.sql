5c5b6397c1dc7ea8feb631a2aaf0374eded0936f
Tuba Mohsin <tuba.mohsin@vault.insurance>
2025-06-20 10:34:51 -0400

    changed

--- SQL/DDL/674_Alter_table_tbroker_license.sql
+++ SQL/DDL/674_Alter_table_tbroker_license.sql
@@ -4,5 +4,5 @@ FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'edw_core'                
 AND TABLE_NAME = 'tbroker_license'                  
 AND COLUMN_NAME = 'license_type'                    
-) BEGIN ALTER TABLE edw_stage.tbroker_license ADD license_type VARCHAR(255) NULL END ; 
+) BEGIN ALTER TABLE edw_core.tbroker_license ADD license_type VARCHAR(255) NULL END ; 
 
