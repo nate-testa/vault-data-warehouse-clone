840456d7c176ed15b5b6ad3e1444d43dfdf41f0f
Architha Gudimalla <architha.gudimalla@vault.insurance>
2025-06-23 21:59:27 -0400

    Added_product_cd_underwriter_nm

--- SQL/DDL/672_Alter_Table_customer_hubspot_feed.sql
+++ SQL/DDL/672_Alter_Table_customer_hubspot_feed.sql
@@ -4,12 +4,4 @@ FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA='edw_integration'                
 AND TABLE_NAME = 'customer_hubspot_feed'                  
 AND COLUMN_NAME = 'underwriter_nm'                    
-) BEGIN ALTER TABLE edw_integration.customer_hubspot_feed ADD underwriter_nm VARCHAR(255) NULL END ;  
-
-IF NOT EXISTS (                
-SELECT 1                    
-FROM INFORMATION_SCHEMA.COLUMNS                
-WHERE TABLE_SCHEMA='edw_integration'                
-AND TABLE_NAME = 'customer_hubspot_feed'                  
-AND COLUMN_NAME = 'product_cd'                    
-) BEGIN ALTER TABLE edw_integration.customer_hubspot_feed ADD product_cd VARCHAR(255) NULL END ;  
+) BEGIN ALTER TABLE edw_integration.customer_hubspot_feed ADD underwriter_nm VARCHAR(255) NULL END ; 
