28d7b56a90c23a1be1ac9508bec491ad8da5062c
Architha Gudimalla <architha.gudimalla@vault.insurance>
2025-06-30 17:22:07 -0400

    Updated_to_use_insured_nm_as_last_nm

--- SQL/Stored Procedures/sp_customer_hubspot_feed_commercial.sql
+++ SQL/Stored Procedures/sp_customer_hubspot_feed_commercial.sql
@@ -6,6 +6,7 @@
 -------------------------------------------------------------------------------------------------------------------
 -- 05/30/25		Dinesh Bobbili			    1. Created this procedure
 -- 06/18/25		Dinesh Bobbili			    2. AD9853 added underwriter_nm column 
+-- 06/30/25		Architha Gudimalla		    3. Updated last name to use policy insured_nm
 -- ================================================================================================================== 
 
 CREATE OR ALTER PROCEDURE edw_core.sp_customer_hubspot_feed_commercial
@@ -51,7 +52,7 @@ BEGIN
 		SELECT
 			pol.policy_no,
 			null as first_nm,
-			null last_nm,
+			pol.insured_nm last_nm,
 			case when cust.email like '%papermail%' or cust.email like '%@%@%' then null else cust.email end email, 
 			pol.risk_state_cd,
 			pol.product_cd AS product_nm,
