6b5bb54284ddfb399f70c0ac64bd06bfd610c4a0
Rushin Shah <rushin.shah@vault.insurance>
2025-07-21 12:47:26 -0400

    Add PEL limit to WD tables

---
+++ SQL/DDL/696_Alter_table_policy_workday_written_premium_feed.sql
@@ -0,0 +1,39 @@
+IF NOT EXISTS (					
+SELECT 1					
+FROM INFORMATION_SCHEMA.COLUMNS					
+WHERE TABLE_SCHEMA='edw_integration'					
+AND TABLE_NAME = 'policy_workday_written_premium_feed'					
+AND COLUMN_NAME = 'do_limit_amt'					
+) BEGIN ALTER TABLE edw_integration.policy_workday_written_premium_feed ADD do_limit_amt varchar(255) END
+
+IF NOT EXISTS (					
+SELECT 1					
+FROM INFORMATION_SCHEMA.COLUMNS					
+WHERE TABLE_SCHEMA='edw_integration'					
+AND TABLE_NAME = 'policy_workday_written_premium_feed'					
+AND COLUMN_NAME = 'employment_practices_liability_amt'					
+) BEGIN ALTER TABLE edw_integration.policy_workday_written_premium_feed ADD employment_practices_liability_amt varchar(255) END
+
+IF NOT EXISTS (					
+SELECT 1					
+FROM INFORMATION_SCHEMA.COLUMNS					
+WHERE TABLE_SCHEMA='edw_integration'					
+AND TABLE_NAME = 'policy_workday_written_premium_feed'					
+AND COLUMN_NAME = 'pel_limit_amt'					
+) BEGIN ALTER TABLE edw_integration.policy_workday_written_premium_feed ADD pel_limit_amt varchar(255) END
+
+IF NOT EXISTS (					
+SELECT 1					
+FROM INFORMATION_SCHEMA.COLUMNS					
+WHERE TABLE_SCHEMA='edw_integration'					
+AND TABLE_NAME = 'policy_workday_written_premium_feed'					
+AND COLUMN_NAME = 'uninsured_underinsured_liability_amt'					
+) BEGIN ALTER TABLE edw_integration.policy_workday_written_premium_feed ADD uninsured_underinsured_liability_amt varchar(255) END
+
+IF NOT EXISTS (					
+SELECT 1					
+FROM INFORMATION_SCHEMA.COLUMNS					
+WHERE TABLE_SCHEMA='edw_integration'					
+AND TABLE_NAME = 'policy_workday_written_premium_feed'					
+AND COLUMN_NAME = 'uninsured_underinsured_motorist_liability_amt'					
+) BEGIN ALTER TABLE edw_integration.policy_workday_written_premium_feed ADD uninsured_underinsured_motorist_liability_amt varchar(255) END
