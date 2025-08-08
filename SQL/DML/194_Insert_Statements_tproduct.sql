56cd78221a7583f7ff9db5ee82d8c35f50ec41ab
Rushin Shah <rushin.shah@vault.insurance>
2025-08-08 06:48:13 -0400

    NFP addition to tproduct and tsource_system

--- SQL/DML/194_Insert_Statements_tproduct.sql
+++ SQL/DML/194_Insert_Statements_tproduct.sql
@@ -1,2 +1,18 @@
-INSERT INTO edw_core.tproduct (product_cd,product_nm,ebao_product_cd,update_ts,product_category_nm) VALUES
-	 (N'GRPEL',N'Group Umbrella',NULL,getdate(),'PersonalLines');
+INSERT INTO edw_core.tproduct (
+    product_cd,
+    product_nm,
+    ebao_product_cd,
+    update_ts,
+    product_category_nm
+)
+SELECT
+    N'GRPEL',
+    N'Group Umbrella',
+    NULL,
+    GETDATE(),
+    'PersonalLines'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM edw_core.tproduct
+    WHERE product_cd = 'GRPEL'
+);
--- SQL/DML/195_Insert_Statements_tsource_system.sql
+++ SQL/DML/195_Insert_Statements_tsource_system.sql
@@ -1,2 +1,12 @@
-INSERT INTO edw_core.tsource_system (source_system_nm,update_ts) VALUES
-     (N'NFP',getdate());
+INSERT INTO edw_core.tsource_system (
+    source_system_nm,
+    update_ts
+)
+SELECT
+    N'NFP',
+    GETDATE()
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM edw_core.tsource_system
+    WHERE source_system_nm = 'NFP'
+);
