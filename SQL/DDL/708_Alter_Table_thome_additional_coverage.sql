b3aa31d52e84f2758a2021b536eb4b1a27f04cc4
Tuba Mohsin <tuba.mohsin@vault.insurance>
2025-08-12 13:05:47 -0400

    PR for Aug 22nd Release

---
+++ SQL/DDL/708_Alter_Table_thome_additional_coverage.sql
@@ -0,0 +1,24 @@
+IF NOT EXISTS (					
+SELECT 1					
+FROM INFORMATION_SCHEMA.COLUMNS					
+WHERE TABLE_SCHEMA='edw_core'					
+AND TABLE_NAME = 'thome_additional_coverage'					
+AND COLUMN_NAME = 'defense_coverage_within_limits_in'					
+) BEGIN ALTER TABLE edw_core.thome_additional_coverage ADD defense_coverage_within_limits_in varchar(255) END
+
+IF NOT EXISTS (					
+SELECT 1					
+FROM INFORMATION_SCHEMA.COLUMNS					
+WHERE TABLE_SCHEMA='edw_core'					
+AND TABLE_NAME = 'thome_additional_coverage'					
+AND COLUMN_NAME = 'wind_sublimit_in'					
+) BEGIN ALTER TABLE edw_core.thome_additional_coverage ADD wind_sublimit_in varchar(255) END
+
+IF NOT EXISTS (					
+SELECT 1					
+FROM INFORMATION_SCHEMA.COLUMNS					
+WHERE TABLE_SCHEMA='edw_core'					
+AND TABLE_NAME = 'thome_additional_coverage'					
+AND COLUMN_NAME = 'wind_sublimit_value_amt'					
+) BEGIN ALTER TABLE edw_core.thome_additional_coverage ADD wind_sublimit_value_amt varchar(255) END
+
