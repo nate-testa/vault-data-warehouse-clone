bc3617e6685f22ea55adb9b1e62bee2e6d2e6ae8
Yunus Mohammed <yunus.mohammed@vault.insurance>
2025-08-19 15:18:51 -0400

    Removed space

--- SQL/DDL/715_Alter_Table_policy_current_carrier_auto_np01.sql
+++ SQL/DDL/715_Alter_Table_policy_current_carrier_auto_np01.sql
@@ -3,7 +3,7 @@ SELECT 1
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA='edw_integration'
 AND TABLE_NAME = 'policy_current_carrier_auto_np01_feed'
-AND COLUMN_NAME = 'reporting_period_begin_dt '
+AND COLUMN_NAME = 'reporting_period_begin_dt'
 ) 
 BEGIN 
     ALTER TABLE edw_integration.policy_current_carrier_auto_np01_feed ADD reporting_period_begin_dt date not null
