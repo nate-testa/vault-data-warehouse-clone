cdd8ee1eb911e0295bd5de1e5ac82744bc061612
Yunus Mohammed <yunus.mohammed@vault.insurance>
2025-08-29 15:19:04 -0400

    Extra line removed

--- SQL/DDL/726_Alter_Table_AccountObject.sql
+++ SQL/DDL/726_Alter_Table_AccountObject.sql
@@ -5,5 +5,3 @@ WHERE TABLE_SCHEMA='edw_stage'
 AND TABLE_NAME = 'AccountObject'
 AND LOWER(COLUMN_NAME) = 'IsDeletedOnRenewal'
 ) BEGIN ALTER TABLE edw_stage.AccountObject ADD IsDeletedOnRenewal bit NULL END ;
-
-select * from edw_stage.AccountObject
