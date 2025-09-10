726a90d2870ca8107ef9c69ef97aa49986a22ea8
Alberto Almario <alberto.almario@vault.insurance>
2025-09-10 15:53:31 -0500

    Configure connections to new database vault_ai.docuclaims

--- AI/app/services/snowflake_service.py
+++ AI/app/services/snowflake_service.py
@@ -10,28 +10,15 @@ def get_sf_conn(config):
     warehouse = config.get("SF_WAREHOUSE", "default_warehouse")
     database = config.get("SF_DATABASE", "default_database")
     schema = config.get("SF_SCHEMA", "default_schema")
-    use_sso = config.get("USE_SSO", False)
 
-    conn = None
-    if use_sso:
-        conn = snowflake.connector.connect(
-            account=account,
-            user=user,
-            role=role,
-            authenticator="externalbrowser",
-            warehouse=warehouse,
-            database=database,
-            schema=schema
-        )
-    else:
-        conn = snowflake.connector.connect(
-            account=account,
-            user=user,
-            password=pat,
-            role=role,
-            warehouse=warehouse,
-            database=database,
-            schema=schema
-        )
+    conn = snowflake.connector.connect(
+        account=account,
+        user=user,
+        password=pat,
+        role=role,
+        warehouse=warehouse,
+        database=database,
+        schema=schema
+    )
     logger.info(f"Snowflake connection established for user '{user}' on account '{account}' with role '{role}'.")
     return conn
