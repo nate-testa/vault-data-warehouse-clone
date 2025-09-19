a990831319eb831f8d80873d4a8abeee81c6f3a7
dinesh.bobbili <dinesh.bobbili@vault.insurance>
2025-09-19 12:10:00 -0400

    formatted

--- SQL/DML/208_Insert_tvalidation_sql.sql
+++ SQL/DML/208_Insert_tvalidation_sql.sql
@@ -1,15 +1,5 @@
 INSERT INTO edw_core.tvalidation_sql (validation_sql_desc , source_sql , target_sql , active_in , frequency_desc , create_ts , update_ts)
 SELECT
-'' ,
-       '' AS source_sql ,
-       'select 0' AS target_sql ,
-       'Y' AS active_in ,
-       'Daily' AS frequency_desc ,
-       getdate() AS create_ts ,
-       getdate() AS update_ts;
-
-INSERT INTO edw_core.tvalidation_sql (validation_sql_desc , source_sql , target_sql , active_in , frequency_desc , create_ts , update_ts)
-SELECT
 'HONK : VIN > 17 characters' ,
        'select count(*)
 from edw_core.tdaily_inforce_policy dip 
@@ -71,7 +61,6 @@ from edw_core.tdaily_inforce_policy dip
        'Daily' AS frequency_desc ,
        getdate() AS create_ts ,
        getdate() AS update_ts;
-
 
 
 INSERT INTO edw_core.tvalidation_sql (validation_sql_desc , source_sql , target_sql , active_in , frequency_desc , create_ts , update_ts)
