37c575a4ee1ccad6fe8c0d86534f690fcdf226af
Yunus Mohammed <yunus.mohammed@vault.insurance>
2025-09-23 11:42:08 -0400

    Column name updated

--- SQL/DDL/732_Alter_Table_tclaim.sql
+++ SQL/DDL/732_Alter_Table_tclaim.sql
@@ -3,8 +3,8 @@ SELECT 1
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA='edw_core'
 AND TABLE_NAME = 'tclaim'
-AND COLUMN_NAME = 'facts_of_loss'
+AND COLUMN_NAME = 'loss_location_desc'
 )
 BEGIN 
-    ALTER TABLE edw_core.tclaim ADD facts_of_loss nvarchar(max) NULL
+    ALTER TABLE edw_core.tclaim ADD loss_location_desc nvarchar(max) NULL
 END ;
