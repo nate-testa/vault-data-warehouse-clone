2bf836ba044715e26b80420ced9977db320ebc34
Rushin Shah <rushin.shah@vault.insurance>
2025-09-29 12:18:28 -0400

    Hubspot Customer Feed Field Additions

--- SQL/DDL/738_Alter_Table_customer_hubspot_feed.sql
+++ SQL/DDL/738_Alter_Table_customer_hubspot_feed.sql
@@ -27,8 +27,8 @@ SELECT 1
 FROM INFORMATION_SCHEMA.COLUMNS					
 WHERE TABLE_SCHEMA='edw_integration'					
 AND TABLE_NAME = 'customer_hubspot_feed'					
-AND COLUMN_NAME = 'inforce_in'					
-) BEGIN ALTER TABLE edw_integration.customer_hubspot_feed ADD inforce_in varchar(255) END
+AND COLUMN_NAME = 'policy_inforce_in'					
+) BEGIN ALTER TABLE edw_integration.customer_hubspot_feed ADD policy_inforce_in varchar(255) END
 
 IF NOT EXISTS (					
 SELECT 1					
