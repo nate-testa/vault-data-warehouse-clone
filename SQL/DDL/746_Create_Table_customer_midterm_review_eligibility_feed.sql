bf7b523759ecf19f90a197c3501c6e813247faee
dinesh.bobbili <dinesh.bobbili@vault.insurance>
2025-10-07 12:35:41 -0400

    test ccommit

--- SQL/DDL/746_Create_Table_customer_midterm_review_eligibility_feed.sql
+++ SQL/DDL/746_Create_Table_customer_midterm_review_eligibility_feed.sql
@@ -6,7 +6,7 @@
 		customer_id varchar(50),
 		midterm_review_year int,
 		midterm_review_process_in varchar(255),
-		reason_desc varchar(255),
+		reason_desc varchar(255) ,
         data nvarchar(max) null,
 		create_ts datetime2(7),
 		update_ts datetime2(7),
