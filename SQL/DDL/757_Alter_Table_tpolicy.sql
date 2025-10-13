35a408ba0ef5efe4d5c31fbc4d9c77df3ffb03e3
Yunus Mohammed <yunus.mohammed@vault.insurance>
2025-10-13 09:35:51 -0400

    name updated

--- SQL/DDL/757_Alter_Table_tpolicy.sql
+++ SQL/DDL/757_Alter_Table_tpolicy.sql
@@ -3,8 +3,8 @@ SELECT 1
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA='edw_core'
 AND TABLE_NAME = 'tpolicy'
-AND COLUMN_NAME = 'bor_change_In'
+AND COLUMN_NAME = 'bor_change_in'
 ) 
 BEGIN 
-    ALTER TABLE edw_core.tpolicy ADD bor_change_In VARCHAR(255) NULL
+    ALTER TABLE edw_core.tpolicy ADD bor_change_in VARCHAR(255) NULL
 END ;
