f3c3402115149e907d96be5523519ed31a35aa3f
dinesh.bobbili <dinesh.bobbili@vault.insurance>
2025-11-10 12:48:14 -0500

    NFP-sp_titem_summary- initial dev

--- SQL/Stored Procedures/sp_titem_summary.sql
+++ SQL/Stored Procedures/sp_titem_summary.sql
@@ -25,10 +25,12 @@ GO
 -- 07/03/24		Yunus Mohammed					13. Added policy_history_sk
 -- 07/18/24		Architha Gudimalla				14. Updated logic for @last_source_extract_ts
 -- 07/08/25		Architha Gudimalla				15. Updated EP logic 
+-- 11/10/25		Dinesh Bobbili					16. AD11639 - Added source_system_sk filter for NFP process
 -- ==================================================================================================================================================== 
 
 CREATE OR ALTER PROCEDURE [edw_core].[sp_titem_summary]
-@in_month_end_dt date = null
+@in_month_end_dt date = null,
+@in_source_system VARCHAR(10) = null
 AS 
 BEGIN
     -- SET NOCOUNT ON added to prevent extra result sets from
@@ -62,6 +64,9 @@ BEGIN
 		DECLARE @proc_run_month_end_dt date
 		DECLARE @proc_run_month INT
 		DECLARE @parameter_desc VARCHAR(255)
+		
+		DECLARE @param_ssk VARCHAR(50)
+		select @param_ssk=source_system_sk from edw_core.tsource_system where source_system_nm = @in_source_system;
 		
 		DECLARE c1_rec CURSOR
 		FOR  
@@ -130,6 +135,7 @@ BEGIN
 				 and   transaction_effective_dt_sk <> expiration_dt_sk
 				 and	calendar_month_sk = @month_end_dt_sk 
 				and   expiration_dt_sk > @month_begin_dt_sk
+				and source_system_sk = isnull(@param_ssk, source_system_sk)
 				 group by policy_sk, item_sk, policy_transaction_type_sk, transaction_seq_no
 				 union all
 				 SELECT policy_sk, item_sk, policy_transaction_type_sk , transaction_seq_no 
@@ -139,6 +145,7 @@ BEGIN
 				 and   transaction_effective_dt_sk <> expiration_dt_sk
 				 and	calendar_month_sk = @month_end_dt_sk 
 				and   expiration_dt_sk > @month_begin_dt_sk
+				and source_system_sk = isnull(@param_ssk, source_system_sk)
 				 group by policy_sk, item_sk, policy_transaction_type_sk, transaction_seq_no ; 
 				 
 				DROP TABLE IF EXISTS edw_temp.titem_summary_max_tr;
@@ -163,6 +170,7 @@ BEGIN
 				and   transaction_effective_dt_sk <= @end_dt_sk
 				and   transaction_dt_sk <= @end_dt_sk 
 				and   expiration_dt_sk > @month_begin_dt_sk
+				and source_system_sk = isnull(@param_ssk, source_system_sk)
 				group by policy_sk, item_sk;
 
 				--remove the policy if the policy is issued and cancelled multiple times and the last seq no is not a rein or cancel, no need to adjust exposures
@@ -485,6 +493,7 @@ BEGIN
 				 and   tr.transaction_effective_dt_sk <> tr.expiration_dt_sk
 				 and   (pol.expiration_dt > @month_begin_dt --or (tr.transaction_dt_sk - tr.expiration_dt_sk) <= 60
 						) --dateadd(month,-2,@month_begin_dt)
+				 and tr.source_system_sk = isnull(@param_ssk, tr.source_system_sk)
 				 group by tr.policy_sk, tr.item_sk, tr.product_sk--, tr.customer_sk, tr.broker_sk, tr.product_sk, pol.source_system_sk  
 				)
 				INSERT INTO edw_core.titem_summary
