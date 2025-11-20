310cdc0ea9b02fb83aed1dd355536f26d341a64c
dinesh.bobbili <dinesh.bobbili@vault.insurance>
2025-11-20 11:51:09 -0500

    Update logic and changed insert to merge

--- SQL/Stored Procedures/sp_producer_hubspot_feed.sql
+++ SQL/Stored Procedures/sp_producer_hubspot_feed.sql
@@ -6,6 +6,7 @@
 ---------------------------------------------------------------------------------------------------
 -- 07/17/24		Hernando Gonzalez			1. Created this procedure 
 -- 07/26/24		Hernando Gonzalez			2. Updated logic for @last_source_extract_ts
+-- 11/20/25		Dinesh Bobbili				3. Update logic and changed insert to merge
 -- ================================================================================================= 
 
 CREATE OR ALTER PROCEDURE [edw_core].[sp_producer_hubspot_feed]
@@ -55,39 +56,86 @@ BEGIN
 		FROM edw_core.tproducer p	
 		INNER JOIN edw_core.tbroker br
 			ON p.broker_sk = br.broker_sk
-		and greatest(p.create_ts, p.update_ts) > @last_source_extract_ts;
+		and greatest(p.create_ts, p.update_ts) > @last_source_extract_ts
+		and not exists (select 1 from edw_integration.producer_hubspot_feed f where p.producer_id = f.producer_id)
+		UNION ALL 
+		SELECT
+			p.broker_id,
+			p.producer_id,
+			p.email,
+			p.first_nm,
+			p.last_nm,
+			p.phone_no,
+			br.broker_status,
+			p.title,
+			p.producer_role [producer_role],
+			p.producer_status,
+			getdate() as create_ts,
+			getdate() as update_ts,
+			@etl_audit_sk AS etl_audit_sk
+		FROM edw_core.tproducer p	
+		INNER JOIN edw_core.tbroker br
+			ON p.broker_sk = br.broker_sk
+		and greatest(p.create_ts, p.update_ts) > @last_source_extract_ts
+		and exists (select 1 from edw_integration.producer_hubspot_feed f where p.producer_id = f.producer_id
+		and (ISNULL(p.broker_id,'')        <> ISNULL(f.broker_id,'')
+			OR ISNULL(p.email,'')            <> ISNULL(f.email,'')
+			OR ISNULL(p.first_nm,'')         <> ISNULL(f.first_nm,'')
+			OR ISNULL(p.last_nm,'')          <> ISNULL(f.last_nm,'')
+			OR ISNULL(p.phone_no,'')         <> ISNULL(f.phone_no,'')
+			OR ISNULL(br.broker_status,'')    <> ISNULL(f.broker_status,'')
+			OR ISNULL(p.title,'')            <> ISNULL(f.title,'')
+			OR ISNULL(p.producer_role,'')    <> ISNULL(f.producer_role,'')
+			OR ISNULL(p.producer_status,'')  <> ISNULL(f.producer_status,'')))
+		;
 
-        -- Start Insert process
-        INSERT INTO [edw_integration].[producer_hubspot_feed](
-            [broker_id]
-    		,[producer_id]
-    		,[email]
-    		,[first_nm]
-    		,[last_nm]
-    		,[phone_no]
-    		,[broker_status]
-    		,[title]
-    		,[producer_role]
-    		,[producer_status]
-            ,[create_ts]
-            ,[update_ts]
-            ,[etl_audit_sk]
-        )
-        SELECT 
-            [broker_id],
-			[producer_id],
-			[email],
-			[first_nm],
-			[last_nm],
-			[phone_no],
-			[broker_status],
-			[title],
-			[producer_role],
-			[producer_status],
-            [create_ts],
-            [update_ts],
-            [etl_audit_sk]
-        FROM [edw_temp].[producer_hubspot_feed_temp1];
+        -- Start Merge process
+        MERGE edw_integration.producer_hubspot_feed AS Target
+		USING edw_temp.producer_hubspot_feed_temp1 AS Source
+			ON Target.producer_id = Source.producer_id
+		WHEN NOT MATCHED BY TARGET
+		THEN INSERT (
+				broker_id,
+				producer_id,
+				email,
+				first_nm,
+				last_nm,
+				phone_no,
+				broker_status,
+				title,
+				producer_role,
+				producer_status,
+				create_ts,
+				update_ts,
+				etl_audit_sk
+			)
+			VALUES (
+				Source.broker_id,
+				Source.producer_id,
+				Source.email,
+				Source.first_nm,
+				Source.last_nm,
+				Source.phone_no,
+				Source.broker_status,
+				Source.title,
+				Source.producer_role,
+				Source.producer_status,
+				Source.create_ts,
+				Source.update_ts,
+				Source.etl_audit_sk
+			)
+		WHEN MATCHED
+		THEN UPDATE SET
+				Target.broker_id       = Source.broker_id,
+				Target.email           = Source.email,
+				Target.first_nm        = Source.first_nm,
+				Target.last_nm         = Source.last_nm,
+				Target.phone_no        = Source.phone_no,
+				Target.broker_status   = Source.broker_status,
+				Target.title           = Source.title,
+				Target.producer_role   = Source.producer_role,
+				Target.producer_status = Source.producer_status,
+				Target.update_ts       = Source.update_ts;
         --************End************
 
 		SET @rows_affected=@@ROWCOUNT;
