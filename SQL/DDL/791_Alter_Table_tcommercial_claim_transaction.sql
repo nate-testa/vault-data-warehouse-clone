f508135b20bd4165e010e2bdf0e0936ef1382c92
Yunus Mohammed <yunus.mohammed@vault.insurance>
2025-11-24 13:41:27 -0500

    Removed tclaim_transaction's ddl

--- SQL/DDL/791_Alter_Table_tcommercial_claim_transaction.sql
+++
@@ -1,9 +0,0 @@
-IF NOT EXISTS (SELECT *
-        FROM INFORMATION_SCHEMA.COLUMNS
-        WHERE
-        TABLE_SCHEMA = 'edw_commercial'
-        AND TABLE_NAME = 'tcommercial_claim_transaction'
-        AND COLUMN_NAME = 'subrogation_expense_recovery_amt')
-BEGIN
-    ALTER TABLE edw_commercial.tcommercial_claim_transaction ADD subrogation_expense_recovery_amt DECIMAL(15,2) NULL;
-END;
