0eb7927f4f56ea08521e5760405a7b4226a5f872
Hernando Gonzalez Garcia <Hernando.Gonzalez.Garcia@Vault.Insurance>
2025-11-28 09:50:24 -0500

    New field for account stage

---
+++ SQL/DDL/796_Alter_Table_STAGE_Account.sql
@@ -0,0 +1,10 @@
+IF EXISTS (
+SELECT 1
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA='edw_stage'
+AND TABLE_NAME = 'Account'
+AND COLUMN_NAME = 'RenewalReviewStartedDate'
+) 
+BEGIN 
+    ALTER TABLE edw_stage.Account ADD RenewalReviewStartedDate datetime2(7) NULL
+END
