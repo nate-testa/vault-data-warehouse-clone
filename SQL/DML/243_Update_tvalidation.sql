833b4d598c697484a577733c6ecbf65d9d0205f4
Alberto Almario <alberto.almario@vault.insurance>
2025-12-03 16:56:33 -0500

    remove filter by sk and filter claimreportstatus = A only.

--- SQL/DML/243_Update_tvalidation.sql
+++ SQL/DML/243_Update_tvalidation.sql
@@ -1,8 +1,7 @@
 update edw_core.tvalidation_sql
 set
 	source_sql = 
-	'SELECT count(*) FROM edw_integration.claim_clue_auto_feed WHERE LTRIM(RTRIM(PolicyHolderMailAddressStreetName)) = '''' OR LTRIM(RTRIM(PolicyHolderMailAddressCity)) = '''' OR LTRIM(RTRIM(PolicyHolderMailAddressState)) = '''' OR LTRIM(RTRIM(PolicyHolderMailAddressZip)) = '''' OR    LTRIM(RTRIM(policyHolderNameFirst)) = '''' OR     LTRIM(RTRIM(policyHolderNameLast)) = '''' OR LTRIM(RTRIM(claimReportingStatus)) = '''' OR LTRIM(RTRIM(claimAmount)) = '''' OR (LTRIM(RTRIM(claimtype)) = '''' AND LTRIM(RTRIM(claimReportingStatus)) <> ''R'') OR LTRIM(RTRIM(policyNumber)) = '''' OR LTRIM(RTRIM(policyType)) = '''' OR LTRIM(RTRIM(contribCompany)) = '''' OR LTRIM(RTRIM(claimNumber)) = '''' OR claimAmount LIKE ''%-%'''
+	'SELECT count(*) FROM edw_integration.claim_clue_auto_feed WHERE LTRIM(RTRIM(PolicyHolderMailAddressStreetName)) = '''' OR LTRIM(RTRIM(PolicyHolderMailAddressCity)) = '''' OR LTRIM(RTRIM(PolicyHolderMailAddressState)) = '''' OR LTRIM(RTRIM(PolicyHolderMailAddressZip)) = '''' OR    LTRIM(RTRIM(policyHolderNameFirst)) = '''' OR     LTRIM(RTRIM(policyHolderNameLast)) = '''' OR LTRIM(RTRIM(claimReportingStatus)) = '''' OR LTRIM(RTRIM(claimAmount)) = '''' OR (LTRIM(RTRIM(claimtype)) = '''' AND LTRIM(RTRIM(claimReportingStatus)) = ''A'') OR LTRIM(RTRIM(policyNumber)) = '''' OR LTRIM(RTRIM(policyType)) = '''' OR LTRIM(RTRIM(contribCompany)) = '''' OR LTRIM(RTRIM(claimNumber)) = '''' OR claimAmount LIKE ''%-%'''
 	,update_ts = GETDATE()
 where validation_sql_desc = 'Clue Auto - null values'
-and validation_sql_sk = 102
 go
