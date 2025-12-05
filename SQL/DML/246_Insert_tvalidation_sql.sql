b230b6f9b5e7b6d8365b93764c10bb4764a21809
dinesh.bobbili <dinesh.bobbili@vault.insurance>
2025-12-05 13:12:19 -0500

    updated

--- SQL/DML/246_Insert_tvalidation_sql.sql
+++ SQL/DML/246_Insert_tvalidation_sql.sql
@@ -3,7 +3,8 @@ SELECT
 'tproducer- Duplicate email for Active and pending producer' ,
 'select count(*) from edw_core.tproducer  a  
 where exists (select  email from edw_core.tproducer b where   a.email = b.email group by email having count(*) > 1 )
-and email in (select email from edw_core.tproducer where producer_status = ''Active'')'  AS source_sql ,
+and email in (select email from edw_core.tproducer where producer_status = ''Active'' )
+and email in (select email from edw_core.tproducer where producer_status = ''Pending'' )'  AS source_sql ,
        'select  0' AS target_sql ,
        'Y' AS active_in ,
        'Daily' AS frequency_desc ,
