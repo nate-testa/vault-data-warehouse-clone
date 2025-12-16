c20b123f7d19a1474cc9be1644b5a1768342e1e9
Architha Gudimalla <architha.gudimalla@vault.insurance>
2025-12-16 18:09:02 -0500

    updated

--- SQL/DDL/803_Alter_Table_trenewal_summary.sql
+++ SQL/DDL/803_Alter_Table_trenewal_summary.sql
@@ -3,6 +3,14 @@ SELECT 1
 FROM INFORMATION_SCHEMA.COLUMNS					
 WHERE TABLE_SCHEMA='edw_core'					
 AND TABLE_NAME = 'trenewal_summary'					
+AND COLUMN_NAME = 'prior_issued_ct'					
+) BEGIN ALTER TABLE edw_core.trenewal_summary ADD prior_issued_ct int END ; 
+
+IF NOT EXISTS (					
+SELECT 1					
+FROM INFORMATION_SCHEMA.COLUMNS					
+WHERE TABLE_SCHEMA='edw_core'					
+AND TABLE_NAME = 'trenewal_summary'					
 AND COLUMN_NAME = 'accepted_renewal_ct'					
 ) BEGIN ALTER TABLE edw_core.trenewal_summary ADD accepted_renewal_ct int END ; 
 
@@ -21,6 +29,22 @@ WHERE TABLE_SCHEMA='edw_core'
 AND TABLE_NAME = 'trenewal_summary'					
 AND COLUMN_NAME = 'outstanding_renewal_ct'					
 ) BEGIN ALTER TABLE edw_core.trenewal_summary ADD outstanding_renewal_ct int END ; 
+
+IF NOT EXISTS (					
+SELECT 1					
+FROM INFORMATION_SCHEMA.COLUMNS					
+WHERE TABLE_SCHEMA='edw_core'					
+AND TABLE_NAME = 'trenewal_summary'					
+AND COLUMN_NAME = 'in_progress_renewal_ct'					
+) BEGIN ALTER TABLE edw_core.trenewal_summary ADD in_progress_renewal_ct int END ; 
+
+IF NOT EXISTS (					
+SELECT 1					
+FROM INFORMATION_SCHEMA.COLUMNS					
+WHERE TABLE_SCHEMA='edw_core'					
+AND TABLE_NAME = 'trenewal_summary'					
+AND COLUMN_NAME = 'closed_with_no_offer_renewal_ct'					
+) BEGIN ALTER TABLE edw_core.trenewal_summary ADD closed_with_no_offer_renewal_ct int END ; 
 
 IF NOT EXISTS (					
 SELECT 1					
