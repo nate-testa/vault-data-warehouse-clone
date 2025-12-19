78c51a449935879581b27fdf23e3785ddcdd8082
dinesh.bobbili <dinesh.bobbili@vault.insurance>
2025-12-19 07:37:36 -0500

    updated

--- SQL/DML/252_Insert_tvalidation.sql
+++ SQL/DML/252_Insert_tvalidation.sql
@@ -1,16 +1,19 @@
 INSERT INTO edw_core.tvalidation_sql (validation_sql_desc , source_sql , target_sql , active_in , frequency_desc , create_ts , update_ts)
 SELECT
-'Home location address having greater than 96 characters' ,
+'thome_location - Home location address having greater than 96 characters' ,
 'SELECT count(*)
 FROM edw_core.thome_location hl
 WHERE LEN(
-        ISNULL(address_line_1, '''') +
-        ISNULL(address_line_2, '''') +
-        ISNULL(unit_no, '''') +
-        ISNULL(city_nm, '''') +
-        ISNULL(state_cd, '''') +
-        ISNULL(zip_cd, '''')
-      ) > 96'  AS source_sql ,
+replace(
+        CONCAT(
+            isnull(       hl.address_line_1, ''''),
+            isnull('' ''  + hl.address_line_2, ''''),
+            isnull('' ''  + hl.unit_no, ''''),
+            isnull('', '' + hl.city_nm, ''''),
+            isnull('', '' + hl.state_cd, ''''),
+            isnull('' ''  + hl.zip_cd, '''')
+         ),''  '',''''
+)) > 96'  AS source_sql ,
        'select  0' AS target_sql ,
        'Y' AS active_in ,
        'Daily' AS frequency_desc ,
