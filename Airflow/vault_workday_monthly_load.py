510eacf7f756c3b34c2283164ca101918fccd5aa
Alberto Almario <alberto.almario@vault.insurance>
2026-01-06 09:56:47 -0500

    add new task tvalidation_result_email

--- Airflow/vault_workday_monthly_load.py
+++ Airflow/vault_workday_monthly_load.py
@@ -2,14 +2,41 @@ import pendulum
 from datetime import timedelta
 from airflow import DAG
 from airflow.utils.task_group import TaskGroup
-from airflow.providers.microsoft.mssql.operators.mssql import MsSqlOperator
+from airflow.operators.python import PythonOperator
 from airflow.operators.email import EmailOperator
-from airflow.operators.dummy import DummyOperator
-from vault_edw_HTML_format import get_sp_success_data_HTML, get_sp_error_data_HTML, get_HTML_on_vault_format
+from airflow.operators.empty import EmptyOperator
+from airflow.providers.microsoft.mssql.operators.mssql import MsSqlOperator
+from airflow.providers.microsoft.mssql.hooks.mssql import MsSqlHook
+from vault_edw_HTML_format import get_sp_success_data_HTML, get_sp_error_data_HTML, get_HTML_on_vault_format, get_vault_data_HTML
 
 to_email = "itdatateam@vault.insurance"
 # to_email = "hernando.gonzalez.garcia@vault.insurance, alberto.valbuena@vault.insurance"
 cc_email = ""
+
+
+def check_tvalidation_result_and_send_email(**kwargs):
+    sql_qry = """
+                select 
+                    vr.validation_result_sk, vr.validation_sql_sk, vr.process_run_start_ts, vr.process_run_end_ts, 
+                    vs.validation_sql_desc, vr.source_value, vr.target_value, vr.status_desc
+                from edw_core.tvalidation_result as vr
+                inner join edw_core.tvalidation_sql as vs on vr.validation_sql_sk = vs.validation_sql_sk
+                where vs.frequency_desc = 'Monthly'
+                and vr.status_desc = 'Failure'
+                and cast(vr.process_run_start_ts as date) = cast(getdate() as date)
+                order by vr.process_run_start_ts desc
+              """
+    mssql_hook = MsSqlHook(mssql_conn_id='Vault_EDW')
+    result = mssql_hook.get_first(sql_qry)
+    if result is not None:
+        EmailOperator(
+            task_id='send_email_tvalidation_result',
+            to=to_email,
+            subject='Airflow - Report - Workday Validation Errors',
+            html_content=get_vault_data_HTML(sql_qry,'There are validation errors. Please review the details below.'),
+            dag=kwargs['dag'],
+        ).execute(context=kwargs)
+
 
 def on_failure_callback(context):
 
@@ -58,7 +85,7 @@ with DAG(
 ) as dag:
     
 
-    start = DummyOperator(
+    start = EmptyOperator(
         task_id='start',
     )
 
@@ -163,6 +190,13 @@ with DAG(
             autocommit=True,
         )
 
+        tvalidation_result_email = PythonOperator(
+            task_id='tvalidation_result_email',
+            python_callable=check_tvalidation_result_and_send_email,
+            provide_context=True,
+            dag=dag,
+        )
+
         send_workday_email = EmailOperator(
             task_id='send_workday_email',
             to=to_email,
@@ -170,11 +204,11 @@ with DAG(
             html_content=get_sp_success_data_HTML(vault_workday_monthly_load_group_items, 'All stored procedures executed successfully for all the Workday tables'),
         )
 
-        sp_policy_workday_unearned_premium_feed >> sp_policy_workday_written_premium_feed >> sp_policy_workday_ceded_premium_feed >> sp_claim_workday_payment >> sp_claim_workday_reserve_feed >> sp_claim_workday_reserve_feed_itd >> sp_claim_litigation_workday_payment >> sp_claim_litigation_workday_reserve_feed >> sp_claim_litigation_workday_reserve_feed_itd >> sp_tvalidation_result >> send_workday_email
+        sp_policy_workday_unearned_premium_feed >> sp_policy_workday_written_premium_feed >> sp_policy_workday_ceded_premium_feed >> sp_claim_workday_payment >> sp_claim_workday_reserve_feed >> sp_claim_workday_reserve_feed_itd >> sp_claim_litigation_workday_payment >> sp_claim_litigation_workday_reserve_feed >> sp_claim_litigation_workday_reserve_feed_itd >> sp_tvalidation_result >> tvalidation_result_email >> send_workday_email
 
 
 
-    end = DummyOperator(
+    end = EmptyOperator(
         task_id='end',
     )
 
