467525b9bf89626cbbed4adef479eb0bc5e4acf8
Yunus Mohammed <yunus.mohammed@vault.insurance>
2026-01-07 13:09:23 -0500

    new columns added

---
+++ SQL/DDL/827_Alter_Table_tpolicy.sql
@@ -0,0 +1,21 @@
+IF NOT EXISTS (
+SELECT 1
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA='edw_core'
+AND TABLE_NAME = 'tpolicy'
+AND COLUMN_NAME = 'current_producer_nm'
+) 
+BEGIN
+    ALTER TABLE edw_core.tpolicy ADD current_producer_nm  VARCHAR(255) NULL
+END ; 
+
+IF NOT EXISTS (
+SELECT 1
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA='edw_core'
+AND TABLE_NAME = 'tpolicy'
+AND COLUMN_NAME = 'current_underwriter_nm'
+) 
+BEGIN
+    ALTER TABLE edw_core.tpolicy ADD current_underwriter_nm   VARCHAR(255) NULL
+END ;
