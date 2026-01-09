5074421069dcd8e792acc29e2c4155ab212be8eb
Yunus Mohammed <yunus.mohammed@vault.insurance>
2026-01-09 09:39:14 -0500

    cancellation_sub_reason_desc length increased

---
+++ SQL/DDL/839_Alter_Table_tpolicy_history.sql
@@ -0,0 +1,10 @@
+IF EXISTS (
+SELECT 1
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA='edw_core'
+AND TABLE_NAME = 'tpolicy_history'
+AND COLUMN_NAME = 'cancellation_sub_reason_desc'
+) 
+BEGIN
+    ALTER TABLE edw_core.tpolicy_history ALTER COLUMN cancellation_sub_reason_desc  VARCHAR(2000) NULL
+END ; 
