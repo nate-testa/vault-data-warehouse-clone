3d23ded3725e0c48aa08fd171b8cead82cf7eaf5
Rushin Shah <rushin.shah@vault.insurance>
2026-01-30 10:53:39 -0500

    Broker Commission Email API

--- SQL/DDL/861_Create_Table_broker_commission_email_api.sql
+++ SQL/DDL/861_Create_Table_broker_commission_email_api.sql
@@ -1,4 +1,6 @@
 -- Broker commission statement email and address information 
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[edw_integration].[broker_commission_email_api]') AND type in (N'U'))
+BEGIN
 CREATE TABLE edw_integration.broker_commission_email_api 
 ( 
 commission_statement_email varchar(255), 
@@ -6,11 +8,13 @@ agency_code  varchar(255),
 agency_name varchar(255), 
 agency_city varchar(255),
 agency_state varchar(255), 
-create_ts datetime ,
-update_ts datetime ,
+create_ts datetime2(7),
+update_ts datetime2(7) ,
 etl_audit_sk int,
 CONSTRAINT pk_broker_commission_email_api PRIMARY KEY(agency_code,commission_statement_email)
 );
+END;
 
 INSERT INTO	edw_integration.tintegration_table_detail(table_nm,	table_type,	table_desc,	load_method, load_type,	load_frequency,	create_ts,update_ts)
 VALUES ('broker_commission_email_api','API','This table provides broker commission statement email and address information in an API for billing team','Stored Procedure','Insert','Daily',getdate(),getdate());
+
