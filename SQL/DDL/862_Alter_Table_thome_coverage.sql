2a1db97369aeb627e5b9760a1bc43dfb5a0370f5
Tuba Mohsin <tuba.mohsin@vault.insurance>
2026-01-30 12:04:01 -0500

    updated

--- SQL/DDL/862_Alter_Table_thome_coverage.sql
+++ SQL/DDL/862_Alter_Table_thome_coverage.sql
@@ -3,9 +3,21 @@
     FROM INFORMATION_SCHEMA.COLUMNS
     WHERE TABLE_SCHEMA = 'edw_core'
       AND TABLE_NAME = 'thome_coverage'
-      AND COLUMN_NAME = 'high_risk_wui_property_in '
+      AND COLUMN_NAME = 'high_risk_wui_property_in'
 )
 BEGIN
     ALTER TABLE edw_core.thome_coverage
     ADD high_risk_wui_property_in  VARCHAR(255) NULL;
+END;
+
+ IF NOT EXISTS (
+    SELECT 1
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_SCHEMA = 'edw_core'
+      AND TABLE_NAME = 'thome_coverage'
+      AND COLUMN_NAME = 'effective_built_year'
+)
+BEGIN
+    ALTER TABLE edw_core.thome_coverage
+    ADD effective_built_year  VARCHAR(255) NULL;
 END;
