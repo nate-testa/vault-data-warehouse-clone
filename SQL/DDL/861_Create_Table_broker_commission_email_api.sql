15b07e439316256ed5560a727e44f85058d919a4
Rushin Shah <rushin.shah@vault.insurance>
2026-02-02 10:38:57 -0500

    Updated DDL

--- SQL/DDL/861_Create_Table_broker_commission_email_api.sql
+++ SQL/DDL/861_Create_Table_broker_commission_email_api.sql
@@ -3,15 +3,15 @@ IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[edw_inte
 BEGIN
 CREATE TABLE edw_integration.broker_commission_email_api 
 ( 
-broker_code  varchar(255),
-broker_name varchar(255), 
-broker_city varchar(255),
-broker_state varchar(255), 
-commission_statement_email varchar(255), 
+broker_id  varchar(255),
+broker_nm varchar(255), 
+broker_city_nm varchar(255),
+broker_state_cd varchar(255), 
+commission_statement_email varchar(2000), 
 create_ts datetime2(7),
 update_ts datetime2(7) ,
 etl_audit_sk int,
-CONSTRAINT pk_broker_commission_email_api PRIMARY KEY(broker_code,commission_statement_email)
+CONSTRAINT pk_broker_commission_email_api PRIMARY KEY(broker_id,commission_statement_email)
 );
 END;
 
