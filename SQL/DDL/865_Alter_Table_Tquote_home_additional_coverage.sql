e35d301f48b611170a1874ec9386403fc83827f8
Architha Gudimalla <architha.gudimalla@vault.insurance>
2026-02-03 18:54:11 -0500

    updated

---
+++ SQL/DDL/865_Alter_Table_Tquote_home_additional_coverage.sql
@@ -0,0 +1,11 @@
+ IF NOT EXISTS (
+    SELECT 1
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_SCHEMA = 'edw_core'
+      AND TABLE_NAME = 'tquote_home_additional_coverage'
+      AND COLUMN_NAME = 'theft_or_loss_general_conditions_approval_in'
+)
+BEGIN
+    ALTER TABLE edw_core.tquote_home_additional_coverage
+    ADD theft_or_loss_general_conditions_approval_in VARCHAR(255) NULL;
+END;
