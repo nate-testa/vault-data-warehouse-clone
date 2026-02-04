94a2cd33aa684e12a9310509ca3a68561e718d9a
Rushin Shah <rushin.shah@vault.insurance>
2026-02-04 15:56:03 -0500

    DDL - tbroker.california_dba_nm

---
+++ SQL/DDL/822_Alter_Table_tbroker.sql
@@ -0,0 +1,10 @@
+IF NOT EXISTS (
+    SELECT 1
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_SCHEMA = 'edw_core'
+      AND TABLE_NAME = 'tbroker'
+      AND COLUMN_NAME = 'california_dba_nm'
+)
+BEGIN
+    ALTER TABLE edw_core.tbroker ADD california_dba_nm VARCHAR(256) NULL;
+END;
