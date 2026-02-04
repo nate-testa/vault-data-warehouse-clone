5ec43c8189d68a15721d0b8e83523bc9fd22990e
dinesh.bobbili <dinesh.bobbili@vault.insurance>
2026-02-04 14:01:42 -0500

    updated

--- SQL/DDL/881_Alter_Table_customer_hubspot_feed.sql
+++ SQL/DDL/881_Alter_Table_customer_hubspot_feed.sql
@@ -3,7 +3,7 @@ IF NOT EXISTS (
     FROM INFORMATION_SCHEMA.COLUMNS					
     WHERE TABLE_SCHEMA='edw_integration'					
       AND TABLE_NAME = 'customer_hubspot_feed'					
-      AND COLUMN_NAME = 'inforce_net_premium_amt'					
+      AND COLUMN_NAME = 'total_policy_premium_amt'					
 ) 
 BEGIN 
-ALTER TABLE edw_integration.customer_hubspot_feed ADD inforce_net_premium_amt int END ;
+ALTER TABLE edw_integration.customer_hubspot_feed ADD total_policy_premium_amt int END ;
