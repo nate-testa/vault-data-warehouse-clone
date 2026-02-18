9704c20e9b092596642649c4e9955558bf009d36
Yunus Mohammed <yunus.mohammed@vault.insurance>
2026-02-18 13:39:44 -0500

    Colum  name updated

--- SQL/DML/266_Insert_coverage_mapping_snapsheet.sql
+++ SQL/DML/266_Insert_coverage_mapping_snapsheet.sql
@@ -4,6 +4,6 @@ product_nm,table_nm,column_nm,snapsheet_coverage_nm,snapsheet_coverage_cd,covera
 )
 select 'Group Personal Excess Liability' as product_nm,	'tgrpel_coverage' as table_nm,'employment_practises_liability_limit_amt' as column_nm,'Employment Practices Liability' as snapsheet_coverage_nm,'EMPL' as snapsheet_coverage_nm,'Limit' as coverage_type,NULL as snapsheet_deductible_type,getdate() as create_ts,getdate() as update_ts
 union
-select 'Group Personal Excess Liability' as product_nm,	'tgrpel_coverage' as table_nm,'group_excess_liability_limit_amt' as column_nm,'Excess liability' as snapsheet_coverage_nm,'EXL' as snapsheet_coverage_nm,'Limit' as coverage_type,	NULL as snapsheet_deductible_type,getdate() as create_ts,getdate() as update_ts
+select 'Group Personal Excess Liability' as product_nm,	'tgrpel_coverage' as table_nm,'excess_liability_limit_amt' as column_nm,'Excess liability' as snapsheet_coverage_nm,'EXL' as snapsheet_coverage_nm,'Limit' as coverage_type,	NULL as snapsheet_deductible_type,getdate() as create_ts,getdate() as update_ts
 union
 select 'Group Personal Excess Liability' as product_nm,	'tgrpel_coverage' as table_nm,'uninsured_motorist_liability_limit_amt' as column_nm,'UM/UIM Motorist Liability' as snapsheet_coverage_nm,'EXUMOT' as snapsheet_coverage_nm,'Limit' as coverage_type,NULL as snapsheet_deductible_type,getdate() as create_ts,getdate() as update_ts
