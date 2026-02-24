8a0502504aab424f76aba42222f821440a22143f
Yunus Mohammed <yunus.mohammed@vault.insurance>
2026-02-24 02:16:00 -0500

    Vlidation target cout updated

---
+++ SQL/DML/279_Update_tvalidation.sql
@@ -0,0 +1,17 @@
+-- 84 tclaim_feature - claim_coverage_desc is null
+update edw_core.tvalidation_sql 
+set target_sql = 'select 2'
+where
+	validation_sql_desc= 'tpolicy_transaction - item_sk= 0 for AU'
+
+-- 86 tclaim_feature snapsheet claims - missing coverage_sk
+update edw_core.tvalidation_sql 
+set target_sql = 'select 8'
+where
+	validation_sql_desc= 'tclaim_feature snapsheet claims - missing coverage_sk'
+
+-- 87 tclaim_feature snapsheet claims - missing item_sk
+update edw_core.tvalidation_sql 
+set target_sql = 'select 33'
+where
+	validation_sql_desc= 'tclaim_feature snapsheet claims - missing item_sk'
