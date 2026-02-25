f5a855fde8c3402c438259248f47bd790ce758fc
Yunus Mohammed <yunus.mohammed@vault.insurance>
2026-02-25 05:59:01 -0500

    Added risk_address_country_nm

--- SQL/DDL/902_Alter_Table_tpolicy.sql
+++ SQL/DDL/902_Alter_Table_tpolicy.sql
@@ -63,3 +63,14 @@ AND COLUMN_NAME = 'risk_address_zip_cd'
 BEGIN 
 	ALTER TABLE edw_core.tpolicy ADD risk_address_zip_cd VARCHAR(255) NULL
 END;
+
+IF NOT EXISTS (
+SELECT 1
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA='edw_core'
+AND TABLE_NAME = 'tpolicy'
+AND COLUMN_NAME = 'risk_address_country_nm'
+) 
+BEGIN 
+	ALTER TABLE edw_core.tpolicy ADD risk_address_country_nm VARCHAR(255) NULL
+END;
