45a37b930306c7e6155c4315878d676e09f863ff
Hernando Gonzalez Garcia <Hernando.Gonzalez.Garcia@Vault.Insurance>
2026-02-25 14:44:25 -0500

    add non lux ROL

---
+++ SQL/DDL/912_Alter_Table_thome_coverage.sql
@@ -0,0 +1,10 @@
+IF NOT EXISTS (
+SELECT 1
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA='edw_core'
+AND TABLE_NAME = 'thome_coverage'
+AND COLUMN_NAME = 'rate_on_line_exclude_collection'
+) 
+BEGIN 
+	ALTER TABLE edw_core.thome_coverage ADD rate_on_line_exclude_collection DECIMAL(15,2) NULL
+END; 
