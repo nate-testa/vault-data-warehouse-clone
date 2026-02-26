8695f4673b61db8fcc87631ced7c3ea8dbe87cfa
Yunus Mohammed <yunus.mohammed@vault.insurance>
2026-02-26 06:08:15 -0500

    new update included

--- SQL/DML/279_Update_tvalidation.sql
+++ SQL/DML/279_Update_tvalidation.sql
@@ -15,3 +15,9 @@ update edw_core.tvalidation_sql
 set target_sql = 'select 33'
 where
 	validation_sql_desc= 'tclaim_feature snapsheet claims - missing item_sk'
+
+-- 81 Snapsheet Validation- Cancelled approved reserves/payments
+update edw_core.tvalidation_sql 
+set target_sql = 'select 9'
+where
+	validation_sql_desc= 'Snapsheet Validation- Cancelled approved reserves/payments'
