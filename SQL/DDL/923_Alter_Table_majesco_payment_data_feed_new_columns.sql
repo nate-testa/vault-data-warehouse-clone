1fb13561fe275143efc4ca01391f1d5b1068ae7a
Alberto Almario <alberto.almario@vault.insurance>
2026-03-02 13:59:46 -0500

    add new columns for majesco data feed tables

--- SQL/DDL/923_Alter_Table_majesco_payment_data_feed_new_columns.sql
+++ SQL/DDL/923_Alter_Table_majesco_payment_data_feed_new_columns.sql
@@ -17,3 +17,23 @@ IF NOT EXISTS (
 ) 
 BEGIN 
 ALTER TABLE edw_stage.stage_majesco_payment_data_feed ADD payment_identifier varchar(255) END ;
+
+IF NOT EXISTS (					
+    SELECT 1					
+    FROM INFORMATION_SCHEMA.COLUMNS					
+    WHERE TABLE_SCHEMA='edw_stage'					
+      AND TABLE_NAME = 'stage_majesco_payment_data_feed'					
+      AND COLUMN_NAME = 'datafixindicator_yn'					
+) 
+BEGIN 
+ALTER TABLE edw_stage.stage_majesco_payment_data_feed ADD datafixindicator_yn varchar(255) null END ;
+
+IF NOT EXISTS (					
+    SELECT 1					
+    FROM INFORMATION_SCHEMA.COLUMNS					
+    WHERE TABLE_SCHEMA='edw_stage'					
+      AND TABLE_NAME = 'stage_majesco_payment_data_feed'					
+      AND COLUMN_NAME = 'datafix_date'					
+) 
+BEGIN 
+ALTER TABLE edw_stage.stage_majesco_payment_data_feed ADD datafix_date varchar(255) null END ;
---
+++ SQL/DDL/925_Alter_Table_majesco_installment_data_feed_new_columns.sql
@@ -0,0 +1,19 @@
+IF NOT EXISTS (					
+    SELECT 1					
+    FROM INFORMATION_SCHEMA.COLUMNS					
+    WHERE TABLE_SCHEMA='edw_stage'					
+      AND TABLE_NAME = 'stage_majesco_installment_data_feed'					
+      AND COLUMN_NAME = 'datafixindicator_yn'					
+) 
+BEGIN 
+ALTER TABLE edw_stage.stage_majesco_installment_data_feed ADD datafixindicator_yn varchar(255) null END ;
+
+IF NOT EXISTS (					
+    SELECT 1					
+    FROM INFORMATION_SCHEMA.COLUMNS					
+    WHERE TABLE_SCHEMA='edw_stage'					
+      AND TABLE_NAME = 'stage_majesco_installment_data_feed'					
+      AND COLUMN_NAME = 'datafix_date'					
+) 
+BEGIN 
+ALTER TABLE edw_stage.stage_majesco_installment_data_feed ADD datafix_date varchar(255) null END ;
