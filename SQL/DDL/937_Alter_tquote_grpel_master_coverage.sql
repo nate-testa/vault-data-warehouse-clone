bec194966c2c23219e70d8274ca0552581be853c
Tuba Mohsin <tuba.mohsin@vault.insurance>
2026-03-10 13:36:31 -0400

    Updated

---
+++ SQL/DDL/937_Alter_tquote_grpel_master_coverage.sql
@@ -0,0 +1,8 @@
+    ALTER TABLE edw_core.tquote_grpel_master_coverage drop column enrollment_initial_start_dt;
+    
+    ALTER TABLE edw_core.tquote_grpel_master_coverage drop column enrollment_preiod_in_days;
+    
+    ALTER TABLE edw_core.tquote_grpel_master_coverage drop column enrollment_frequency;
+    
+    ALTER TABLE edw_core.tquote_grpel_master_coverage drop column override_enrollment_to_open_in;
+
