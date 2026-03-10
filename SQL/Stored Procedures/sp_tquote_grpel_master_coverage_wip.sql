c37684b122a1ef5d483d5aa9dca24502f66ae387
Yunus Mohammed <yunus.mohammed@vault.insurance>
2026-03-10 08:41:40 -0400

    WIP proc merge stmt added

--- SQL/Stored Procedures/sp_tquote_grpel_master_coverage_wip.sql
+++ SQL/Stored Procedures/sp_tquote_grpel_master_coverage_wip.sql
@@ -203,38 +203,10 @@ BEGIN
 		)
 		) pivottable
 
-		
-		INSERT INTO [edw_core].[tquote_grpel_master_coverage]
+		MERGE INTO [edw_core].[tquote_grpel_master_coverage_tier] AS Target
+		USING
 		(
-		grpel_master_quote_no,effective_dt,expiration_dt,transaction_seq_no
-		,broker_id,customer_id,product_cd,risk_state_cd,insured_nm,insured_type
-		,mailing_address_line1,mailing_address_line2,mailing_address_city_nm,mailing_address_state_cd
-		,mailing_address_zip_cd,mailing_address_county_nm,mailing_address_country_nm,insured_first_nm,insured_last_nm
-		,mobile_phone_no,email
-		,enrollment_initial_start_dt,enrollment_preiod_in_days,enrollment_frequency,override_enrollment_to_open_in
-		,auto_liability_limit_amt,no_of_average_homes,no_of_average_vehicles,no_of_average_watercraft,no_of_youthful_driver,mvr_trigger_rule
-		,commission_pc,minimum_premium_amt,prior_nfp_policy_no,prior_nfp_policy_expiring_dt
-		,excess_liability_limit_1m_premium_amt,excess_liability_limit_1m_override_premium_amt,excess_liability_limit_3m_premium_amt
-		,excess_liability_limit_3m_override_premium_amt,excess_liability_limit_5m_premium_amt,excess_liability_limit_5m_override_premium_amt
-		,excess_liability_limit_10m_premium_amt,excess_liability_limit_10m_override_premium_amt,excess_liability_limit_15m_premium_amt
-		,excess_liability_limit_15m_override_premium_amt,excess_liability_limit_20m_premium_amt,excess_liability_limit_20m_override_premium_amt
-		,excess_liability_limit_30m_premium_amt,excess_liability_limit_30m_override_premium_amt
-		,uninsured_underinsured_motorist_liability_limit_1m_premium_amt,uninsured_underinsured_motorist_liability_limit_1m_override_premium_amt
-		,uninsured_underinsured_motorist_liability_limit_2m_premium_amt,uninsured_underinsured_motorist_liability_limit_2m_override_premium_amt
-		,uninsured_underinsured_motorist_liability_limit_3m_premium_amt,uninsured_underinsured_motorist_liability_limit_3m_override_premium_amt
-		,uninsured_underinsured_motorist_liability_limit_5m_premium_amt,uninsured_underinsured_motorist_liability_limit_5m_override_premium_amt
-		,uninsured_underinsured_motorist_liability_limit_10m_premium_amt,uninsured_underinsured_motorist_liability_limit_10m_override_premium_amt
-		,employment_practices_liability_limit_250_250_25_premium_amt,employment_practices_liability_limit_250_250_25_override_premium_amt
-		,employment_practices_liability_limit_500_500_50_premium_amt,employment_practices_liability_limit_500_500_50_override_premium_amt
-		,family_trust_management_liability_limit_1m_premium_amt,family_trust_management_liability_limit_1m_override_premium_amt
-		,non_profit_do_liability_limit_1m_premium_amt,non_profit_do_liability_limit_1m_override_premium_amt
-		,non_profit_do_liability_limit_2m_premium_amt,non_profit_do_liability_limit_2m_override_premium_amt
-		,non_profit_do_liability_limit_3m_premium_amt,non_profit_do_liability_limit_3m_override_premium_amt
-		,non_profit_do_liability_limit_4m_premium_amt,non_profit_do_liability_limit_4m_override_premium_amt
-		,non_profit_do_liability_limit_5m_premium_amt,non_profit_do_liability_limit_5m_override_premium_amt
-		,source_system_sk,create_ts,update_ts,etl_audit_sk
-		)
-		select
+			select
 			a.PolicyNumber as grpel_master_quote_no,a.EffectiveDate as effective_dt, a.ExpirationDate as expiration_dt,
 			a.transaction_seq_no,
 			a.broker_id,a.customer_id,a.product_cd,a.risk_state_cd,a.insured_nm, a.insured_type,
@@ -267,6 +239,149 @@ BEGIN
 		from
 			edw_temp.tquote_grpel_master_coverage_wip_temp1 a
 			left join edw_temp.tquote_grpel_master_coverage_wip_temp2 b on a.Id = b.Id
+		) as [source]
+		ON
+		    target.grpel_master_quote_no = source.grpel_master_quote_no AND
+		    --TARGET.effective_dt = SOURCE.effective_dt AND
+		    target.transaction_seq_no = source.transaction_seq_no
+		WHEN NOT MATCHED BY TARGET THEN
+		INSERT
+		(
+		grpel_master_quote_no,effective_dt,expiration_dt,transaction_seq_no
+		,broker_id,customer_id,product_cd,risk_state_cd,insured_nm,insured_type
+		,mailing_address_line1,mailing_address_line2,mailing_address_city_nm,mailing_address_state_cd
+		,mailing_address_zip_cd,mailing_address_county_nm,mailing_address_country_nm,insured_first_nm,insured_last_nm
+		,mobile_phone_no,email
+		,enrollment_initial_start_dt,enrollment_preiod_in_days,enrollment_frequency,override_enrollment_to_open_in
+		,auto_liability_limit_amt,no_of_average_homes,no_of_average_vehicles,no_of_average_watercraft,no_of_youthful_driver,mvr_trigger_rule
+		,commission_pc,minimum_premium_amt,prior_nfp_policy_no,prior_nfp_policy_expiring_dt
+		,excess_liability_limit_1m_premium_amt,excess_liability_limit_1m_override_premium_amt,excess_liability_limit_3m_premium_amt
+		,excess_liability_limit_3m_override_premium_amt,excess_liability_limit_5m_premium_amt,excess_liability_limit_5m_override_premium_amt
+		,excess_liability_limit_10m_premium_amt,excess_liability_limit_10m_override_premium_amt,excess_liability_limit_15m_premium_amt
+		,excess_liability_limit_15m_override_premium_amt,excess_liability_limit_20m_premium_amt,excess_liability_limit_20m_override_premium_amt
+		,excess_liability_limit_30m_premium_amt,excess_liability_limit_30m_override_premium_amt
+		,uninsured_underinsured_motorist_liability_limit_1m_premium_amt,uninsured_underinsured_motorist_liability_limit_1m_override_premium_amt
+		,uninsured_underinsured_motorist_liability_limit_2m_premium_amt,uninsured_underinsured_motorist_liability_limit_2m_override_premium_amt
+		,uninsured_underinsured_motorist_liability_limit_3m_premium_amt,uninsured_underinsured_motorist_liability_limit_3m_override_premium_amt
+		,uninsured_underinsured_motorist_liability_limit_5m_premium_amt,uninsured_underinsured_motorist_liability_limit_5m_override_premium_amt
+		,uninsured_underinsured_motorist_liability_limit_10m_premium_amt,uninsured_underinsured_motorist_liability_limit_10m_override_premium_amt
+		,employment_practices_liability_limit_250_250_25_premium_amt,employment_practices_liability_limit_250_250_25_override_premium_amt
+		,employment_practices_liability_limit_500_500_50_premium_amt,employment_practices_liability_limit_500_500_50_override_premium_amt
+		,family_trust_management_liability_limit_1m_premium_amt,family_trust_management_liability_limit_1m_override_premium_amt
+		,non_profit_do_liability_limit_1m_premium_amt,non_profit_do_liability_limit_1m_override_premium_amt
+		,non_profit_do_liability_limit_2m_premium_amt,non_profit_do_liability_limit_2m_override_premium_amt
+		,non_profit_do_liability_limit_3m_premium_amt,non_profit_do_liability_limit_3m_override_premium_amt
+		,non_profit_do_liability_limit_4m_premium_amt,non_profit_do_liability_limit_4m_override_premium_amt
+		,non_profit_do_liability_limit_5m_premium_amt,non_profit_do_liability_limit_5m_override_premium_amt
+		,source_system_sk,create_ts,update_ts,etl_audit_sk
+		)
+		VALUES 
+		(
+		[source].grpel_master_quote_no,[source].effective_dt,[source].expiration_dt,[source].transaction_seq_no
+		,[source].broker_id,[source].customer_id,[source].product_cd,[source].risk_state_cd,[source].insured_nm,[source].insured_type
+		,[source].mailing_address_line1,[source].mailing_address_line2,[source].mailing_address_city_nm,[source].mailing_address_state_cd
+		,[source].mailing_address_zip_cd,[source].mailing_address_county_nm,[source].mailing_address_country_nm,[source].insured_first_nm,[source].insured_last_nm
+		,[source].mobile_phone_no,[source].email
+		,[source].enrollment_initial_start_dt,[source].enrollment_preiod_in_days,[source].enrollment_frequency,[source].override_enrollment_to_open_in
+		,[source].auto_liability_limit_amt,[source].no_of_average_homes,[source].no_of_average_vehicles,[source].no_of_average_watercraft,[source].no_of_youthful_driver,[source].mvr_trigger_rule
+		,[source].commission_pc,[source].minimum_premium_amt,[source].prior_nfp_policy_no,[source].prior_nfp_policy_expiring_dt
+		,[source].excess_liability_limit_1m_premium_amt,[source].excess_liability_limit_1m_override_premium_amt,[source].excess_liability_limit_3m_premium_amt
+		,[source].excess_liability_limit_3m_override_premium_amt,[source].excess_liability_limit_5m_premium_amt,[source].excess_liability_limit_5m_override_premium_amt
+		,[source].excess_liability_limit_10m_premium_amt,[source].excess_liability_limit_10m_override_premium_amt,[source].excess_liability_limit_15m_premium_amt
+		,[source].excess_liability_limit_15m_override_premium_amt,[source].excess_liability_limit_20m_premium_amt,[source].excess_liability_limit_20m_override_premium_amt
+		,[source].excess_liability_limit_30m_premium_amt,[source].excess_liability_limit_30m_override_premium_amt
+		,[source].uninsured_underinsured_motorist_liability_limit_1m_premium_amt,[source].uninsured_underinsured_motorist_liability_limit_1m_override_premium_amt
+		,[source].uninsured_underinsured_motorist_liability_limit_2m_premium_amt,[source].uninsured_underinsured_motorist_liability_limit_2m_override_premium_amt
+		,[source].uninsured_underinsured_motorist_liability_limit_3m_premium_amt,[source].uninsured_underinsured_motorist_liability_limit_3m_override_premium_amt
+		,[source].uninsured_underinsured_motorist_liability_limit_5m_premium_amt,[source].uninsured_underinsured_motorist_liability_limit_5m_override_premium_amt
+		,[source].uninsured_underinsured_motorist_liability_limit_10m_premium_amt,[source].uninsured_underinsured_motorist_liability_limit_10m_override_premium_amt
+		,[source].employment_practices_liability_limit_250_250_25_premium_amt,[source].employment_practices_liability_limit_250_250_25_override_premium_amt
+		,[source].employment_practices_liability_limit_500_500_50_premium_amt,[source].employment_practices_liability_limit_500_500_50_override_premium_amt
+		,[source].family_trust_management_liability_limit_1m_premium_amt,[source].family_trust_management_liability_limit_1m_override_premium_amt
+		,[source].non_profit_do_liability_limit_1m_premium_amt,[source].non_profit_do_liability_limit_1m_override_premium_amt
+		,[source].non_profit_do_liability_limit_2m_premium_amt,[source].non_profit_do_liability_limit_2m_override_premium_amt
+		,[source].non_profit_do_liability_limit_3m_premium_amt,[source].non_profit_do_liability_limit_3m_override_premium_amt
+		,[source].non_profit_do_liability_limit_4m_premium_amt,[source].non_profit_do_liability_limit_4m_override_premium_amt
+		,[source].non_profit_do_liability_limit_5m_premium_amt,[source].non_profit_do_liability_limit_5m_override_premium_amt
+		,[source].source_system_sk,[source].create_ts,[source].update_ts,[source].etl_audit_sk
+		)
+		WHEN MATCHED THEN
+		    UPDATE 
+            SET
+				[target]. effective_dt = source.effective_dt
+				,[target].broker_id = [source].broker_id
+				,[target].customer_id = [source].customer_id
+				,[target].product_cd = [source].product_cd
+				,[target].risk_state_cd= [source].risk_state_cd
+				,[target].insured_nm= [source].insured_nm
+				,[target].insured_type = [source].insured_type
+				,[target].mailing_address_line1= [source].mailing_address_line1
+				,[target].mailing_address_line2 = [source].mailing_address_line2
+				,[target].mailing_address_city_nm= [source].mailing_address_city_nm
+				,[target].mailing_address_state_cd = [source].mailing_address_state_cd
+				,[target].mailing_address_zip_cd =[source].mailing_address_zip_cd
+				,[target].mailing_address_county_nm = [source].mailing_address_county_nm
+				,[target].mailing_address_country_nm = [source].mailing_address_country_nm
+				,[target].insured_first_nm= [source].insured_first_nm
+				,[target].insured_last_nm= [source].insured_last_nm
+				,[target].mobile_phone_no= [source].mobile_phone_no
+				,[target].email= [source].email
+				,[target].enrollment_initial_start_dt =[source].enrollment_initial_start_dt
+				,[target].enrollment_preiod_in_days =[source].enrollment_preiod_in_days
+				,[target].enrollment_frequency=[source].enrollment_frequency
+				,[target].override_enrollment_to_open_in=[source].override_enrollment_to_open_in
+				,[target].auto_liability_limit_amt=[source].auto_liability_limit_amt
+				,[target].no_of_average_homes=[source].no_of_average_homes
+				,[target].no_of_average_vehicles=[source].no_of_average_vehicles
+				,[target].no_of_average_watercraft= [source].no_of_average_watercraft
+				,[target].no_of_youthful_driver= [source].no_of_youthful_driver
+				,[target].mvr_trigger_rule=[source].mvr_trigger_rule
+				,[target].commission_pc=[source].commission_pc
+				,[target].minimum_premium_amt=[source].minimum_premium_amt
+				,[target].prior_nfp_policy_no=[source].prior_nfp_policy_no
+				,[target].prior_nfp_policy_expiring_dt=[source].prior_nfp_policy_expiring_dt
+				,[target].excess_liability_limit_1m_premium_amt=[source].excess_liability_limit_1m_premium_amt
+				,[target].excess_liability_limit_1m_override_premium_amt= [source].excess_liability_limit_1m_override_premium_amt
+				,[target].excess_liability_limit_3m_premium_amt= [source].excess_liability_limit_3m_premium_amt
+				,[target].excess_liability_limit_3m_override_premium_amt=[source].excess_liability_limit_3m_override_premium_amt
+				,[target].excess_liability_limit_5m_premium_amt = [source].excess_liability_limit_5m_premium_amt
+				,[target].excess_liability_limit_5m_override_premium_amt =[source].excess_liability_limit_5m_override_premium_amt
+				,[target].excess_liability_limit_10m_premium_amt=[source].excess_liability_limit_10m_premium_amt
+				,[target].excess_liability_limit_10m_override_premium_amt= [source].excess_liability_limit_10m_override_premium_amt
+				,[target].excess_liability_limit_15m_premium_amt = [source].excess_liability_limit_15m_premium_amt
+				,[target].excess_liability_limit_15m_override_premium_amt = [source].excess_liability_limit_15m_override_premium_amt
+				,[target].excess_liability_limit_20m_premium_amt = [source].excess_liability_limit_20m_premium_amt
+				,[target].excess_liability_limit_20m_override_premium_amt= [source].excess_liability_limit_20m_override_premium_amt
+				,[target].excess_liability_limit_30m_premium_amt = [source].excess_liability_limit_30m_premium_amt,
+				.[target].excess_liability_limit_30m_override_premium_amt = [source].excess_liability_limit_30m_override_premium_amt
+				,[target].uninsured_underinsured_motorist_liability_limit_1m_premium_am = [source].uninsured_underinsured_motorist_liability_limit_1m_premium_amt
+				,[target].uninsured_underinsured_motorist_liability_limit_1m_override_premium_amt= [source].uninsured_underinsured_motorist_liability_limit_1m_override_premium_amt
+				,[target].uninsured_underinsured_motorist_liability_limit_2m_premium_amt = [source].uninsured_underinsured_motorist_liability_limit_2m_premium_amt
+				,[target].uninsured_underinsured_motorist_liability_limit_2m_override_premium_amt = [source].uninsured_underinsured_motorist_liability_limit_2m_override_premium_amt
+				,[target].uninsured_underinsured_motorist_liability_limit_3m_premium_amt = [source].uninsured_underinsured_motorist_liability_limit_3m_premium_amt
+				,[target].uninsured_underinsured_motorist_liability_limit_3m_override_premium_amt =[source].uninsured_underinsured_motorist_liability_limit_3m_override_premium_amt
+				,[target].uninsured_underinsured_motorist_liability_limit_5m_premium_amt= [source].uninsured_underinsured_motorist_liability_limit_5m_premium_amt
+				,[target].uninsured_underinsured_motorist_liability_limit_5m_override_premium_amt=[source].uninsured_underinsured_motorist_liability_limit_5m_override_premium_amt
+				,[target].uninsured_underinsured_motorist_liability_limit_10m_premium_amt = [source].uninsured_underinsured_motorist_liability_limit_10m_premium_amt
+				,[target].uninsured_underinsured_motorist_liability_limit_10m_override_premium_amt= [source].uninsured_underinsured_motorist_liability_limit_10m_override_premium_amt
+				,[target].employment_practices_liability_limit_250_250_25_premium_amt=[source].employment_practices_liability_limit_250_250_25_premium_amt
+				,[target].employment_practices_liability_limit_250_250_25_override_premium_amt=[source].employment_practices_liability_limit_250_250_25_override_premium_amt
+				,[target].employment_practices_liability_limit_500_500_50_premium_amt=[source].employment_practices_liability_limit_500_500_50_premium_amt
+				,[target].employment_practices_liability_limit_500_500_50_override_premium_amt=[source].employment_practices_liability_limit_500_500_50_override_premium_amt
+				,[target].family_trust_management_liability_limit_1m_premium_amt= [source].family_trust_management_liability_limit_1m_premium_amt
+				,[target].family_trust_management_liability_limit_1m_override_premium_amt =[source].family_trust_management_liability_limit_1m_override_premium_amt
+				,[target].non_profit_do_liability_limit_1m_premium_amt= [source].non_profit_do_liability_limit_1m_premium_amt
+				,[target].non_profit_do_liability_limit_1m_override_premium_amt = [source].non_profit_do_liability_limit_1m_override_premium_amt
+				,[target].non_profit_do_liability_limit_2m_premium_amt = [source].non_profit_do_liability_limit_2m_premium_amt
+				,[target].non_profit_do_liability_limit_2m_override_premium_amt = [source].non_profit_do_liability_limit_2m_override_premium_amt
+				,[target].non_profit_do_liability_limit_3m_premium_amt = [source].non_profit_do_liability_limit_3m_premium_amt
+				,[target].non_profit_do_liability_limit_3m_override_premium_amt =[source].non_profit_do_liability_limit_3m_override_premium_amt
+				,[target].non_profit_do_liability_limit_4m_premium_amt =[source].non_profit_do_liability_limit_4m_premium_amt
+				,[target].non_profit_do_liability_limit_4m_override_premium_amt= [source].non_profit_do_liability_limit_4m_override_premium_amt
+				,[target].non_profit_do_liability_limit_5m_premium_amt = [source].non_profit_do_liability_limit_5m_premium_amt
+				,[target].non_profit_do_liability_limit_5m_override_premium_amt=[source].non_profit_do_liability_limit_5m_override_premium_amt
+				,[target].update_ts = getdate()
+		;
 
 		SET @rows_affected=@@ROWCOUNT;
 	
