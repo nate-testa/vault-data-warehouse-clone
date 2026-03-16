b6a0372e8d21caed34436bb6ca52043c1890930f
Tuba Mohsin <tuba.mohsin@vault.insurance>
2026-03-16 17:31:06 -0400

    New staging table

---
+++ SQL/DDL/947_Create_EDW_stage_BrokerageProducer.sql
@@ -0,0 +1,22 @@
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES 
+               WHERE TABLE_SCHEMA = 'edw_stage' 
+               AND TABLE_NAME = 'BrokerageProducer')
+BEGIN
+CREATE TABLE edw_stage.BrokerageProducer
+(
+[Id]                               [uniqueidentifier] NOT NULL,
+[BrokerageId]                      [uniqueidentifier] NOT NULL,
+[Name]                             [nvarchar](500) NULL,
+[MailingAddressLine1]              [nvarchar](500) NULL,
+[MailingAddressLine2]              [nvarchar](500) NULL,
+[MailingAddressLineUnit]           [nvarchar](500) NULL,
+[MailingAddressCity]               [nvarchar](500) NULL,
+[MailingAddressState]              [nvarchar](500) NULL,
+[MailingAddressZipCode]            [nvarchar](500) NULL,
+[MailingAddressCounty]             [nvarchar](500) NULL,
+[MailingAddressCountry]            [nvarchar](500) NULL,
+[Phone]                            [nvarchar](20) NULL,
+[CreatedDate]                      [datetime2](7) NOT NULL,
+[UpdatedDate]                      [datetime2](7) NULL
+);
+END
