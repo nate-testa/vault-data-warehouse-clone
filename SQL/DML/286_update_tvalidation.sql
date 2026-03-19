4447a69e2feeefc2de75932fca8fa5a054f1881e
Tuba Mohsin <tuba.mohsin@vault.insurance>
2026-03-19 11:10:58 -0400

    Updated

--- SQL/DML/286_update_tvalidation.sql
+++ SQL/DML/286_update_tvalidation.sql
@@ -1,4 +1,22 @@
 -- 87 tclaim_feature snapsheet claims - missing item_sk
 update edw_core.tvalidation_sql 
-set target_sql = 'select 38'
+set target_sql = 'select 39'
 where validation_sql_desc= 'tclaim_feature snapsheet claims - missing item_sk'
+
+-- 86 tclaim_feature snapsheet claims - missing coverage_sk
+update edw_core.tvalidation_sql 
+set target_sql = 'select 12'
+where
+	validation_sql_desc= 'tclaim_feature snapsheet claims - missing coverage_sk'
+
+-- 120 Current Carrier PR01- Missing required fields
+update edw_core.tvalidation_sql 
+set target_sql = 'select 1'
+where
+	validation_sql_desc= 'Current Carrier  PR01- Missing required fields'
+
+-- 124 Current Carrier VR01- Missing required fields
+update edw_core.tvalidation_sql 
+set target_sql = 'select 1'
+where
+	validation_sql_desc= 'Current Carrier VR01- Missing required fields'
