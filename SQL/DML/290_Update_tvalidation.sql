349f23b144d1f9b5ac67c44a786e704a30105103
Tuba Mohsin <tuba.mohsin@vault.insurance>
2026-03-26 10:38:44 -0400

    New validations

---
+++ SQL/DML/290_Update_tvalidation.sql
@@ -0,0 +1,4 @@
+-- 87 tclaim_feature snapsheet claims - missing item_sk
+update edw_core.tvalidation_sql 
+set target_sql = 'select 40'
+where validation_sql_desc= 'tclaim_feature snapsheet claims - missing item_sk'
