fdfb78f4e3eaa2b0608fc3ae49233db866c93543
Tuba Mohsin <tuba.mohsin@vault.insurance>
2026-03-27 13:18:08 -0400

    Updated 3_27

--- SQL/DML/290_Update_tvalidation.sql
+++ SQL/DML/290_Update_tvalidation.sql
@@ -2,3 +2,13 @@
 update edw_core.tvalidation_sql 
 set target_sql = 'select 40'
 where validation_sql_desc= 'tclaim_feature snapsheet claims - missing item_sk'
+
+-- 25 tpolicy_transaction - policies having vehicle_coverage_sk = 0 for AU
+update edw_core.tvalidation_sql 
+set target_sql = 'select 1'
+where validation_sql_desc= 'tpolicy_transaction - policies having vehicle_coverage_sk = 0 for AU' 
+
+-- 44  tpolicy_transaction - LUX - collection_class_type_sk = 0
+update edw_core.tvalidation_sql 
+set target_sql = 'select 2'
+where validation_sql_desc= 'tpolicy_transaction - LUX - collection_class_type_sk = 0'
