9e75ace069cf6f3d02938910df3ea131c5ccd3f7
Tuba Mohsin <tuba.mohsin@vault.insurance>
2026-04-13 14:30:31 -0400

    Update

--- SQL/DML/293_Update_tvalidation.sql
+++ SQL/DML/293_Update_tvalidation.sql
@@ -2,3 +2,9 @@
 update edw_core.tvalidation_sql 
 set target_sql = 'select 43'
 where validation_sql_desc= 'tclaim_feature snapsheet claims - missing item_sk'
+
+-- 84 tclaim_feature - claim_coverage_desc is null
+
+update edw_core.tvalidation_sql 
+set target_sql = 'select 3'
+where validation_sql_desc= 'tclaim_feature - claim_coverage_desc is null'
