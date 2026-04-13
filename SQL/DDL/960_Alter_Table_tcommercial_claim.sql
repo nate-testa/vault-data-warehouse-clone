06bbd56b7d386fb8307ee1c166b43a99a93b1075
Yunus Mohammed <yunus.mohammed@vault.insurance>
2026-04-13 11:42:29 -0400

    Commercial_claim new columns added

---
+++ SQL/DDL/960_Alter_Table_tcommercial_claim.sql
@@ -0,0 +1,57 @@
+IF NOT EXISTS (
+    SELECT 1
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_SCHEMA = 'edw_commercial'
+      AND TABLE_NAME = 'tpolicy_transaction_summary'
+      AND COLUMN_NAME = 'loss_location_desc'
+)
+BEGIN
+    ALTER TABLE edw_commercial.tcommercial_claim ADD loss_location_desc DECIMAL(15,2) ;
+END;
+ 
+IF NOT EXISTS (
+    SELECT 1
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_SCHEMA = 'edw_commercial'
+      AND TABLE_NAME = 'tcommercial_claim'
+      AND COLUMN_NAME = 'large_loss_in'
+)
+BEGIN
+    ALTER TABLE edw_commercial.tcommercial_claim ADD large_loss_in DECIMAL(15,2) ;
+END;
+
+IF NOT EXISTS (
+    SELECT 1
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_SCHEMA = 'edw_commercial'
+      AND TABLE_NAME = 'tcommercial_claim'
+      AND COLUMN_NAME = 'closed_reason_desc'
+)
+BEGIN
+    ALTER TABLE edw_commercial.tcommercial_claim ADD closed_reason_desc VARCHAR(255);
+END;
+
+IF NOT EXISTS (
+    SELECT 1
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_SCHEMA = 'edw_commercial'
+      AND TABLE_NAME = 'tcommercial_claim'
+      AND COLUMN_NAME = 'last_update_ts'
+)
+BEGIN
+    ALTER TABLE edw_commercial.tcommercial_claim ADD last_update_ts DATETIME;
+END;
+
+IF NOT EXISTS (
+    SELECT 1
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_SCHEMA = 'edw_commercial'
+      AND TABLE_NAME = 'tcommercial_claim'
+      AND COLUMN_NAME = 'closed_reason_desc'
+)
+BEGIN
+    ALTER TABLE edw_commercial.tcommercial_claim ADD closed_reason_desc VARCHAR(255) ;
+END;
+
+
+ 
