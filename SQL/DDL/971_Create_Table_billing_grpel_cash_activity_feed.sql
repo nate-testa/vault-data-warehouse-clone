0acca8c0e36bca26def49a4900979e8ff8c9469d
Rushin Shah <rushin.shah@vault.insurance>
2026-04-23 18:23:07 -0400

    GPEL Billing Table

---
+++ SQL/DDL/971_Create_Table_billing_grpel_cash_activity_feed.sql
@@ -0,0 +1,38 @@
+IF NOT EXISTS
+(SELECT 1 FROM INFORMATION_SCHEMA.TABLES
+WHERE TABLE_SCHEMA = 'edw_integration'
+AND TABLE_name = 'billing_grpel_cash_activity_feed')
+BEGIN
+
+CREATE TABLE edw_integration.billing_grpel_cash_activity_feed
+( 
+company varchar(255),
+group_account varchar(255) NOT NULL,
+group_name varchar(255),
+effective_date date,
+expiration_date date,
+payor_type varchar(255),
+product varchar(255),
+payment_from varchar(255),
+category varchar(255),
+paid_via varchar(255),
+reference_code nvarchar(max),
+payment_date date,
+amount decimal(15,2),
+month_end date,
+create_ts datetime2 ,
+update_ts datetime2 ,
+etl_audit_sk int
+);
+END; 
+
+IF EXISTS
+(SELECT 1 FROM edw_integration.tintegration_table_detail
+	where table_nm = 'billing_grpel_cash_activity_feed')
+BEGIN
+	delete edw_integration.tintegration_table_detail
+	where table_nm = 'billing_grpel_cash_activity_feed' ; 
+END ; 
+
+INSERT INTO edw_integration.tintegration_table_detail (table_nm,table_type,table_desc,load_method,load_type,load_frequency,create_ts,update_ts)
+VALUES ('billing_grpel_cash_activity_feed','Feed','This table provides monthly cash activity for group excess policies','Stored Procedure','Insert','Monthly',getdate(),getdate());
---
+++ SQL/DDL/972_Create_Table_billing_grpel_payment_due_feed.sql
@@ -0,0 +1,35 @@
+IF NOT EXISTS
+(SELECT 1 FROM INFORMATION_SCHEMA.TABLES
+WHERE TABLE_SCHEMA = 'edw_integration'
+AND TABLE_name = 'billing_grpel_payment_due_feed')
+BEGIN
+
+CREATE TABLE edw_integration.billing_grpel_payment_due_feed
+( 
+company varchar(255),
+group_account varchar(255) NOT NULL,
+group_name varchar(255),
+effective_date date,
+expiration_date date,
+payor_type varchar(255),
+product varchar(255),
+total_premium decimal(15,2),
+payments_made decimal(15,2),
+balance_due_as_of_month_end decimal(15,2),
+month_end date,
+create_ts datetime2 ,
+update_ts datetime2 ,
+etl_audit_sk int
+);
+END; 
+
+IF EXISTS
+(SELECT 1 FROM edw_integration.tintegration_table_detail
+	where table_nm = 'billing_grpel_payment_due_feed')
+BEGIN
+	delete edw_integration.tintegration_table_detail
+	where table_nm = 'billing_grpel_payment_due_feed' ; 
+END ; 
+
+INSERT INTO edw_integration.tintegration_table_detail (table_nm,table_type,table_desc,load_method,load_type,load_frequency,create_ts,update_ts)
+VALUES ('billing_grpel_payment_due_feed','Feed','This table provides payment amount due for group excess policies at month end','Stored Procedure','Insert','Monthly',getdate(),getdate());
