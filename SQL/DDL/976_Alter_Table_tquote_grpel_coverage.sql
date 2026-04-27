942305bc06997f13c59971b56b7c2f1d300a0630
Tuba Mohsin <tuba.mohsin@vault.insurance>
2026-04-27 17:03:44 -0400

    updated tquote_grpel_coverage

--- SQL/DDL/976_Alter_Table_tquote_grpel_coverage.sql
+++ SQL/DDL/976_Alter_Table_tquote_grpel_coverage.sql
@@ -8,3 +8,38 @@
 BEGIN
     ALTER TABLE edw_core.tquote_grpel_coverage ADD no_other_watercraft Varchar(255);
 END;
+
+      IF NOT EXISTS (
+    SELECT 1
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_SCHEMA = 'edw_core'
+      AND TABLE_NAME = 'tquote_grpel_coverage'
+      AND COLUMN_NAME = 'underlying_auto_liability_limit_amt'
+)
+BEGIN
+    ALTER TABLE edw_core.tquote_grpel_coverage ADD underlying_auto_liability_limit_amt Varchar(255);
+END;
+
+      IF NOT EXISTS (
+    SELECT 1
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_SCHEMA = 'edw_core'
+      AND TABLE_NAME = 'tquote_grpel_coverage'
+      AND COLUMN_NAME = 'underlying_home_liability_limit_amt'
+)
+BEGIN
+    ALTER TABLE edw_core.tquote_grpel_coverage ADD underlying_home_liability_limit_amt Varchar(255);
+END;
+
+      IF NOT EXISTS (
+    SELECT 1
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_SCHEMA = 'edw_core'
+      AND TABLE_NAME = 'tquote_grpel_coverage'
+      AND COLUMN_NAME = 'underlying_watercraft_liability_limit_amt'
+)
+BEGIN
+    ALTER TABLE edw_core.tquote_grpel_coverage ADD underlying_watercraft_liability_limit_amt Varchar(255);
+END;
+
+
