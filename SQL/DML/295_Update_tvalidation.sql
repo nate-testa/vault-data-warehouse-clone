01fd451c1e8eb0e3a16634d9d9934d47463e8873
Tuba Mohsin <tuba.mohsin@vault.insurance>
2026-04-28 15:20:45 -0400

    Updated

--- SQL/DML/295_Update_tvalidation.sql
+++ SQL/DML/295_Update_tvalidation.sql
@@ -12,5 +12,16 @@ where validation_sql_desc= 'tclaim_feature - claim_coverage_desc is null'
 
 -- 86 tclaim_feature snapsheet claims - missing coverage_sk
 update edw_core.tvalidation_sql 
-set target_sql = 'select 15'
+set target_sql = 'select 16'
 where validation_sql_desc= 'tclaim_feature snapsheet claims - missing coverage_sk'
+
+-- 81 Snapsheet Validation- Cancelled approved reserves/payments
+set source_sql='select count(*) 
+from edw_stage_snapsheet.financial_transactions 
+where stage=''cancelled'' 
+and approved_at is not null 
+and remote_identifier is null 
+and financial_transaction_type != ''recovery'''
+,target_sql='select 4',
+validation_sql_desc='Snapsheet Validation- Cancelled approved reserves/payments'
+where validation_sql_desc='Snapsheet Validation- Cancelled approved reserves/payments'
