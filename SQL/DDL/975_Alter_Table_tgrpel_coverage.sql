facc18593a87c1951de76d86184e1b447cb91f74
Tuba Mohsin <tuba.mohsin@vault.insurance>
2026-04-28 12:34:04 -0400

    Update

--- SQL/DDL/975_Alter_Table_tgrpel_coverage.sql
+++ SQL/DDL/975_Alter_Table_tgrpel_coverage.sql
@@ -3,8 +3,8 @@
     FROM INFORMATION_SCHEMA.COLUMNS
     WHERE TABLE_SCHEMA = 'edw_core'
       AND TABLE_NAME = 'tgrpel_coverage'
-      AND COLUMN_NAME = 'no_other_watercraft'
+      AND COLUMN_NAME = 'no_of_other_watercraft'
 )
 BEGIN
-    ALTER TABLE edw_core.tgrpel_coverage ADD no_other_watercraft Varchar(255);
+    ALTER TABLE edw_core.tgrpel_coverage ADD no_of_other_watercraft Varchar(255);
 END;
--- SQL/DDL/976_Alter_Table_tquote_grpel_coverage.sql
+++ SQL/DDL/976_Alter_Table_tquote_grpel_coverage.sql
@@ -3,10 +3,10 @@
     FROM INFORMATION_SCHEMA.COLUMNS
     WHERE TABLE_SCHEMA = 'edw_core'
       AND TABLE_NAME = 'tquote_grpel_coverage'
-      AND COLUMN_NAME = 'no_other_watercraft'
+      AND COLUMN_NAME = 'no_of_other_watercraft'
 )
 BEGIN
-    ALTER TABLE edw_core.tquote_grpel_coverage ADD no_other_watercraft Varchar(255);
+    ALTER TABLE edw_core.tquote_grpel_coverage ADD no_of_other_watercraft Varchar(255);
 END;
 
       IF NOT EXISTS (
