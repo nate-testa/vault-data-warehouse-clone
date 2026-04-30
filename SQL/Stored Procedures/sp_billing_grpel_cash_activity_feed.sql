86aaf2fbd2f8baba63da09c97efa852400dd13be
Yunus Mohammed <yunus.mohammed@vault.insurance>
2026-04-30 10:34:32 -0400

    Code updated

--- SQL/Stored Procedures/sp_billing_grpel_cash_activity_feed.sql
+++ SQL/Stored Procedures/sp_billing_grpel_cash_activity_feed.sql
@@ -2,7 +2,7 @@
 -- =================================================================================================
 -- Author:		Yunus Mohammed
 -- Create Date: 04/29/2026
--- Description: This procedures inserts workday reserve data
+-- Description: This procedures inserts GRPEL Cash Activity feed
 ---------------------------------------------------------------------------------------------------
 -- Change date      |Author						|	Change Description
 ---------------------------------------------------------------------------------------------------
@@ -27,8 +27,12 @@ BEGIN
 		SELECT @last_source_extract_ts = edw_core.fn_get_last_source_extract_ts(@process_nm);
 		EXEC edw_core.sp_ins_tetl_audit @process_nm,@current_date,@etl_audit_sk=@etl_audit_sk OUTPUT;
 
+		DECLARE @year_month INT,@begin_dt DATE,@end_dt DATE,@begin_sk INT,@end_sk INT
+
 		DECLARE @last_day_month DATE, @year_month INT;
-		select @year_month = yearmonth
+
+		DECLARE cur_main CURSOR FOR
+		select yearmonth
 		from edw_core.tdate
 		where
 			actual_dt > case
@@ -37,67 +41,85 @@ BEGIN
 							end
 			and actual_dt < cast(@current_date as date)
 		group by yearmonth
-		order by 1;	
-		
-		SELECT @last_day_month = actual_dt FROM edw_core.tdate WHERE yearmonth = @year_month and month_end_in = 'Y';
+		order by 1; 
 
-		DELETE FROM edw_integration.billing_grpel_cash_activity_feed
-        WHERE month_end = @last_day_month; 
-		
-		WITH billing_grpel_cash_activity_feed_temp AS
-		(
-            select 
-                'Vault E & S Insurance Company' as company,
-                bap.grpel_master_policy_no as group_account,
-                gmc.insured_nm as group_name,
-                gmc.effective_dt as effective_date,
-                gmc.expiration_dt as expiration_date,
-                bap.bill_type as payor_type,
-                'Group Personal Excess Liability' as product,
-                bap.payment_from_type as payment_from,
-                bap.receivable_cd as category,
-                bap.payment_method as paid_via,
-                bap.user_remark reference_code,
-                bap.payment_dt as payment_date,
-                bap.payment_amt as amount,
-                @last_day_month AS month_end,
-                GETDATE() as create_ts,
-                GETDATE() as update_ts,
-				@etl_audit_sk as etl_audit_sk
-                from
-                    edw_core.tbilling_account_payment bap
-                    inner join edw_core.tgrpel_master_coverage gmc on bap.grpel_master_policy_no = gmc.grpel_master_policy_no
-                    and gmc.transaction_seq_no = 
-                                            (
-                                                select max(transaction_seq_no)
-                                                from
-                                                    edw_core.tgrpel_master_coverage gmc1
-                                                where
-                                                    gmc1.grpel_master_policy_no = gmc.grpel_master_policy_no
-                                            )
-        )
-              
+		OPEN cur_main
+		FETCH NEXT FROM cur_main INTO @year_month
+		WHILE @@FETCH_STATUS = 0
+		BEGIN
+			EXEC edw_core.sp_ins_tetl_audit @process_nm,@current_date,@etl_audit_sk=@etl_audit_sk OUTPUT; 	
+			
+			select @begin_dt = MIN(actual_dt),@end_dt = MAX(actual_dt), 
+			@begin_sk = MIN(date_sk), @end_sk = MAX(date_sk) 
+			from
+			edw_core.tdate
+			where
+			yearmonth=@year_month;
 
-		INSERT INTO edw_integration.billing_grpel_cash_activity_feed
-		(
-            company,group_account,group_name,effective_date,expiration_date,payor_type,product,payment_from,category,paid_via,reference_code,payment_date,amount,
-            month_end,create_ts,update_ts,etl_audit_sk
-		)
-		SELECT
-            company,group_account,group_name,effective_date,expiration_date,payor_type,product,payment_from,category,paid_via,reference_code,payment_date,amount,
-            month_end,create_ts,update_ts,etl_audit_sk
-		FROM
-			billing_grpel_cash_activity_feed_temp
-		
-		SET @rows_affected=@@ROWCOUNT;
+			SET @parameter_desc= 'last_source_extract_ts >=' + CAST(@begin_dt AS VARCHAR(200))
 
-		-- Update control table
-		SET @new_last_source_extract_ts =dateadd(day,-1,cast(@current_date as date))
-		EXEC edw_core.sp_upd_tetl_control @process_nm,@new_last_source_extract_ts;
+			DELETE FROM edw_integration.billing_grpel_cash_activity_feed
+        	WHERE month_end = @year_month; 
 
-		-- Update audit table
-		SET @parameter_desc= @parameter_desc + ' AND last_source_extract_ts <=' + CAST(@new_last_source_extract_ts AS VARCHAR(200))
-		EXEC edw_core.sp_upd_tetl_audit @etl_audit_sk,@rows_affected,@parameter_desc;
+			WITH billing_grpel_cash_activity_feed_temp AS
+			(
+				select 
+					'Vault E & S Insurance Company' as company,
+					bap.grpel_master_policy_no as group_account,
+					gmc.insured_nm as group_name,
+					gmc.effective_dt as effective_date,
+					gmc.expiration_dt as expiration_date,
+					bap.bill_type as payor_type,
+					'Group Personal Excess Liability' as product,
+					bap.payment_from_type as payment_from,
+					bap.receivable_cd as category,
+					bap.payment_method as paid_via,
+					bap.user_remark reference_code,
+					bap.payment_dt as payment_date,
+					bap.payment_amt as amount,
+					@end_dt AS month_end,
+					GETDATE() as create_ts,
+					GETDATE() as update_ts,
+					@etl_audit_sk as etl_audit_sk
+					from
+						edw_core.tbilling_account_payment bap
+						inner join edw_core.tgrpel_master_coverage gmc on bap.grpel_master_policy_no = gmc.grpel_master_policy_no
+						and gmc.transaction_seq_no = 
+												(
+													select max(transaction_seq_no)
+													from
+														edw_core.tgrpel_master_coverage gmc1
+													where
+														gmc1.grpel_master_policy_no = gmc.grpel_master_policy_no
+												)
+					WHERE
+						CAST(bap.TransactionDate as date) between @begin_dt and @end_dt
+			)              
+
+			INSERT INTO edw_integration.billing_grpel_cash_activity_feed
+			(
+				company,group_account,group_name,effective_date,expiration_date,payor_type,product,payment_from,category,paid_via,reference_code,payment_date,amount,
+				month_end,create_ts,update_ts,etl_audit_sk
+			)
+			SELECT
+				company,group_account,group_name,effective_date,expiration_date,payor_type,product,payment_from,category,paid_via,reference_code,payment_date,amount,
+				month_end,create_ts,update_ts,etl_audit_sk
+			FROM
+				billing_grpel_cash_activity_feed_temp
+
+			END		
+			
+			SET @rows_affected=@@ROWCOUNT;
+
+			-- Update control table
+			SET @new_last_source_extract_ts= dateadd(day,-1,cast(@current_date as date))
+			EXEC edw_core.sp_upd_tetl_control @process_nm,@new_last_source_extract_ts;
+
+			-- Update audit table
+			SET @parameter_desc= @parameter_desc + ' AND last_source_extract_ts <=' + CAST(@new_last_source_extract_ts AS VARCHAR(200))
+			EXEC edw_core.sp_upd_tetl_audit @etl_audit_sk,@rows_affected,@parameter_desc;		
+
+			SELECT @last_source_extract_ts = edw_core.fn_get_last_source_extract_ts(@process_nm);
 
 	END TRY
 	BEGIN CATCH
