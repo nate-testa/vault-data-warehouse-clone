0aa77a4044bad34d6b016c6a2158c29cd4cbe810
Tuba Mohsin <tuba.mohsin@vault.insurance>
2026-05-12 16:08:40 -0400

    Updated 123 to 2

--- SQL/DML/303_Update_tvalidation.sql
+++ SQL/DML/303_Update_tvalidation.sql
@@ -8,3 +8,9 @@ where validation_sql_desc= 'tclaim_feature snapsheet claims - missing coverage_s
 update edw_core.tvalidation_sql 
 set target_sql = 'select 48'
 where validation_sql_desc= 'tclaim_feature snapsheet claims - missing item_sk'
+
+--123 Current Carrier SJ01- Policies having no A1 record  
+
+update edw_core.tvalidation_sql 
+set target_sql = 'select 2'
+where validation_sql_desc= 'Current Carrier SJ01- Policies having no A1 record'
