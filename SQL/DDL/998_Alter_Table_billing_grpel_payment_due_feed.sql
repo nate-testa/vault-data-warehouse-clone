9a22b932d7cb0975de08e277ed28e9b27454877e
Yunus Mohammed <yunus.mohammed@vault.insurance>
2026-05-14 02:23:08 -0400

    Columns renamed and new columns added

---
+++ SQL/DDL/998_Alter_Table_billing_grpel_payment_due_feed.sql
@@ -0,0 +1,55 @@
+IF EXISTS (
+    SELECT 1
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_SCHEMA = 'edw_integration'
+      AND TABLE_NAME = 'billing_grpel_payment_due_feed'
+      AND COLUMN_NAME = 'total_premium'
+)
+BEGIN
+    exec sp_rename 'edw_integration.billing_grpel_payment_due_feed.total_premium','total_participant_premium';
+END;
+
+IF EXISTS (
+    SELECT 1
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_SCHEMA = 'edw_integration'
+      AND TABLE_NAME = 'billing_grpel_payment_due_feed'
+      AND COLUMN_NAME = 'payments_made'
+)
+BEGIN
+    exec sp_rename 'edw_integration.billing_grpel_payment_due_feed.payments_made','payments_received';
+END;
+
+IF EXISTS (
+    SELECT 1
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_SCHEMA = 'edw_integration'
+      AND TABLE_NAME = 'billing_grpel_payment_due_feed'
+      AND COLUMN_NAME = 'balance_due_as_of_month_end'
+)
+BEGIN
+    exec sp_rename 'edw_integration.billing_grpel_payment_due_feed.balance_due_as_of_month_end','net_amount_due_vault';
+END;
+
+IF NOT EXISTS (
+    SELECT 1
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_SCHEMA = 'edw_integration'
+      AND TABLE_NAME = 'billing_grpel_payment_due_feed'
+      AND COLUMN_NAME = 'group_minimum_premium'
+)
+BEGIN
+    ALTER TABLE edw_integration.billing_grpel_payment_due_feed ADD group_minimum_premium DECIMAL(15,2);
+END;
+
+
+IF NOT EXISTS (
+    SELECT 1
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_SCHEMA = 'edw_integration'
+      AND TABLE_NAME = 'billing_grpel_payment_due_feed'
+      AND COLUMN_NAME = 'commission_due_broker'
+)
+BEGIN
+    ALTER TABLE edw_integration.billing_grpel_payment_due_feed ADD commission_due_broker DECIMAL(15,2);
+END;
