3fc3cbbd97c2bc7e207c14244e53a355363f61bf
Tuba Mohsin <tuba.mohsin@vault.insurance>
2026-06-02 10:20:32 -0400

    add 86

--- SQL/DML/306_Update_tvalidation.sql
+++ SQL/DML/306_Update_tvalidation.sql
@@ -1,15 +1,20 @@
-   -- 84 tclaim_feature - claim_coverage_desc is null
+-- 84 tclaim_feature - claim_coverage_desc is null
 update edw_core.tvalidation_sql 
 set target_sql = 'select 1'
 where validation_sql_desc= 'tclaim_feature - claim_coverage_desc is null'
 
-  -- 87 tclaim_feature snapsheet claims - missing item_sk
+-- 87 tclaim_feature snapsheet claims - missing item_sk
 update edw_core.tvalidation_sql 
 set target_sql = 'select 47'
 where validation_sql_desc= 'tclaim_feature snapsheet claims - missing item_sk'
 
 
-  -- 164 tpolicy_transaction - ceded premium but no gross premium
+--86 tclaim_feature snapsheet claims - missing coverage_sk
+update edw_core.tvalidation_sql 
+set target_sql = 'select 7'
+where validation_sql_desc= 'tclaim_feature snapsheet claims - missing coverage_sk'
+
+-- 164 tpolicy_transaction - ceded premium but no gross premium
 update edw_core.tvalidation_sql 
 set active_in = 'N'
 where validation_sql_desc= 'tpolicy_transaction - ceded premium but no gross premium'
